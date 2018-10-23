fe38cc8be7d383b88c702aa9f5a8bb0fdfa5c34a
Manuel Carrizo <manuelcarrizo@users.noreply.github.com>
2018-10-23 04:11:13 +0000

    Added db:seed with admin user (#32)
    Add db:seed for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Admin',
+  email: 'admin@circuitverse.org',
+  password: 'password',
+  admin: true
+)
