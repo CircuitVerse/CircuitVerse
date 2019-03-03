736ca11fddea2590ea49fec17f8ba34e79e84e40
Ankit Kataria <ankitkataria28@gmail.com>
2019-03-04 03:38:11 +0530

    Add and initialize pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :auth_error
+  rescue_from ApplicationPolicy::CustomAuthException, with: :custom_auth_error
+
+  def auth_error
+    render plain: 'You are not authorized to do the requested operation'
+  end
+
+  def custom_auth_error(exception)
+    render plain: "Not Authorized: #{exception.custom_message}"
+  end
 end
