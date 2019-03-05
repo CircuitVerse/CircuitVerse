64a734ce45fa564cbede5a81433d908c13f445b4
shubhankarsharma00 <shubhankarsharma78@gmail.com>
2019-03-05 20:41:58 +0530

    Revert "tried to add tags"
    This reverts commit 1e2dc8f7baf32709b529ec673c66ab3eb9106319.

--- app/models/tag.rb
+++
@@ -1,4 +0,0 @@
-class Tag < ApplicationRecord
-	has_many :taggings
-	has_many :projects, through: :taggings
-end
--- app/models/tagging.rb
+++
@@ -1,4 +0,0 @@
-class Tagging < ApplicationRecord
-  belongs_to :project
-  belongs_to :tag
-end
