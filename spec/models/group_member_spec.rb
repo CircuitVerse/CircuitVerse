c26f2005bef9edb77bda212006a6615fb39483d4
Ankit Kataria <ankitkataria28@gmail.com>
2019-03-07 21:12:53 +0530

    Add tests for assignment, group, group_member and invitation public methods

--- spec/models/group_member_spec.rb
+++ spec/models/group_member_spec.rb
@@ -1,20 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe GroupMember, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+    @group = FactoryBot.create(:group, mentor: @user)
+  end
+
   describe "associations" do
     it { should belong_to(:group) }
     it { should belong_to(:user) }
   end
 
   describe "callbacks" do
-    before do
-      @user = FactoryBot.create(:user)
-      @group = FactoryBot.create(:group, mentor: @user)
-    end
-
     it "should call respective callbacks" do
       expect_any_instance_of(GroupMember).to receive(:send_welcome_email)
       FactoryBot.create(:group_member, user: @user, group: @group)
+    end
+  end
+
+  describe "public methods" do
+    it "sends welcome email" do
+      group_member = FactoryBot.create(:group_member, user: @user, group: @group)
+      expect {
+        group_member.send_welcome_email
+      }.to have_enqueued_job.on_queue('mailers')
     end
   end
 end
