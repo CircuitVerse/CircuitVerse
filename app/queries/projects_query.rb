093f7955a96e6d352f46fe476b243e0afa6d860c
Jaideep Singh <jaideep329@gmail.com>
2019-03-07 17:54:41 +0530

    added query object

---
+++ app/queries/projects_query.rb
@@ -0,0 +1,29 @@
+class ProjectsQuery
+  attr_reader :relation
+
+  def initialize(relation = Project.all)
+    @relation = relation
+  end
+
+  def search_name_description(query)
+    if  ActiveRecord::Base.connection_config["adapter"] === "postgresql"
+      full_text_search_name_description(query)
+    else
+      simple_search_name_description(query)
+    end
+  end
+
+  def full_text_search_name_description(query)
+    relation.includes(:author).search(query).select("id,author_id,image_preview,name,description,view")
+  end
+
+  def simple_search_name_description(query)
+    relation.includes(:author).where("name LIKE ? OR description LIKE ?","%#{query}%","%#{query}%").select("id,author_id,image_preview,name,description,view")
+  end
+
+  def public_and_not_forked
+    relation.where(project_access_type: "Public", forked_project_id: nil)
+  end
+
+
+end
