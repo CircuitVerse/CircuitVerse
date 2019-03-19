a50556e62bc89da54c6dea08ac9ba9fa22bc4a02
shubhankarsharma00 <shubhankarsharma78@gmail.com>
2019-03-19 16:47:25 +0530

    added sample projetcs

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,49 @@ User.create(name: 'Admin',
 )
 users = User.create([{ name: 'user1',email: 'user1@circuitverse.org',password: 'password' },
   { name: 'user2',email: 'user2@circuitverse.org',password: 'password' }])
+
+# private,public,limited access
+
+projects = Project.create([{ name:'Private',
+author_id: users.first.id,
+project_access_type: 'Private',
+description: 'description' }, { name:'Public',
+author_id: users.first.id,
+project_access_type: 'Public',
+description: 'description' }, { name:'Limited access',
+author_id: users.first.id,
+project_access_type: 'Limited access',
+description: 'description' }])
+
+#examples
+
+Project.create([{ name:'Full Adder',
+author_id: users.first.id,
+data: File.read('db/examples/fullAdder.json'),
+project_access_type: 'Public',
+description: 'description' },{ name:'SAP',
+author_id: users.first.id,
+data: File.read('db/examples/SAP.json'),
+project_access_type: 'Public',
+description: 'SAP-1 short for simple as possible computer is a 8 Bit computer. It can perform simple operations like Addition and Subtraction.' },{ name:'ALU-74LS181',
+author_id: users.first.id,
+data: File.read('db/examples/ALU-74LS181.json'),
+project_access_type: 'Public',
+description: 'description' }])
+
+#groups
+
 group = Group.create(name: 'group1',
   mentor_id: users.first.id,
 )
 GroupMember.create(group_id: group.id,
   user_id: users.second.id,
 )
+
+#tags
+
+tag = Tag.create(name:"example")
+Tagging.create([{ tag_id: tag.id,
+project_id: projects.first.id },{ tag_id: tag.id,
+project_id: projects.second.id },{ tag_id: tag.id,
+project_id: projects.third.id }])
