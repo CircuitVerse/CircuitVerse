b17edebc386dd82eed298445bc9adf13c1656fca
amansingla97 <amansingla972gmail.com>
2019-03-19 18:33:26 +0530

    codeclimate spaces

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :project do
-    name {Faker::name}
+    name { Faker.name }
     association :author, factory: :user
-    project_access_type {"Private"}
-    description {Faker::Lorem.sentence}
-    data {Faker::Lorem.sentence}
+    project_access_type { "Private" }
+    description { Faker::Lorem.sentence }
+    data { Faker::Lorem.sentence }
   end
 end
