20d8ad921cc0f9534da27871446a0c9c0baac387
Navonil Das <navneeladas@gmail.com>
2019-04-05 15:04:16 +0530

    Fixes #336 (#340)

--- public/js/modules.js
+++ public/js/modules.js
@@ -1511,8 +1511,9 @@ function Splitter(x, y, scope = globalScope, dir = "RIGHT", bitWidth = undefined
     CircuitElement.call(this, x, y, scope, dir, bitWidth);
     this.rectangleObject = false;
 
-    this.bitWidthSplit = bitWidthSplit || prompt("Enter bitWidth Split").split(' ').map(function (x) {
-        return parseInt(x, 10) || 1;
+    this.bitWidthSplit = bitWidthSplit || prompt("Enter bitWidth Split").split(' ').filter(x => x != '').map(function(x) {
+        return parseInt(x, 10)||1;
+      
     });
     this.splitCount = this.bitWidthSplit.length;
 
