cab5dde9c3196e0969859f484bd76b6326c4ad23
Ankit Kataria <ankitkataria28@gmail.com>
2019-05-30 20:11:04 +0530

    Add grade model validations

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -4,4 +4,26 @@ class Grade < ApplicationRecord
   belongs_to :project
   belongs_to :grader, class_name: "User", foreign_key: :user_id
   belongs_to :assignment
+
+  validate :grading_scale, :assignment_project
+
+  private
+    def grading_scale
+      valid = case assignment.grading_scale
+              when "no_scale"
+                false
+              when "letter"
+                grade.match(/^(A|B|C|D|E|F)$/).present?
+              when "percent"
+                grade.match(/^[0-9][0-9]?$|^100$/).present?
+      end
+
+      errors.add(:grade, "Grade does not match scale or assignment cannot be graded") unless valid
+    end
+
+    def assignment_project
+      if project.assignment_id != assignment.id
+        errors.add(:project, "Project is not a part of the assignment")
+      end
+    end
 end
