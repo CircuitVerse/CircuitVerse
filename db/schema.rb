22cecea5b52355a904064e8ae5319ed78f0c41d8
Shubhankar Sharma <32474302+shubhankarsharma00@users.noreply.github.com>
2019-08-04 15:17:47 +0530

    Restrict duplication of tags (#422)
    - Allows only unique tags to exist with a project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190728103204) do
+
+ActiveRecord::Schema.define(version: 20190804064213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
