06b837e45517cdef7e77f83916c8215f5272791d
Fadi Farag <47584014+NumberC@users.noreply.github.com>
2019-12-04 13:06:56 -0500

    Clarity Issues Fixed

--- app/assets/javascripts/passwordViewToggle.js
+++ app/assets/javascripts/passwordViewToggle.js
@@ -1,6 +1,6 @@
 var icon = document.getElementById('hideOrShow');
 function toggle() {
-    var pass = document.getElementById('formPassword');   
+    var pass = document.getElementById('formPassword');
     if (pass.type === 'password') {
         pass.type = 'text';
         icon.src = 'Eye.png';
