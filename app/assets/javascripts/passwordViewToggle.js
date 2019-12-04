62a663252632a10495674e0ad3ec566b83149e08
Fadi Farag <47584014+NumberC@users.noreply.github.com>
2019-12-04 07:09:14 -0500

    Resolved clarity issues and Fixed Errors

--- app/assets/javascripts/passwordViewToggle.js
+++ app/assets/javascripts/passwordViewToggle.js
@@ -1,11 +1,13 @@
+var icon = document.getElementById('hideOrShow');
 function toggle() {
-  var pass = document.getElementById('formPassword');
-  var icon = document.getElementById('hideOrShow');
-  if (pass.type === 'password') {
-      pass.type = "text";
-      icon.src = "Eye.png";
-  } else{
-      pass.type = "password";
-      icon.src = "EyeClosed.png";
-  }
+    var pass = document.getElementById('formPassword');    
+    if (pass.type === 'password') {
+        pass.type = "text";
+        icon.src = "Eye.png";
+    } else {
+        pass.type = "password";
+        icon.src = "EyeClosed.png";
+    }
 }
+
+icon.addEventListener('click', toggle);
