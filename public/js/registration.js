45eef74c5069527a88bd485fc952542406a7f995
Fadi Farag <47584014+NumberC@users.noreply.github.com>
2019-12-06 21:47:07 -0500

    Create registration.js

---
+++ public/js/registration.js
@@ -0,0 +1,20 @@
+function chooseStatus(index){
+    var schoolStud = document.getElementById("schoolStudentGrp");
+    var collegeStud = document.getElementById("collegeStudentGrp");
+    var teacher = document.getElementById("teacherGrp");
+    var other = document.getElementById("");
+    var arrayOfOptions = [schoolStud, collegeStud, teacher];
+
+    schoolStud.style.display = "none";
+    collegeStud.style.display = "none";
+    teacher.style.display = "none";
+
+    for (i = 0; i < arrayOfOptions.length; i++) {
+        arrayOfOptions[i].style.display = "none";
+    }
+    if(index == -1){
+
+    } else{
+        arrayOfOptions[index].style.display = "block";
+    }
+}
