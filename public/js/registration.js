003bd3afe588c5b1a7ba52dd322554ae390b1dd5
Fadi Farag <47584014+NumberC@users.noreply.github.com>
2019-12-06 21:55:34 -0500

    Clarity Issues

--- public/js/registration.js
+++ public/js/registration.js
@@ -1,20 +1,19 @@
-function chooseStatus(index){
-    var schoolStud = document.getElementById("schoolStudentGrp");
-    var collegeStud = document.getElementById("collegeStudentGrp");
-    var teacher = document.getElementById("teacherGrp");
-    var other = document.getElementById("");
+function chooseStatus(index) {
+    var schoolStud = document.getElementById('schoolStudentGrp');
+    var collegeStud = document.getElementById('collegeStudentGrp');
+    var teacher = document.getElementById('teacherGrp');
     var arrayOfOptions = [schoolStud, collegeStud, teacher];
 
-    schoolStud.style.display = "none";
-    collegeStud.style.display = "none";
-    teacher.style.display = "none";
+    schoolStud.style.display = 'none';
+    collegeStud.style.display = 'none';
+    teacher.style.display = 'none';
 
     for (i = 0; i < arrayOfOptions.length; i++) {
-        arrayOfOptions[i].style.display = "none";
+        arrayOfOptions[i].style.display = 'none';
     }
-    if(index == -1){
+    if (index == -1){
 
     } else{
-        arrayOfOptions[index].style.display = "block";
+        arrayOfOptions[index].style.display = 'block';
     }
 }
