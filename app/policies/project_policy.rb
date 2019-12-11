6c1b801f7f17f76d38eae5398ed92e1fbad50b30
Fadi Farag <47584014+NumberC@users.noreply.github.com>
2019-12-10 19:00:44 -0500

    Fixed Issues Referencing Previous File

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -14,20 +14,6 @@ class ProjectPolicy < ApplicationPolicy
   def can_feature?
     user.present? && user.admin? && project.project_access_type == "Public"
   end
-
-  def user_access?
-    project.check_edit_access(user) || (user.present? && user.admin?)
-  end
-
-  def edit_access?
-    raise @simulator_exception unless user_access?
-    true
-  end
-
-  def view_access?
-    raise @simulator_exception unless project.check_view_access(user)
-    true
-  end
   
   def check_edit_access(user)
     @user_access =
@@ -50,6 +36,20 @@ class ProjectPolicy < ApplicationPolicy
         (project_submission == false &&  !user.nil? && author_id == user.id) || \
         (!user.nil? && Collaboration.find_by(project_id: id, user_id: user.id)) || \
         (!user.nil? && user.admin))
+  end
+  
+  def user_access?
+    check_edit_access(user) || (user.present? && user.admin?)
+  end
+
+  def edit_access?
+    raise @simulator_exception unless user_access?
+    true
+  end
+
+  def view_access?
+    raise @simulator_exception unless check_view_access(user)
+    true
   end
 
   def embed?
