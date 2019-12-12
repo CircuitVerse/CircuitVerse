e54f70bb7cbae88a7dc6c316179b87175b94bd26
Fadi Farag <47584014+NumberC@users.noreply.github.com>
2019-12-12 00:18:15 -0500

    Reference project_policy

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Project, type: :model do
     @user = FactoryBot.create(:user)
     group = FactoryBot.create(:group, mentor: @user)
     @assignment = FactoryBot.create(:assignment, group: group)
-    @project_policy = FactoryBot.create(:project_policy)
+    @project_policy = Project.new
   end
 
   describe "associations" do
@@ -51,6 +51,7 @@ RSpec.describe Project, type: :model do
 
       describe "#check_edit_access #check_view_access #check_direct_view_access" do
         it "returns true for author" do
+          authorize @project_policy
           expect(@project_policy.check_edit_access(@user)).to be_truthy
           expect(@project_policy.check_view_access(@user)).to be_truthy
           expect(@project_policy.check_direct_view_access(@user)).to be_truthy
@@ -59,7 +60,7 @@ RSpec.describe Project, type: :model do
         it "returns true for collaborator" do
           collaborator = FactoryBot.create(:user)
           FactoryBot.create(:collaboration, project: @project, user: collaborator)
-
+          authorize @project_policy
           expect(@project_policy.check_edit_access(collaborator)).to be_truthy
           expect(@project_policy.check_view_access(collaborator)).to be_truthy
           expect(@project_policy.check_direct_view_access(collaborator)).to be_truthy
@@ -67,6 +68,7 @@ RSpec.describe Project, type: :model do
 
         it "returns false otherwise" do
           user = FactoryBot.create(:user)
+          authorize @project_policy
           expect(@project_policy.check_edit_access(user)).to be_falsey
           expect(@project_policy.check_view_access(user)).to be_falsey
           expect(@project_policy.check_direct_view_access(user)).to be_falsey
@@ -94,6 +96,7 @@ RSpec.describe Project, type: :model do
 
       describe "#check_edit_access #check_direct_view_access" do
         it "returns false for edit and direct_view access" do
+          authorize @project_policy
           expect(@project_policy.check_edit_access(@user)).to be_falsey
           expect(@project_policy.check_direct_view_access(@user)).to be_falsey
         end
