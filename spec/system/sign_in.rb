6200c822c58883bea4887ac75601f24a9b6d3141
MrBartusek <bartusekcraft@gmail.com>
2019-12-19 18:13:35 +0100

    fixed hound and codeclimate issues

--- spec/system/sign_in.rb
+++ spec/system/sign_in.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe "Sign In", type: :system do
+describe 'Sign In', type: :system do
   before do
     driven_by(:selenium)
     @user = FactoryBot.create(:user)
@@ -12,31 +12,31 @@ describe "Sign In", type: :system do
     visit '/users/sign_in'
   end
 
-  it "should not sing-in when no credentials" do
-    click_button "Log in"
+  it 'should not sing-in when no credentials' do
+    click_button 'Log in'
 
-    expect(page).to have_text("Invalid Email or password.")
+    expect(page).to have_text('Invalid Email or password.')
   end
 
-  it "should sing-in when valid credentials" do
-    fill_in "Email", with: @user.email
-    fill_in "Password", with: @user.password
-    click_button "Log in"
+  it 'should sing-in when valid credentials' do
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Log in'
 
-    expect(page).to have_text("Signed in successfully.")
-  end
-  
-  it "should not sing-in when password is empty" do
-    fill_in "Email", with: @user.email
-    click_button "Log in"
-
-    expect(page).to have_text("Invalid Email or password.")
+    expect(page).to have_text('Signed in successfully.')
   end
 
-  it "should not sing-in when email is empty" do
-    fill_in "Password", with: @user.password
-    click_button "Log in"
+  it 'should not sing-in when password is empty' do
+    fill_in 'Email', with: @user.email
+    click_button 'Log in'
 
-    expect(page).to have_text("Invalid Email or password.")
+    expect(page).to have_text('Invalid Email or password.')
+  end
+
+  it 'should not sing-in when email is empty' do
+    fill_in 'Password', with: @user.password
+    click_button 'Log in'
+
+    expect(page).to have_text('Invalid Email or password.')
   end
 end
