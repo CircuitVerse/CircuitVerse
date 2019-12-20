f364384ac58c9c2471773742c96bf57ee3f9c43c
MrBartusek <bartusekcraft@gmail.com>
2019-12-20 15:49:12 +0100

    grammar

--- spec/system/signin_spec.rb
+++ spec/system/signin_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-describe "Sing In", type: :system do
+describe "Sign In", type: :system do
   before do
     driven_by(:selenium)
     @user = FactoryBot.create(:user)
@@ -12,13 +12,13 @@ describe "Sing In", type: :system do
     visit "/users/sign_in"
   end
 
-  it "should not sing-in when no credentials" do
+  it "should not sign-in when no credentials" do
     click_button "Log in"
 
     expect(page).to have_text("Invalid Email or password.")
   end
 
-  it "should sing-in when valid credentials" do
+  it "should sign-in when valid credentials" do
     fill_in "Email", with: @user.email
     fill_in "Password", with: @user.password
     click_button "Log in"
@@ -26,14 +26,14 @@ describe "Sing In", type: :system do
     expect(page).to have_text("Signed in successfully.")
   end
 
-  it "should not sing-in when password is empty" do
+  it "should not sign-in when password is empty" do
     fill_in "Email", with: @user.email
     click_button "Log in"
 
     expect(page).to have_text("Invalid Email or password.")
   end
 
-  it "should not sing-in when email is empty" do
+  it "should not sign-in when email is empty" do
     fill_in "Password", with: @user.password
     click_button "Log in"
 
