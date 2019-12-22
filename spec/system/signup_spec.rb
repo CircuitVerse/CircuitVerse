b77b856dc8157582814bd3445f3559c033a96da2
Ananthavijay <vj35.cool@gmail.com>
2019-12-22 12:53:50 +0530

    Update signup_spec.rb

--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -18,45 +18,45 @@ describe "Sign up", type: :system do
   end
 
   it "should not sign-up when password is empty" do
-    fill_in "Name", with: 'user1'
-    fill_in "Email", with: 'user1@example.com'
+    fill_in "Name", with: "user1"
+    fill_in "Email", with: "user1@example.com"
     click_button "Sign up"
 
     expect(page).to have_text("Password can't be blank")
   end
 
   it "should not sign-up when email is empty" do   
-    fill_in "Name", with: 'user1'
-    fill_in "Password", with: 'secret'
-    fill_in "Password confirmation", with: 'secret'
+    fill_in "Name", with: "user1"
+    fill_in "Password", with: "secret"
+    fill_in "Password confirmation", with: "secret"
     click_button "Sign up"
 
     expect(page).to have_text("Email can't be blank")
   end
  
   it "should not sign-up when password is less than 6 characters" do   
-    fill_in "Name", with: 'user1'
-    fill_in "Password", with: 'secr'
-    fill_in 'Password confirmation', with: 'secr'
+    fill_in "Name", with: "user1"
+    fill_in "Password", with: "secr"
+    fill_in 'Password confirmation', with: "secr"
     click_button "Sign up"
 
     expect(page).to have_text("Password is too short (minimum is 6 characters)")
   end
 
   it "should not sign-up when password confirmation is empty" do 
-    fill_in "Name", with: 'user1'
-    fill_in "Email", with: 'user1@example.com'
-    fill_in "Password", with: 'secret'
+    fill_in "Name", with: "user1"
+    fill_in "Email", with: "user1@example.com"
+    fill_in "Password", with: "secret"
     click_button "Sign up"
 
     expect(page).to have_text("Password confirmation doesn't match Password")
   end
 
   it "should sign-up when valid credentials" do
-    fill_in "Name", with: 'user1'
-    fill_in "Email", with: 'user1@example.com'
-    fill_in "Password", with: 'secret'
-    fill_in "Password confirmation", with: 'secret'
+    fill_in "Name", with: "user1"
+    fill_in "Email", with: "user1@example.com"
+    fill_in "Password", with: "secret"
+    fill_in "Password confirmation", with: "secret"
     click_button "Sign up"
 
     expect(page).to have_text("Welcome! You have signed up successfully.")
