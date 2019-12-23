d4b1fa823586dd950df7d43ecb2cfbffc30c9fa5
Lam Jun Rong <bobo999bobo@outlook.com>
2019-12-23 17:25:14 +0800

    Fix tests

--- spec/system/signin_spec.rb
+++ spec/system/signin_spec.rb
@@ -19,22 +19,22 @@ describe "Sign In", type: :system do
   end
 
   it "should sign-in when valid credentials" do
-    fill_in "Email", with: @user.email
-    fill_in "Password", with: @user.password
+    fill_in "user[email]", with: @user.email
+    fill_in "user[password]", with: @user.password
     click_button "Log in"
 
     expect(page).to have_text("Signed in successfully.")
   end
 
   it "should not sign-in when password is empty" do
-    fill_in "Email", with: @user.email
+    fill_in "user[email]", with: @user.email
     click_button "Log in"
 
     expect(page).to have_text("Invalid Email or password.")
   end
 
   it "should not sign-in when email is empty" do
-    fill_in "Password", with: @user.password
+    fill_in "user[password]", with: @user.password
     click_button "Log in"
 
     expect(page).to have_text("Invalid Email or password.")
