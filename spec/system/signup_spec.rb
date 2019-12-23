0617f123c2653b67c4697400724ad0b49cc2b144
Ananthavijay <vj35.cool@gmail.com>
2019-12-23 18:25:40 +0530

    Fixing system tests. (#768)
    So fast

--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -28,7 +28,6 @@ describe "Sign up", type: :system do
   it "should not sign-up when email is empty" do
     fill_in "Name", with: "user1"
     fill_in "Password", with: "secret"
-    fill_in "Password confirmation", with: "secret"
     click_button "Sign up"
 
     expect(page).to have_text("Email can't be blank")
@@ -37,26 +36,16 @@ describe "Sign up", type: :system do
   it "should not sign-up when password is less than 6 characters" do
     fill_in "Name", with: "user1"
     fill_in "Password", with: "secr"
-    fill_in "Password confirmation", with: "secr"
     click_button "Sign up"
 
     expect(page).to have_text("Password is too short (minimum is 6 characters)")
   end
 
-  it "should not sign-up when password confirmation is empty" do
-    fill_in "Name", with: "user1"
-    fill_in "Email", with: "user1@example.com"
-    fill_in "Password", with: "secret"
-    click_button "Sign up"
-
-    expect(page).to have_text("Password confirmation doesn't match Password")
-  end
 
   it "should sign-up when valid credentials" do
     fill_in "Name", with: "user1"
     fill_in "Email", with: "user1@example.com"
     fill_in "Password", with: "secret"
-    fill_in "Password confirmation", with: "secret"
     click_button "Sign up"
 
     expect(page).to have_text("Welcome! You have signed up successfully.")
