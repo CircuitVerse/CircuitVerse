14fc0f344bae2b40c7b9983df8d552bbe2ea352a
Josh Heng <josh@joshheng.co.uk>
2019-12-26 11:31:21 +0000

    Various code stying changes

--- public/js/search.js
+++ public/js/search.js
@@ -4,29 +4,25 @@ $(document).ready(() => {
         $('#nav-search').remove();
     }
 
-    //Highlight searched text
+    // Highlight searched text
     var searchText = $('.search-bar-input').val().trim();
 
-    if (searchText != "") {
-      $('.feature').find('p, .card-title, .card-text, .description, .text-muted').each(function() { 
-        //Loop over each result
+    if (searchText !== '') {
+      $('.feature').find('p, .card-title, .card-text, .description, .text-muted').each(function () { 
+        // Loop over each result
         var text = $(this).html();
-  
-        var currentPos = 0
-        loop = true
-  
-        while (loop && currentPos < text.length) { //Loop over all text and highlight any occurences of the search term
+
+        var currentPos = 0;
+        loop = true;
+
+        while (loop && currentPos < text.length) { // Loop over all text and highlight any occurences of the search term
           var occurence = text.toLowerCase().indexOf(searchText.toLowerCase(), currentPos);
-          
+
           if (occurence == -1) {
             loop = false;
-          } 
+          }
           else {
-            text = text.substring(0, occurence)
-            + '<span class="search-match">'
-            + text.substring(occurence, occurence + searchText.length)
-            + '</span>'
-            + text.substring(occurence + searchText.length);
+            text = `${text.substring(0, occurence)}<span class="search-match">${text.substring(occurence, occurence + searchText.length)}</span>${text.substring(occurence + searchText.length)}`;
           }
           currentPos += 34 + searchText.length;
         }
