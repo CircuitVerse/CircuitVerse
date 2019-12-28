1e96e619a023ab1c462019167462e63143122d5e
Lam Jun Rong <bobo999bobo@outlook.com>
2019-12-28 16:46:35 +0800

    Add bootstrap search dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,6 @@
 //= require jquery_ujs
 //= require commontator/application
 //= require bootstrap-typeahead-rails
+//= require popper.js/dist/umd/popper.min.js
 //= require bootstrap/dist/js/bootstrap.min.js
-//= require popper.js/dist/popper.min.js
 //= require serviceworker-companion
--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,16 @@
+/* jshint esversion: 6 */
+
 $(document).ready(() => {
     // remove search in nav bar on the search page
     if (window.location.href.includes('search')) {
         $('#nav-search').remove();
     }
+
+    $('.dropdown > div > a').on('click', () => {
+        const $a = $(this);
+        $('#dropdownSearchMenuLink').text($a.html());
+        $('.dropdown .dropdown-menu').removeClass('show');
+        $('.dropdown input[name=resource]').val($a.text());
+        return false;
+    });
 });
