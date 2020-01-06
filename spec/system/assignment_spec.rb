abbd6e84c79b2d187294cf0ac553dae4c70b0af2
MrBartusek <bartusekcraft@gmail.com>
2020-01-06 17:46:21 +0100

    'make assignment project' test

--- spec/system/assignment_spec.rb
+++ spec/system/assignment_spec.rb
@@ -6,41 +6,55 @@ describe "Assignments", type: :system do
   before do
     @mentor = FactoryBot.create(:user)
     @group = FactoryBot.create(:group, mentor: @mentor)
+    @member = FactoryBot.create(:user)
+    FactoryBot.create(:group_member, group: @group, user: @member)
   end
 
-  it 'should create assignment' do
-    sign_in @mentor
-    visit new_group_assignment_path(@group)
-    
-    name = Faker::Lorem.word
-    deadline = Faker::Date.forward(days: 7)
-    description = Faker::Lorem.sentence
+  describe 'user is mentor' do
+    it 'should create assignment' do
+      sign_in @mentor
+      visit new_group_assignment_path(@group)
+      
+      name = Faker::Lorem.word
+      deadline = Faker::Date.forward(days: 7)
+      description = Faker::Lorem.sentence
 
-    fill_assignments(name,deadline,description,grading: true)
+      fill_assignments(name,deadline,description,grading: true)
 
-    click_button "Create Assignment"
+      click_button "Create Assignment"
 
-    expect(page).to have_text("Assignment was successfully created.")
-    expect(page).to have_text(name)
-    expect(page).to have_text(deadline.strftime("%d %b,%Y"))
+      expect(page).to have_text("Assignment was successfully created.")
+      expect(page).to have_text(name)
+      expect(page).to have_text(deadline.strftime("%d %b,%Y"))
+    end
+
+    it 'should edit assignment' do
+      sign_in @mentor
+      @assignment = FactoryBot.create(:assignment, group: @group)
+      visit edit_group_assignment_path(@group,@assignment)
+      
+      name = Faker::Lorem.word
+      deadline = Faker::Date.forward(days: 7)
+      description = Faker::Lorem.sentence
+
+      fill_assignments(name,deadline,description,grading: false)
+
+      click_button "Update Assignment"
+
+      expect(page).to have_text("Assignment was successfully updated.")
+      expect(page).to have_text(name)
+      expect(page).to have_text(deadline.strftime("%d %b,%Y"))
+    end
   end
-
-  it 'should edit assignment' do
-    sign_in @mentor
-    @assignment = FactoryBot.create(:assignment, group: @group)
-    visit edit_group_assignment_path(@group,@assignment)
-    
-    name = Faker::Lorem.word
-    deadline = Faker::Date.forward(days: 7)
-    description = Faker::Lorem.sentence
-
-    fill_assignments(name,deadline,description,grading: false)
-
-    click_button "Update Assignment"
-
-    expect(page).to have_text("Assignment was successfully updated.")
-    expect(page).to have_text(name)
-    expect(page).to have_text(deadline.strftime("%d %b,%Y"))
+  
+  describe 'user is member' do
+    it 'should be able to make assignment project' do
+      @assignment = FactoryBot.create(:assignment, group: @group)
+      sign_in @member
+      visit group_path(@group)
+      click_on 'Start Working'
+      expect(page).to have_content(@member.name + "/" + @assignment.name) 
+    end
   end
 
   def fill_assignments(name,deadline,description,grading:)
