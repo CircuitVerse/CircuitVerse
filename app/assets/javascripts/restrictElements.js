3c6d7d0ba7b818d11e4d6d07d149d620a5553ae4
MrBartusek <bartusekcraft@gmail.com>
2020-01-07 15:48:27 +0100

    Removed undercores

--- app/assets/javascripts/restrictElements.js
+++ app/assets/javascripts/restrictElements.js
@@ -1,4 +1,4 @@
-function _handleMainCheckbox() {
+function handleMainCheckbox() {
     $('#restrict-elements').change((e) => {
         e.preventDefault();
         const radio = $(e.currentTarget);
@@ -12,7 +12,7 @@ function _handleMainCheckbox() {
     $('#restrict-elements').trigger('change');
 }
 
-function _restrictionsMap(restrictions) {
+function restrictionsMap(restrictions) {
     const map = {};
     for (let i = 0; i < restrictions.length; i++) {
         map[restrictions[i]] = true;
@@ -20,11 +20,11 @@ function _restrictionsMap(restrictions) {
     return map;
 }
 
-function _htmlRowName(name) {
+function htmlRowName(name) {
     return `<div class="circuit-element-category"> ${name} </div>`;
 }
 
-function _htmlInlineCheckbox(elementName, checked) {
+function htmlInlineCheckbox(elementName, checked) {
     return `
     <div class="form-check form-check-inline">
         <input class="form-check-input" type="checkbox" class='element-restriction' id="checkbox-${elementName}" value="${elementName} ${checked}">
@@ -32,29 +32,29 @@ function _htmlInlineCheckbox(elementName, checked) {
     </div>`;
 }
 
-function _generateRow(name, elements, restrictionMap) {
-    let html = _htmlRowName(name);
+function generateRow(name, elements, restrictionMap) {
+    let html = htmlRowName(name);
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i];
         const checked = restrictionMap[element] ? 'checked' : '';
-        html += _htmlInlineCheckbox(element, checked);
+        html += htmlInlineCheckbox(element, checked);
     }
     return html;
 }
 
-function _loadHtml(elementHierarchy, restrictionMap) {
+function loadHtml(elementHierarchy, restrictionMap) {
     for (let i = 0; i < Object.entries(elementHierarchy).length; i++) {
         const category = Object.entries(elementHierarchy)[i];
-        const html = _generateRow(category[0], category[1], restrictionMap);
+        const html = generateRow(category[0], category[1], restrictionMap);
         $('.restricted-elements-list').append(html);
     }
 }
 
 function loadRestrictions(restrictions) {
-    _handleMainCheckbox();
+    handleMainCheckbox();
 
     const { elementHierarchy } = metadata;
-    const restrictionMap = _restrictionsMap(restrictions);
+    const restrictionMap = restrictionsMap(restrictions);
 
-    _loadHtml(elementHierarchy, restrictionMap);
+    loadHtml(elementHierarchy, restrictionMap);
 }
