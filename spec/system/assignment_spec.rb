e8e31b7e9a53b3f83adcd7536ac4b44101cfb98a
MrBartusek <bartusekcraft@gmail.com>
2020-01-09 16:35:56 +0100

    restricted elements tests and cleanup

--- spec/system/assignment_spec.rb
+++ spec/system/assignment_spec.rb
@@ -16,16 +16,16 @@ describe "Assignments", type: :system do
       visit new_group_assignment_path(@group)
 
       name = Faker::Lorem.word
-      deadline = Faker::Date.forward(days: 7)
+      deadline = Faker::Date.forward(days: 23)
       description = Faker::Lorem.sentence
 
       fill_assignments(name, deadline, description, grading: true)
 
       click_button "Create Assignment"
-
       expect(page).to have_text("Assignment was successfully created.")
-      expect(page).to have_text(name)
-      expect(page).to have_text(deadline.strftime("%d %b,%Y"))
+
+      click_link "Show"
+      check_show_page(name, deadline, description, "Input, Button, Power")
     end
 
     it "should be able to edit assignment" do
@@ -34,16 +34,17 @@ describe "Assignments", type: :system do
       visit edit_group_assignment_path(@group, @assignment)
 
       name = Faker::Lorem.word
-      deadline = Faker::Date.forward(days: 7)
+      deadline = Faker::Date.forward(days: 23)
       description = Faker::Lorem.sentence
 
       fill_assignments(name, deadline, description, grading: false)
+      uncheck "checkbox-Power"
 
       click_button "Update Assignment"
-
       expect(page).to have_text("Assignment was successfully updated.")
-      expect(page).to have_text(name)
-      expect(page).to have_text(deadline.strftime("%d %b,%Y"))
+
+      click_link "Show"
+      check_show_page(name, deadline, description, "Input, Button")
     end
   end
 
@@ -59,21 +60,29 @@ describe "Assignments", type: :system do
 
   def fill_assignments(name, deadline, description, grading:)
     fill_in "Name", with: name
-    fill_in "Deadline", with: deadline.strftime("%d/%m/%Y %H:%M")
+    fill_in "Deadline", with: deadline.strftime("%d/%m/%Y 12:00")
+    sleep 0.1
+    find("#assignment_deadline").send_keys :enter
     fill_in_editor "#editor", with: description
 
     if :grading == true then
       select "percent", from: "assignment_grading_scale"
     end
 
-    # TODO: not impelemented cause #778 is not merged
-    # check "restrict-elements"
-    # check "checkbox-Input"
-    # check "checkbox-Button"
-    # check "checkbox-Power"
+    check "restrict-elements"
+    check "checkbox-Input"
+    check "checkbox-Button"
+    check "checkbox-Power"
   end
 
   def fill_in_editor(editor, with:)
     find(editor).send_keys with
+  end
+
+  def check_show_page(name, deadline, description, restricted_elements)
+    expect(page).to have_text(name)
+    expect(page).to have_text(description)
+    expect(page).to have_text(deadline.strftime("%d %b %Y"))
+    expect(page).to have_text(restricted_elements)
   end
 end
