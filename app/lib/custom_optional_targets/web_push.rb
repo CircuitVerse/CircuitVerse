cbb0b1376b820ae404916853289e6ac73eca41c0
Fadi Farag <47584014+NumberC@users.noreply.github.com>
2020-01-12 21:07:48 -0500

    Revert "Add loader for Zeitwerk in web_push"
    This reverts commit 38f066357b4cebcb9306062b9f755e9404089616.

--- app/lib/custom_optional_targets/web_push.rb
+++ app/lib/custom_optional_targets/web_push.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 require "zeitwerk"
-loader = Zeitwerk::Loader.for_gem
-loader.setup
+
 
 module CustomOptionalTarget
   class WebPush < ActivityNotification::OptionalTarget::Base
