794e5452711332ade55529b878bb4c7da9daed85
junron <h1710074@nushigh.edu.sg>
2020-01-15 19:38:16 +0800

    Add group management system tests (#814)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Warden::Test::Helpers
 end
 
 Shoulda::Matchers.configure do |config|
