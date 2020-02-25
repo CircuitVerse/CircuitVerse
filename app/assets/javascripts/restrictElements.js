f902a982283493bd3cefcd250347d25bfe9ee277
harshgujrati <harsh19may@gmail.com>
2020-02-25 23:52:39 +0530

    New UI Homepage

--- app/assets/javascripts/restrictElements.js
+++ app/assets/javascripts/restrictElements.js
@@ -58,3 +58,34 @@ function loadRestrictions(restrictions) {
 
     loadHtml(elementHierarchy, restrictionMap);
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("row_homepage");
+  //var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      slides[slideIndex-1].style.display = "inline-block";
+  }
+}
+  //for (i = 0; i < dots.length; i++) {
+      //dots[i].className = dots[i].className.replace(" active", "");
+  //}
+  //dots[slideIndex-1].className += " active";
+
+
