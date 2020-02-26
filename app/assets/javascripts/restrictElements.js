e90a834ea61d5990cebb6a90acf042fa1379cb84
harshgujrati <harsh19may@gmail.com>
2020-02-27 00:09:23 +0530

    Resolving Merge Conflict

--- app/assets/javascripts/restrictElements.js
+++ app/assets/javascripts/restrictElements.js
@@ -58,3 +58,29 @@ function loadRestrictions(restrictions) {
 
     loadHtml(elementHierarchy, restrictionMap);
 }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("row_homepage");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "inline-block";
+  
+}
