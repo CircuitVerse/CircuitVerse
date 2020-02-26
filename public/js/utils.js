1f4dd92f7dec0a8b58773d4fb66ca0211a09dcc1
Sj1218305 <jainshobit0@gmail.com>
2020-02-26 23:09:47 +0530

    Added Step-By-Step Guide

--- public/js/utils.js
+++ public/js/utils.js
@@ -2,25 +2,3 @@
 function stripTags(string="") {
     return string.replace(/(<([^>]+)>)/ig, '').trim();
 }
-$(".vpop").on('click', function(e) {
-    e.preventDefault();
-    $("#video-popup-overlay,#video-popup-iframe-container,#video-popup-container,#video-popup-close").show();
-    
-    var srchref='',autoplay='',id=$(this).data('id');
-    if($(this).data('type') == 'vimeo') var srchref="//player.vimeo.com/video/";
-    else if($(this).data('type') == 'youtube') var srchref="https://www.youtube.com/embed/";
-    
-    if($(this).data('autoplay') == true) autoplay = '?autoplay=1';
-    
-    $("#video-popup-iframe").attr('src', srchref+id+autoplay);
-    
-    $("#video-popup-iframe").on('load', function() {
-      $("#video-popup-container").show();
-    });
-  });
-  
-  $("#video-popup-close, #video-popup-overlay").on('click', function(e) {
-    $("#video-popup-iframe-container,#video-popup-container,#video-popup-close,#video-popup-overlay").hide();
-    $("#video-popup-iframe").attr('src', '');
-  });
-  
