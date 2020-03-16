06911d9ba1415d993ba34d5a8d82b83e2e93264f
harshgujrati <harsh19may@gmail.com>
2020-03-16 18:46:45 +0530

    Change linkno to id

--- app/controllers/logix_controller.rb
+++ app/controllers/logix_controller.rb
@@ -22,36 +22,18 @@ class LogixController < ApplicationController
   end
 
   def examples
-    @example = [{
-                  name: "Full Adder from 2-Half Adders",
-                  linkno: "247",
-                  img: "examples/fullAdder_n.png"
-                },
-                { 
-                  name: "16 Bit ripple carry adder",
-                  linkno: "248",
-                  img: "examples/RippleCarry_n.jpeg"
-                },
-                {
-                  name: "Asynchronous Counter",
-                  linkno: "249",
-                  img: "examples/AsyncCounter_n.jpeg"
-                },
-                {
-                  name: "Keyboard",
-                  linkno: "250",
-                  img: "examples/Keyboard_n.jpeg"
-                },
-                {
-                  name: "FlipFlop",
-                  linkno: "251",
-                  img: "examples/FlipFlop_n.jpeg"
-                },
-                { 
-                  name: "ALU 74LS181 by Ananth Shreekumar",
-                  linkno: "252",
-                  img: "examples/ALU_n.png"
-                }]
+    @example = [{ name: "Full Adder from 2-Half Adders", id: "users/3/projects/247",
+                  img: "examples/fullAdder_n.png" },
+                { name: "16 Bit ripple carry adder", id: "users/3/projects/248",
+                  img: "examples/RippleCarry_n.jpeg" },
+                { name: "Asynchronous Counter", id: "users/3/projects/249",
+                  img: "examples/AsyncCounter_n.jpeg" },
+                { name: "Keyboard", id: "users/3/projects/250",
+                  img: "examples/Keyboard_n.jpeg" },
+                { name: "FlipFlop", id: "users/3/projects/251",
+                  img: "examples/FlipFlop_n.jpeg" },
+                { name: "ALU 74LS181 by Ananth Shreekumar", id: "users/3/projects/252",
+                  img: "examples/ALU_n.png" }]
   end
 
   def features
