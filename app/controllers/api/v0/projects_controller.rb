eb10c8fbaaa31e9906adbc6ac06d766bfba2abb9
Harinandan <harinandan.alukkal@gmail.com>
2020-03-21 22:55:35 +0530

    CodeClimate Issues V2

--- app/controllers/api/v0/projects_controller.rb
+++ app/controllers/api/v0/projects_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class Api::V0::ProjectsController < ApplicationController
   def index
     @projects = Project.all
@@ -8,12 +9,11 @@ class Api::V0::ProjectsController < ApplicationController
 
   def show
     @projects = Project.find(params[:id])
-    if (@projects.project_access_type == "Public")
+    if @projects.project_access_type == "Public"
       json_string = ProjectsSerializer.new(@projects).serialized_json
       render json: json_string
     else
       render plain: "The record you wish access could not be found"
     end
   end
-
 end
--- app/serializers/projects_serializer.rb
+++ app/serializers/projects_serializer.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class ProjectsSerializer
   include FastJsonapi::ObjectSerializer
   set_type :projects
