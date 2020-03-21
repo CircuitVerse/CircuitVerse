f20e4ac4a9d8b1f930c2f8bd19106852e9fa1fac
Harinandan <harinandan.alukkal@gmail.com>
2020-03-21 22:48:15 +0530

    Fixes for CodeClimate Issues

--- app/serializers/projects_serializer.rb
+++ app/serializers/projects_serializer.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
 class ProjectsSerializer
   include FastJsonapi::ObjectSerializer
   set_type :projects
-  attribute :name, if: Proc.new {|params|
-    params[:project_access_type]=='Public'
+  attribute :name, if: Proc.new { |params|
+    params[:project_access_type] == "Public"
   }
 end
