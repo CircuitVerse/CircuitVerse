40b625ae819b5815e71f7c696b8b0e130a1871e5
miftahunajat <miftahunajat@gmail.com>
2020-03-22 15:44:12 +0700

    Add api/v0/projects endpoint

---
+++ app/controllers/api/v0/projects_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Api::V0::ProjectsController < ApplicationController
+  before_action :set_project, only: [:show]
+
+  def index
+    @projects = Project.open
+    response = ProjectSerializer.new(@projects).serialized_json
+    render json: response
+  end
+  # GET /projects/1
+  # GET /projects/1.json
+  def show
+    if @project.project_access_type == "Public"
+      @project.increase_views(current_user)
+      response = ProjectSerializer.new(@project).serialized_json
+      render json: response
+    else
+      render json: { errors: { title: :unauthorized,
+        status: 401,
+        detail: "This project is not public" } }, status: :unauthorized
+    end
+  end
+
+  private
+    def set_project
+      @project = Project.find(params[:id])
+      @author = @project.author
+    end
+end
---
+++ app/serializers/project_serializer.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class ProjectSerializer
+  include FastJsonapi::ObjectSerializer
+  set_type :projects
+  attributes :image_preview, :description, :view, :tags, :name
+  attributes :author do |project|
+    "#{project.author.name}"
+  end
+  attributes :stars_count do |proj|
+    proj.stars.count
+  end
+
+  attributes :link_to_project do |project|
+    "/users/#{project.author.id}/projects/#{project.id}"
+  end
+end
