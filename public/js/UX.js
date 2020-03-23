2be46330b9c2bc4eb75a7acfc3afbcdf8cb5ef80
jha11aditya <44377261+jha11aditya@users.noreply.github.com>
2020-03-23 12:00:35 +0000

    Added help (#847)
    * Update modules.js
    
    added helplink
    
    * Update UX.js
    
    added on click to CircuitVerse tip button and changed its name to CircuitVerse Help
    
    * Update modules.js
    
    fixed multiple spaces
    
    * Update UX.js
    
    fixed spaces for codeclimate
    
    * Update UX.js
    
    one more space for codeclimate
    
    * Update UX.js
    
    added i icon
    
    * for issue 817 added help link to docs
    
    * This commit is for issue #817 where i added help-to-docs feature on tip button
    
    Update modules.js
    
    added helplink
    
    Update UX.js
    
    added on click to CircuitVerse tip button and changed its name to CircuitVerse Help
    
    Update modules.js
    
    fixed multiple spaces
    
    Update UX.js
    
    fixed spaces for codeclimate
    
    Update UX.js
    
    one more space for codeclimate
    
    Update UX.js
    
    added i icon
    
    for issue 817 added help link to docs
    
    * Update UX.js
    
    Co-authored-by: adi <170010011@iitdh.ac.in>

--- public/js/UX.js
+++ public/js/UX.js
@@ -206,7 +206,7 @@ function showProperties(obj) {
     // Tooltip can be defined in the prototype or the object itself, in addition to help map.
     var tooltipText = obj && (obj.tooltipText);
     if (tooltipText) {
-        $('#moduleProperty-inner').append('<p><button id="toolTipButton" class="btn btn-primary btn-xs" type="button" >CircuitVerse Tip</button></p>');
+        $('#moduleProperty-inner').append('<p><button id="toolTipButton" class="btn btn-primary btn-xs" type="button" >CircuitVerse Help &#9432</button></p>');
         $('#toolTipButton').hover(function () {
             $("#Help").addClass("show");
             $("#Help").empty();
@@ -215,6 +215,11 @@ function showProperties(obj) {
         }); // code goes in document ready fn only
         $('#toolTipButton').mouseleave(function () {
             $("#Help").removeClass("show");
+        });
+        //redirects to obj's specific help page
+        $('#toolTipButton').click(function () {
+            var helplink = obj && (obj.helplink);
+            window.open(helplink);
         });
     }
 
