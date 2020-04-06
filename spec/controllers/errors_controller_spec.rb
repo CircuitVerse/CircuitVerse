81a896551948daf334a51381663522ad4da5b286
harshgujrati <harsh19may@gmail.com>
2020-04-06 12:25:36 +0530

    Modify spec

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -7,7 +7,7 @@ describe ErrorsController, type: :request do
     it "should return 404" do
       get "/404"
       expect(response.body).to include("OOPS,THE PAGE YOU ARE LOOKING FOR CAN'T BE FOUND!")
-      expect(response.body).to include("Error code: 404")
+      expect(response.body).to include("404")
     end
   end
 
@@ -15,7 +15,7 @@ describe ErrorsController, type: :request do
     it "should return 404" do
       get "/500"
       expect(response.body).to include("SERVER ERRORED!, IT'S ME NOT YOU, LET'S TRY AGAIN :)")
-      expect(response.body).to include("Error code: 500")
+      expect(response.body).to include("500")
     end
   end
 
@@ -23,7 +23,7 @@ describe ErrorsController, type: :request do
     it "should return 404" do
       get "/422"
       expect(response.body).to include("UNPROCESSABLE ENTITY!, REQUEST COULD NOT BE PROCESSED :(")
-      expect(response.body).to include("Error code: 422")
+      expect(response.body).to include("422")
     end
   end
 end
