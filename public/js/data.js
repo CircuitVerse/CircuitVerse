66e89dcd3440a816f2a07ec64880f6953dacaaf1
Shivansh Srivastava <42182955+Shivansh2407@users.noreply.github.com>
2020-04-07 10:44:58 +0530

    Update data.js

--- public/js/data.js
+++ public/js/data.js
@@ -442,7 +442,7 @@ function save() {
         // user not signed in, save locally temporarily and force user to sign in
         localStorage.setItem("recover_login", data);
         // Asking user whether they want to login.
-        confirm("You have to login to save the project, you will be redirected to the login page.") window.location.href = "/users/sign_in";
+        if(confirm("You have to login to save the project, you will be redirected to the login page.")) window.location.href = "/users/sign_in";
     } else if (logix_project_id == 0) {
 
         // Create new project - this part needs to be improved and optimised
