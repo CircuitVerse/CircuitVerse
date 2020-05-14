998518d96536630a54553dd886c4ace74e89b9a6
Nitish Aggarwal <royalnitish21@gmail.com>
2020-05-14 22:04:16 +0530

    ptch: Update user serializer to not serialize id in attrs

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -3,11 +3,11 @@
 class Api::V1::UserSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :name, :admin,
-             :country, :educational_institute,
-             :subscribed, :created_at, :updated_at
-
   attribute :email, if: Proc.new { |record, params|
     params[:has_email_access] == true || record.admin
   }
+
+  attributes :name, :admin,
+             :country, :educational_institute,
+             :subscribed, :created_at, :updated_at
 end
