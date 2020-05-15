c5a388e0cee960ee33a57ec2bfdcd4c297d57554
Nitish Aggarwal <royalnitish21@gmail.com>
2020-05-15 13:03:06 +0530

    test: Add specs for /me endpoint

--- spec/controllers/api/v1/users_spec.rb
+++ spec/controllers/api/v1/users_spec.rb
@@ -29,6 +29,32 @@ RSpec.describe Api::V1::UsersController, type: :request do
     end
   end
 
+  describe "GET logged in user" do
+    let!(:user) { FactoryBot.create(:user) }
+
+    context "when not authenticated" do
+      before(:each) do
+        get "/api/v1/me"
+      end
+      it "returns 401 :unauthorized and should have jsonapi errors" do
+        expect(response).to have_http_status(401)
+        expect(response.body).to have_jsonapi_errors()
+      end
+    end
+
+    context "when authenticated" do
+      before(:each) do
+        token = get_auth_token(user)
+        get "/api/v1/me", headers: { "Authorization": "Token #{token}" }
+      end
+
+      it "returns the logged in user" do
+        expect(response).to have_http_status(200)
+        expect(response).to match_response_schema("user")
+      end
+    end
+  end
+
   describe "list a user" do
     let!(:user) { FactoryBot.create(:user) }
 
