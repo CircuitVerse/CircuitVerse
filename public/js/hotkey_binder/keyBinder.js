e0c0e3babf7ccf0f1e0d21bac5b1eec478c1982f
Abstrekt <saammmmmmy@gmail.com>
2020-06-07 20:31:06 +0530

    .

--- public/js/hotkey_binder/keyBinder.js
+++ public/js/hotkey_binder/keyBinder.js
@@ -7,83 +7,102 @@ $("#customShortcutDialog").append(heading);
 $("#customShortcutDialog").append(markUp);
 
 $("#customShortcut").click(() => {
-    closeEdit(); // if edit is showing, close it
-    $("#customShortcutDialog").dialog({
-        buttons: [
-            {
-                text: "Reset to default",
-                click: () => setDefault(),
-                id: "resetDefault",
-            },
-            {
-                text: "Save",
-                click: () => {
-                    submit();
-                    $("#customShortcutDialog").dialog("close");
-                },
-                id: "submitBtn",
-            },
-        ],
-    });
+	closeEdit(); // if edit is showing, close it
+	$("#customShortcutDialog").dialog({
+		resizable: false,
+		create: function () {
+			var pane = $(this).dialog("widget").find(".ui-dialog-buttonpane");
+			$(
+				"<label class='disable--cb' ><input id='' onchange='toggle_hotkey()' type='checkbox'/> Enable </label>"
+			).prependTo(pane);
+			if (
+				localStorage.get("custom_hotkey") &&
+				localStorage.get("custom_hotkey").enable === true
+			)
+				$(".disable--cb > input[type='checkbox']").prop(
+					"checked",
+					true
+				);
+		},
+		buttons: [
+			{
+				text: "Reset to default",
+				click: () => setDefault(),
+				id: "resetDefault",
+			},
+			{
+				text: "Save",
+				click: () => {
+					submit();
+					$("#customShortcutDialog").dialog("close");
+				},
+				id: "submitBtn",
+			},
+		],
+	});
 });
 
 let targetPref = null;
 $("#preference").click((e) => {
-    $("#pressedKeys").text("");
-    $("#pressedKeys").text("");
+	$("#pressedKeys").text("");
+	$("#pressedKeys").text("");
 
-    $("#edit").css("display", "block");
-    $($("#edit")).focus();
-    [, targetPref] = e.target.closest("div").children;
+	$("#edit").css("display", "block");
+	$($("#edit")).focus();
+	[, targetPref] = e.target.closest("div").children;
 });
 
 // Modifiers restriction enabled here
 $("#edit").keydown((e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    let modifiers = ["CTRL", "ALT", "SHIFT"];
-    if (e.keyCode === 27) closeEdit();
-    if (e.keyCode === 13) {
-        override($("#pressedKeys").text());
-        targetPref.innerText = $("#pressedKeys").text();
-        $("#pressedKeys").text("");
-        $("#edit").css("display", "none");
-    }
-    $("#warning").text("");
-    const currentKey = keyCodes[e.keyCode].toUpperCase();
-    if (
-        $("#pressedKeys").text().split(" + ").length === 2 &&
-        !modifiers.includes(currentKey) &&
-        modifiers.includes($("#pressedKeys").text().split(" + ")[1])
-    ) {
-        $("#pressedKeys").append(` + ${currentKey}`);
-    } else if (modifiers.includes($("#pressedKeys").text())) {
-        modifiers = modifiers.filter((mod) => mod === $("#pressedKeys").text());
-        if (!modifiers.includes(currentKey)) {
-            $("#pressedKeys").append(` + ${currentKey}`);
-        }
-    } else {
-        $("#pressedKeys").text("");
-        $("#pressedKeys").text(currentKey);
-    }
-    if (!$("#pressedKeys").text()) {
-        $("#pressedKeys").text(currentKey);
-    }
-    if (
-        ($("#pressedKeys").text().split(" + ").length === 2 &&
-            $("#pressedKeys").text().split(" + ")[1] === "CTRL") ||
-        $("#pressedKeys").text().split(" + ")[0] === "ALT"
-    ) {
-        $("#pressedKeys").text(
-            $("#pressedKeys").text().split(" + ").reverse()
-            .join(" + ")
-        );
-    }
-    warnOverride($("#pressedKeys").text());
+	e.stopPropagation();
+	e.preventDefault();
+	let modifiers = ["CTRL", "ALT", "SHIFT"];
+	if (e.keyCode === 27) closeEdit();
+	if (e.keyCode === 13) {
+		override($("#pressedKeys").text());
+		targetPref.innerText = $("#pressedKeys").text();
+		$("#pressedKeys").text("");
+		$("#edit").css("display", "none");
+	}
+	$("#warning").text("");
+	const currentKey = keyCodes[e.keyCode].toUpperCase();
+	if (
+		$("#pressedKeys").text().split(" + ").length === 2 &&
+		!modifiers.includes(currentKey) &&
+		modifiers.includes($("#pressedKeys").text().split(" + ")[1])
+	) {
+		$("#pressedKeys").append(` + ${currentKey}`);
+	} else if (modifiers.includes($("#pressedKeys").text())) {
+		modifiers = modifiers.filter((mod) => mod === $("#pressedKeys").text());
+		if (!modifiers.includes(currentKey)) {
+			$("#pressedKeys").append(` + ${currentKey}`);
+		}
+	} else {
+		$("#pressedKeys").text("");
+		$("#pressedKeys").text(currentKey);
+	}
+	if (!$("#pressedKeys").text()) {
+		$("#pressedKeys").text(currentKey);
+	}
+	if (
+		($("#pressedKeys").text().split(" + ").length === 2 &&
+			$("#pressedKeys").text().split(" + ")[1] === "CTRL") ||
+		$("#pressedKeys").text().split(" + ")[0] === "ALT"
+	) {
+		$("#pressedKeys").text(
+			$("#pressedKeys").text().split(" + ").reverse().join(" + ")
+		);
+	}
+	warnOverride($("#pressedKeys").text());
 });
 
 //  IFFE
 (() => {
-    setDefault();
-    if (localStorage.userKeys) addKeys("user");
+	if (
+		localStorage.get("custom_hotkey") &&
+		localStorage.get("custom_hotkey").enable === true
+	) {
+		setDefault();
+		if (localStorage.userKeys) addKeys("user");
+	}
 })();
--- public/js/hotkey_binder/model/actions.js
+++ public/js/hotkey_binder/model/actions.js
@@ -1,58 +1,68 @@
 const addKeys = (mode) => {
-    shortcut.removeAll();
-    // shortcut.all_shortcuts = {};
-    if (mode == "user") {
-        let userKeys = localStorage.get("userKeys");
-        for (let pref in userKeys) {
-            let key = userKeys[pref];
-            key = key.split(" ").join("");
-            addShortcut(key, pref);
-        }
-        updateHTML("user");
-    } else if (mode == "default") {
-        let defaultKeys = localStorage.get("defaultKeys");
-        for (let pref in defaultKeys) {
-            let key = defaultKeys[pref];
-            key = key.split(" ").join("");
-            addShortcut(key, pref);
-        }
-        updateHTML("default");
-    }
+	shortcut.removeAll();
+	// shortcut.all_shortcuts = {};
+	if (mode == "user") {
+		let userKeys = localStorage.get("userKeys");
+		for (let pref in userKeys) {
+			let key = userKeys[pref];
+			key = key.split(" ").join("");
+			addShortcut(key, pref);
+		}
+		updateHTML("user");
+	} else if (mode == "default") {
+		let defaultKeys = localStorage.get("defaultKeys");
+		for (let pref in defaultKeys) {
+			let key = defaultKeys[pref];
+			key = key.split(" ").join("");
+			addShortcut(key, pref);
+		}
+		updateHTML("default");
+	}
 };
 
 const setUserKeys = () => {
-    let userKeys = {};
-    let x = 0;
-    while ($("#preference").children()[x]) {
-        userKeys[$("#preference").children()[x].children[0].innerText] = $(
-            "#preference"
-        ).children()[x].children[1].innerText;
-        x++;
-    }
-    localStorage.removeItem("defaultKeys");
-    localStorage.set("userKeys", userKeys);
-    addKeys("user");
+	let userKeys = {};
+	let x = 0;
+	while ($("#preference").children()[x]) {
+		userKeys[$("#preference").children()[x].children[0].innerText] = $(
+			"#preference"
+		).children()[x].children[1].innerText;
+		x++;
+	}
+	localStorage.removeItem("defaultKeys");
+	localStorage.set("userKeys", userKeys);
+	addKeys("user");
 };
 
 const setDefault = () => {
-    localStorage.set("defaultKeys", defaultKeys); //TODO add a confirmation alert
-    addKeys("default");
+	localStorage.set("defaultKeys", defaultKeys); //TODO add a confirmation alert
+	addKeys("default");
 };
 
 const warnOverride = (combo) => {
-    let x = 0;
-    // const currentKeys = localStorage.get("userKeys")
-    // ? localStorage.get("userKeys")
-    // : localStorage.get("defaultKeys");
-    while ($("#preference").children()[x]) {
-        if ($("#preference").children()[x].children[1].innerText === combo) {
-            const asignee = $("#preference").children()[x].children[0]
-                .innerText;
-            $("#warning").text(
-                `This key(s) is already assigned to: ${asignee}, Override?`
-            );
-        }
-        x++;
-    }
+	let x = 0;
+	// const currentKeys = localStorage.get("userKeys")
+	// ? localStorage.get("userKeys")
+	// : localStorage.get("defaultKeys");
+	while ($("#preference").children()[x]) {
+		if ($("#preference").children()[x].children[1].innerText === combo) {
+			const asignee = $("#preference").children()[x].children[0]
+				.innerText;
+			$("#warning").text(
+				`This key(s) is already assigned to: ${asignee}, Override?`
+			);
+		}
+		x++;
+	}
 };
 
+const toggle_hotkey = () => {
+	if ($(".disable--cb > input[type='checkbox']").prop("checked") == false) {
+		localStorage.set("custom_hotkey", { enable: false });
+		shortcut.removeAll();
+	} else {
+		localStorage.set("custom_hotkey", { enable: true });
+		setDefault();
+		if (localStorage.userKeys) addKeys("user");
+	}
+};
