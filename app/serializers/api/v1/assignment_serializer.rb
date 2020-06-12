dc4279c441efb15b4c0b5a23e942e319f8fba570
Nitish Aggarwal <royalnitish21@gmail.com>
2020-06-12 13:57:26 +0530

    feat: Add your_project_id to assignments serializer

--- app/serializers/api/v1/assignment_serializer.rb
+++ app/serializers/api/v1/assignment_serializer.rb
@@ -9,5 +9,11 @@ class Api::V1::AssignmentSerializer
     (assignment.group.mentor_id == params[:current_user].id) || params[:current_user].admin?
   end
 
+  attributes :your_project_id do |assignment, params|
+    # checks for project related to the assignment, returns null if no project found or project_id
+    project = assignment.projects.find { |p| p.author_id == params[:current_user].id }
+    project.nil? ? nil : project.id
+  end
+
   attributes :created_at, :updated_at, :grading_scale, :grades_finalized
 end
