27dd376e2fb332cd754ac23504fd4150de19d480
Abstrekt <saammmmmmy@gmail.com>
2020-07-16 20:54:46 +0530

    -a lite theme,  ++fixes

--- public/src/themer/themer.js
+++ public/src/themer/themer.js
@@ -4,8 +4,21 @@ import getColors from '../modules/colors';
 const themeOptions = [
     {
         "--primary": "#454545",
-        "--text-primary": "#fff",
-        "--text-secondary": "#000",
+        "--text-lite": "#fff",
+        "--text-dark": "#000",
+        "--text-panel": "white",
+
+        "--text-navbar": "white",
+        "--bg-navbar":"#454545",
+        "--qp-br-tl": "#333333",
+        "--qp-br-rd": "#535353",
+        "--qp-box-shadow-1": "#3b3b3b",
+        "--qp-box-shadow-2": "#4f4f4f",
+
+
+        "--bg-circuit": "#bbbbbb",
+        "--br-circuit": "#454545",
+
         "--br-primary": "#fff",
         "--bg-primary-moz": "#454545e6",
         "--bg-primary-chr": "#454545b3",
@@ -15,17 +28,13 @@ const themeOptions = [
         "--bg-secondary": "#bbbbbb",
         "--canvas-stroke": "#eee",
         "--canvas-fill": "white",
-        "--bg-toogle": "#42b983",
-        "--bg-toogle-darken": "#3ca877",
+        "--bg-toggle-btn-primary": "#42b983",
+        "--primary-btn-hov": "#3ca877",
         "--btn-danger": "#dc5656",
         "--btn-danger-darken": "#b03662",
         "--disable": "#6c8b93",
-        "--qp-br-tl": "#333333",
-        "--qp-br-rd": "#535353",
-        "--qp-box-shadow-1": "#3b3b3b",
-        "--qp-box-shadow-2": "#4f4f4f",
-        "--btn-dg-hov": "#ddd",
-        "--btn-dg-hov-text": "#000",
+        "--cus-btn-hov--bg": "#ddd",
+        "--cus-btn-hov-text": "#000",
         "--node": "green",
         "--stroke": "black",
         "--fill": "white",
@@ -48,9 +57,14 @@ const themeOptions = [
     },
     {
         "--primary": "#0F111A", //header bg, panels bg
-        "--text-primary": "#FFF", //normal state text
-        "--text-secondary": "white", //text state on hover, on drop down menu , context menu
+        "--text-lite": "#FFF", //normal state text
+        "--text-dark": "white", //text state on hover, on drop down menu , context menu
+        "--text-panel": "white",
+        
+        "--bg-navbar":"#0F111A",
         "--br-primary": "#665627", //panel border, tabbar circuit border
+        "--br-circuit-cur": "#cccccc",
+        "--bg-circuit": "#727d8d",
         "--bg-primary-moz": "#0f111ae6", //dialog bg
         "--bg-primary-chr": "#0f111ab3", //dialog bg
         "--bg-tabs": "#727d8d", //tabs bar primary bg,
@@ -59,16 +73,16 @@ const themeOptions = [
         "--bg-secondary": "#536c84", //border color input button,
         "--canvas-fill": "#1B2C33", //canvas bg
         "--canvas-stroke": "#6A7980", //canvas stroke
-        "--bg-toogle": "#48a69d",
-        "--bg-toogle-darken": "#3f9189",
+        "--bg-toggle-btn-primary": "#48a69d",
+        "--primary-btn-hov": "#3f9189",
         "--btn-danger": "#c33c6c",
         "--btn-danger-darken": "#b03662",
         "--qp-br-tl": "#282d46", //more ligthen than qp box shadow 1
         "--qp-br-rd": "#1d2132",
         "--qp-box-shadow-1": "#1d2132", //lil lighten base
         "--qp-box-shadow-2": "#0a0b11", //lil darken base
-        "--btn-dg-hov": "#48a69d",
-        "--btn-dg-hov-text": "#fff",
+        "--cus-btn-hov--bg": "#48a69d",
+        "--cus-btn-hov-text": "#fff",
 
         "--node": "#285963",
         "--stroke": "#277F7C",
@@ -88,7 +102,69 @@ const themeOptions = [
         "--wire-norm": "#277F7C",
         "--node-norm": "#FFC231",
         "--splitter": "#0284A8",
-        name: "nightSky",
+        name: "Night Sky",
+    },
+    {
+        "--primary": "#EAEAEB", //header bg
+        "--text-dark": "#6B6B6B", //normal state text
+        "--text-lite": "white", //text state on hover, on drop down menu , context menu
+        "--text-panel": "#6B6B6B",
+        
+        "--bg-navbar":"#6b6b6b",
+        "--text-navbar": "white",
+        "--qp-br-tl": "#969696", //more ligthen than qp box shadow 1
+        "--qp-br-rd": "#545454",
+        "--qp-box-shadow-1": "#747474", //lil lighten base .. top left shadow
+        "--qp-box-shadow-2": "#5f5f5f", //lil darken base //down right shadow
+        
+        // "--bg-tabs": "#EAEAEB", //tabs bar primary bg,
+        "--bg-tabs": "#D7D7D7", //tabs bar primary bg,
+        "--br-circuit-cur": "#42B983",
+        "--bg-circuit": "#A4A4A4",
+        "--br-circuit": "#42B983", 
+
+        "--br-primary": "#42B983", //panel border, tabbar circuit border
+
+        "--context-text-hov": "#6B6B6B",
+        "--context-text": "white",
+
+        "--bg-primary-moz": "rgba(107, 107, 107, 0.904)", //dialog bg, navbar dropwdown //.9 opacity of nav
+        "--bg-primary-chr": "rgba(107, 107, 107, 0.704)", //dialog bg navbar dropwdown // .7 opacity of nav
+
+        "--bg-icons": "#DDDDDD", //ce icon bg
+
+        "--bg-text": "#ddd", //drop down, content menu, text bg on hover
+
+        "--bg-secondary": "#6B6B6B", //border color input button,
+
+        "--bg-toggle-btn-primary": "#42B983",
+        "--primary-btn-hov": "#66C89C",
+        "--btn-danger": "#BF2424",
+        "--btn-danger-darken": "#BF414C",
+        "--cus-btn-hov--bg": "#42B983",
+        "--cus-btn-hov-text": "#fff",
+        
+        "--canvas-fill": "white", //canvas bg
+        "--canvas-stroke": "#BABABA", //canvas stroke
+        "--node": "#42B983",
+        "--stroke": "#6B6B6B",
+        "--fill": "#EAEAEB",
+        "--hover-and-sel": "#FFE99B", //yellow
+        "--wire-draw": "#6B6B6B", //black
+        "--wire-cnt": "#42B983", //
+        "--wire-pow": "#52E539",
+        "--wire-sel": "#0FB2F2",
+        "--wire-lose": "#F10530",
+        "--mini-map": "#42B983",
+        "--mini-map-stroke": "#0FB2F2",
+        "--input-text": "#42B983",
+        "--output-rect": "#0487D9",
+        "--secondary-stroke": "#F10530",
+        "--text": "#454545",
+        "--wire-norm": "#006839",
+        "--node-norm": "#FFC231",
+        "--splitter": "#00B462",
+        name: "Lite-born Spring",
     },
 ];
 
@@ -101,21 +177,21 @@ const themeOptions = [
 //         "--border-all": "#fff", //border color all
 //         navbar: {
 //             "--bg-primary-nav": "#557493", //nav-bar bg color
-//             "--text-primary-nav": "#557493", //project title font color
+//             "--text-lite-nav": "#557493", //project title font color
 //             "--text-seconday-nav": "#557493", //menu font color
 //             "--text-alternate-nav": "#ffffff", // text-color on hover
 //             dropdown: {
-//                 "--text-primary-drop": "#557493", //dropdown font color
+//                 "--text-lite-drop": "#557493", //dropdown font color
 //                 "--text-alternate-drop": "#557493", //dropdown font color on hover
 //             },
 //         },
 //         sideMenu: {
 //             "--bg-primary-side": "#557493", //side menu bg color
-//             "--text-primary-side": "#557493", //heading text font color of side menu
+//             "--text-lite-side": "#557493", //heading text font color of side menu
 //             circuitElements: {
 //                 "--bg-primary-circuit": "#557493", //circuit elements options bg color
 //                 "--bg-secondary-circuit": "#557493", //circuit elements option dropdown bg color
-//                 "--text-primary-circuit": "#557493", //circuit elements option font color
+//                 "--text-lite-circuit": "#557493", //circuit elements option font color
 //             },
 //             properties: {
 //                 //config
@@ -134,21 +210,21 @@ const themeOptions = [
 //         "--border-all": "#fff", //border color all
 //         navbar: {
 //             "--bg-primary-nav": "#557493", //nav-bar bg color
-//             "--text-primary-nav": "#557493", //project title font color
+//             "--text-lite-nav": "#557493", //project title font color
 //             "--text-seconday-nav": "#557493", //menu font color
 //             "--text-alternate-nav": "#ffffff", // text-color on hover
 //             dropdown: {
-//                 "--text-primary-drop": "#557493", //dropdown font color
+//                 "--text-lite-drop": "#557493", //dropdown font color
 //                 "--text-alternate-drop": "#557493", //dropdown font color on hover
 //             },
 //         },
 //         sideMenu: {
 //             "--bg-primary-side": "#557493", //side menu bg color
-//             "--text-primary-side": "#557493", //heading text font color of side menu
+//             "--text-lite-side": "#557493", //heading text font color of side menu
 //             circuitElements: {
 //                 "--bg-primary-circuit": "#557493", //circuit elements options bg color
 //                 "--bg-secondary-circuit": "#557493", //circuit elements option dropdown bg color
-//                 "--text-primary-circuit": "#557493", //circuit elements option font color
+//                 "--text-lite-circuit": "#557493", //circuit elements option font color
 //             },
 //             properties: {
 //                 //config
@@ -236,7 +312,7 @@ export const colorThemes = () => {
             localStorage.setItem("theme", $('.selected').text());
             $('.set').removeClass('set');
             $('.selected').addClass('set');
-            // $('.ui-dialog').find('button:first').trigger('click');
+            $('.ui-dialog').find('button:first').trigger('click');
         }
         if (e.which === 27) {
             updateThemeForStyle(localStorage.getItem('theme'));
@@ -247,8 +323,8 @@ export const colorThemes = () => {
 }
 
 const hackCanvas = () => {
-    globalScope.scale -= 0.0000000000001; //hack
-    dots();
+    // globalScope.scale -= 0.0000000000001; //hack
+    dots(true, false, true);
 }
 
 (() => {
