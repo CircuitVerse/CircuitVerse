38800c496763593bdb783d0ae7d83ad4260bb80b
Nitish Aggarwal <royalnitish21@gmail.com>
2020-07-29 17:47:18 +0530

    test: update oauth endpoint specs

--- spec/requests/api/v1/authentication_controller/oauth_signup_spec.rb
+++ spec/requests/api/v1/authentication_controller/oauth_signup_spec.rb
@@ -4,12 +4,11 @@ require "rails_helper"
 
 RSpec.describe Api::V1::AuthenticationController, "#oauth_signup", type: :request do
   describe "oauth user signup" do
-    let!(:user) { FactoryBot.build(:user) }
-
     context "when user already exists" do
       before do
-        existing_user = FactoryBot.create(:user)
-        post "/api/v1/oauth/signup", params: oauth_signup_params(existing_user), as: :json
+        # creates a user with specified email
+        FactoryBot.create(:user, email: "test@test.com")
+        post "/api/v1/oauth/signup", params: oauth_params, as: :json
       end
 
       it "return status 409 and should have jsonapi errors" do
@@ -18,9 +17,37 @@ RSpec.describe Api::V1::AuthenticationController, "#oauth_signup", type: :reques
       end
     end
 
+    context "with invalid access token" do
+      before do
+        post "/api/v1/oauth/signup", params: {
+          "access_token": "invalid_access_token",
+          "provider": "google"
+        }, as: :json
+      end
+
+      it "return status 401 and should have jsonapi errors" do
+        expect(response).to have_http_status(401)
+        expect(response.parsed_body).to have_jsonapi_errors
+      end
+    end
+
+    context "with unsupported provider type" do
+      before do
+        post "/api/v1/oauth/signup", params: {
+          "access_token": "ya29.a0AfH6SMB5cyjrwei-oi_TJ8Z4hTfw9v1tz-Ubm30AeWdzCpX9UHFY",
+          "provider": "unsupported_provider"
+        }, as: :json
+      end
+
+      it "return status 404 and should have jsonapi errors" do
+        expect(response).to have_http_status(404)
+        expect(response.parsed_body).to have_jsonapi_errors
+      end
+    end
+
     context "with valid params" do
       before do
-        post "/api/v1/oauth/signup", params: oauth_signup_params(user), as: :json
+        post "/api/v1/oauth/signup", params: oauth_params, as: :json
       end
 
       it "return status 201 and respond with token" do
@@ -29,12 +56,9 @@ RSpec.describe Api::V1::AuthenticationController, "#oauth_signup", type: :reques
       end
     end
 
-    def oauth_signup_params(user)
+    def oauth_params
       {
-        "uid": user.uid,
-        "name": user.name,
-        "email": user.email,
-        "password": user.password,
+        "access_token": "ya29.a0AfH6SMB5cyjrwei-oi_TJ8Z4hTfw9v1tz-Ubm30AeWdzCpX9UHFY",
         "provider": "google"
       }
     end
