f4f7c1188c0a6a7e207406ad7749a4906321c427
Nitish Aggarwal <royalnitish21@gmail.com>
2020-08-10 10:29:04 +0530

    feat: Add circuit featured notif

--- app/models/featured_circuit.rb
+++ app/models/featured_circuit.rb
@@ -3,18 +3,28 @@
 class FeaturedCircuit < ApplicationRecord
   belongs_to :project
 
-  after_create :featured_circuit_email
+  after_create :featured_circuit_email, :send_featured_circuit_notif
   validate :project_public
 
   private
 
     def project_public
-      if project.project_access_type != "Public"
-        errors.add(:project, "Featured projects have to be public")
-      end
+      return unless project.project_access_type != "Public"
+
+      errors.add(:project, "Featured projects have to be public")
     end
 
     def featured_circuit_email
       UserMailer.featured_circuit_email(project.author, project).deliver_later
+    end
+
+    def send_featured_circuit_notif
+      return if project.author.fcm.nil?
+
+      FcmNotification.send(
+        project.author.fcm.token,
+        "Project Featured",
+        "Your project #{project.name} is now featured"
+      )
     end
 end
