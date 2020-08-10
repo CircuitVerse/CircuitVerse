f67171639b0248b05db27374fe9c25104ac3aeb8
Nitish Aggarwal <royalnitish21@gmail.com>
2020-08-10 10:28:29 +0530

    feat: Adds new collaboration notif

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -3,4 +3,16 @@
 class Collaboration < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  after_create :send_new_collaboration_notif
+
+  def send_new_collaboration_notif
+    return if user.fcm.nil?
+
+    FcmNotification.send(
+      user.fcm.token,
+      "Added as Collaborator",
+      "#{project.author.name} added you as a collaborator in #{project.name}"
+    )
+  end
 end
