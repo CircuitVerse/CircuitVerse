92a457db4d678ad863893960a61c702c60ba3227
aayush-05 <aayushgupta92780@gmail.com>
2020-08-18 10:52:48 +0530

    Fix builds

--- spec/system/assignment_spec.rb
+++ spec/system/assignment_spec.rb
@@ -85,7 +85,7 @@ describe "Assignments", type: :system do
 
     check "restrict-elements"
     # check "Input"
-    page.find('#checkbox-Button', visible: :all).click
+    page.find('#checkbox-Button', visible: false).check
     # click_on "Button"
     # check "checkbox-Input"
     check "checkbox-Button"
