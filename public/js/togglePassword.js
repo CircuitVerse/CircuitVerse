24940117e8546f738787f18bf8ae1c82086d8f09
aayush-05 <aayushgupta92780@gmail.com>
2020-08-21 14:36:48 +0530

    Fix codeclimate issues

--- public/js/togglePassword.js
+++ public/js/togglePassword.js
@@ -1,25 +1,11 @@
-function toggleLoginPassword() {
-    if ($('.users-login-password-input').attr('type') === 'text') {
-        $('.users-login-password-input').attr('type', 'password');
-        $('#login-toggle-icon').addClass('fa-eye-slash');
-        $('#login-toggle-icon').removeClass('fa-eye');
-    }
-    else {
-        $('.users-login-password-input').attr('type', 'text');
-        $('#login-toggle-icon').addClass('fa-eye');
-        $('#login-toggle-icon').removeClass('fa-eye-slash');
-    }
-}
-
-function toggleSignupPassword() {
-    if ($('.users-signup-password-input').attr('type') === 'text') {
-        $('.users-signup-password-input').attr('type', 'password');
-        $('#signup-toggle-icon').addClass('fa-eye-slash');
-        $('#signup-toggle-icon').removeClass('fa-eye');
-    }
-    else {
-        $('.users-signup-password-input').attr('type', 'text');
-        $('#signup-toggle-icon').addClass('fa-eye');
-        $('#signup-toggle-icon').removeClass('fa-eye-slash');
+function togglePassword() {
+    if ($('.users-password-input').attr('type') === 'text') {
+        $('.users-password-input').attr('type', 'password');
+        $('#toggle-icon').addClass('fa-eye-slash');
+        $('#toggle-icon').removeClass('fa-eye');
+    } else {
+        $('.users-password-input').attr('type', 'text');
+        $('#toggle-icon').addClass('fa-eye');
+        $('#toggle-icon').removeClass('fa-eye-slash');
     }
 }
