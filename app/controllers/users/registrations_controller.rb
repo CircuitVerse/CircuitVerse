4e42a168d59a9117eeed113bb8ff3db9047d4743
aayush-05 <aayushgupta92780@gmail.com>
2020-08-21 17:05:37 +0530

    Add GDPR Popup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,8 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+  respond_to :html, :js
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -38,11 +40,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name age])
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:privacy_confirmation])
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
