c0ad41bee7c57306b58d0b347bc099f5cdd076f2
Satvik Ramaprasad <satviksr@gmail.com>
2020-09-01 15:27:59 +0530

    Removing redundant files

--- public/src/themer/themer.js
+++ public/src/themer/themer.js
@@ -1,9 +1,33 @@
 import { dots } from '../canvasApi';
 import themeOptions from "./themes";
 import themeCardSvg from "./themeCardSvg";
-import getColors from '../modules/colors';
 
-export let colors = getColors();
+const getCanvasColors = () => {
+  let colors = {};
+  colors["hover_select"] = getComputedStyle(document.documentElement).getPropertyValue('--hover-and-sel');
+  colors["fill"] = getComputedStyle(document.documentElement).getPropertyValue('--fill');
+  colors["mini_fill"] = getComputedStyle(document.documentElement).getPropertyValue('--mini-map');
+  colors["mini_stroke"] = getComputedStyle(document.documentElement).getPropertyValue('--mini-map-stroke');
+  colors["stroke"] = getComputedStyle(document.documentElement).getPropertyValue('--stroke');
+  colors["stroke_alt"] = getComputedStyle(document.documentElement).getPropertyValue('--secondary-stroke');
+  colors["input_text"] = getComputedStyle(document.documentElement).getPropertyValue('--input-text');
+  colors["color_wire_draw"] = getComputedStyle(document.documentElement).getPropertyValue('--wire-draw');
+  colors["color_wire_con"] = getComputedStyle(document.documentElement).getPropertyValue('--wire-cnt');
+  colors["color_wire_pow"] = getComputedStyle(document.documentElement).getPropertyValue('--wire-pow');
+  colors["color_wire_sel"] = getComputedStyle(document.documentElement).getPropertyValue('--wire-sel');
+  colors["color_wire_lose"] = getComputedStyle(document.documentElement).getPropertyValue('--wire-lose');
+  colors["color_wire"] = getComputedStyle(document.documentElement).getPropertyValue('--wire-norm');
+  colors["text"] = getComputedStyle(document.documentElement).getPropertyValue('--text');
+  colors["node"] = getComputedStyle(document.documentElement).getPropertyValue('--node');
+  colors["node_norm"] = getComputedStyle(document.documentElement).getPropertyValue('--node-norm');
+  colors["splitter"] = getComputedStyle(document.documentElement).getPropertyValue('--splitter');
+  colors["out_rect"] = getComputedStyle(document.documentElement).getPropertyValue('--output-rect');
+  colors["canvas_stroke"] = getComputedStyle(document.documentElement).getPropertyValue('--canvas-stroke');
+  colors["canvas_fill"] = getComputedStyle(document.documentElement).getPropertyValue('--canvas-fill');
+  return colors;
+}
+
+export let colors = getCanvasColors();
 
 function updateThemeForStyle(themeName) {
     const selectedTheme = themeOptions[themeName];
@@ -11,7 +35,7 @@ function updateThemeForStyle(themeName) {
     Object.keys(selectedTheme).forEach((property, i) => {
         html.style.setProperty(property, selectedTheme[property]);
     });
-    colors = getColors();
+    colors = getCanvasColors();
 }
 
 export default updateThemeForStyle;
