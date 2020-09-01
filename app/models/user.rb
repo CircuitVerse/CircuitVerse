5404bbfcfd5bc6fe0926309709b09ce0d1483386
Bijoy Sijo <bijoysijo21@gmail.com>
2020-09-01 16:55:00 +0530

    add name validation to avoid blank name submissions and special characters in name

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,12 @@ class User < ApplicationRecord
   after_commit :create_members_from_invitations, on: :create
 
   has_attached_file :profile_picture, styles: { medium: "205X240#", thumb: "100x100>" }, default_url: ":style/Default.jpg"
+
+  #validations for user
+
   validates_attachment_content_type :profile_picture, content_type: %r{\Aimage/.*\z}
+  validates :name, presence: true
+  validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
 
   scope :subscribed, -> { where(subscribed: true) }
 
