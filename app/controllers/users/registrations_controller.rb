2bece9e80c5844d89f71471da4d9e5a4d83a94e9
Aboobacker MK <aboobackervyd@gmail.com>
2020-09-18 18:50:20 +0530

    Invisible captcha approach

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+  invisible_captcha only: [:create, :update], honeypot: :subtitle
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
