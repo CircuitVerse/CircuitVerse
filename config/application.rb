953a1a4dc3e3f6e92708c14106c73088320c8d1a
pavanjoshi914 <pavanj914@gmail.com>
2020-09-18 16:33:49 +0530

    adding default font to website#1657

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Logix
     config.site_image = "https://circuitverse.org/img/circuitverse2.svg"
     config.site_description = "Explore Digital circuits online with CircuitVerse. With our easy to use simulator interface, you will be building circuits in no time."
     config.slack_url = "https://join.slack.com/t/circuitverse-team/shared_invite/enQtNjc4MzcyNDE5OTA3LTdjYTM5NjFiZWZlZGI2MmU1MmYzYzczNmZlZDg5MjYxYmQ4ODRjMjQxM2UyMWI5ODUzODQzMDU2ZDEzNjI4NmE"
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
