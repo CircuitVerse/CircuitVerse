a8c0b956cc8a1e10e3072117b43b16a24fac1e36
Satvik Ramaprasad <satviksr@gmail.com>
2020-10-03 01:57:20 +0530

    Fix clipboard copy type

--- public/src/embedListeners.js
+++ public/src/embedListeners.js
@@ -199,49 +199,7 @@ export default function startListeners() {
         gridUpdateSet(true);
         update(); // Schedule update not working, this is INEFFICENT
     }
-
-    document.addEventListener('cut', (e) => {
-        simulationArea.copyList = simulationArea.multipleObjectSelections.slice();
-        if (simulationArea.lastSelected && simulationArea.lastSelected !== simulationArea.root && !simulationArea.copyList.contains(simulationArea.lastSelected)) {
-            simulationArea.copyList.push(simulationArea.lastSelected);
-        }
-
-        var textToPutOnClipboard = cut(simulationArea.copyList);
-        if (isIe) {
-            window.clipboardData.setData('Text', textToPutOnClipboard);
-        } else {
-            e.clipboardData.setData('text/plain', textToPutOnClipboard);
-        }
-        e.preventDefault();
-    });
-    document.addEventListener('copy', (e) => {
-        simulationArea.copyList = simulationArea.multipleObjectSelections.slice();
-        if (simulationArea.lastSelected && simulationArea.lastSelected !== simulationArea.root && !simulationArea.copyList.contains(simulationArea.lastSelected)) {
-            simulationArea.copyList.push(simulationArea.lastSelected);
-        }
-
-        var textToPutOnClipboard = copy(simulationArea.copyList);
-        if (isIe) {
-            window.clipboardData.setData('Text', textToPutOnClipboard);
-        } else {
-            e.clipboardData.setData('text/plain', textToPutOnClipboard);
-        }
-        e.preventDefault();
-    });
-
-    document.addEventListener('paste', (e) => {
-        var data;
-        if (isIe) {
-            data = window.clipboardData.getData('Text');
-        } else {
-            data = e.clipboardData.getData('text/plain');
-        }
-
-        paste(data);
-        e.preventDefault();
-    });
 }
-
 
 var isIe = (navigator.userAgent.toLowerCase().indexOf('msie') != -1
     || navigator.userAgent.toLowerCase().indexOf('trident') != -1);
