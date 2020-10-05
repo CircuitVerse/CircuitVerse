19087ed5b7cfdddd00d4c5b1787a2dc22ef10391
Aboobacker MK <aboobackervyd@gmail.com>
2020-10-06 04:17:50 +0530

    Fix missing action

--- app/controllers/simulator_controller.rb
+++ app/controllers/simulator_controller.rb
@@ -5,8 +5,8 @@ class SimulatorController < ApplicationController
   include ActionView::Helpers::SanitizeHelper
 
   before_action :authenticate_user!, only: %i[create update edit update_image]
-  before_action :set_project, only: %i[show embed]
-  before_action :set_user_project, only: %i[update edit get_data update_image]
+  before_action :set_project, only: %i[show embed get_data]
+  before_action :set_user_project, only: %i[update edit update_image]
   before_action :check_view_access, only: %i[show embed get_data]
   before_action :check_edit_access, only: %i[edit update update_image]
   skip_before_action :verify_authenticity_token, only: %i[get_data create]
