f6fd9dd2d0ac4d52e09939cc13caee226fc90d12
drjyeh <52799302+drjyeh@users.noreply.github.com>
2020-10-08 19:32:17 -0700

    Added download Verilog function

--- public/js/verilog.js
+++ public/js/verilog.js
@@ -1,3 +1,23 @@
+
+
+function convertVerilog(){
+    function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
+    
+    var data = verilog.exportVerilog();
+    download(projectName + ".v", data);
+}
+
 verilog={
     exportVerilog:function(){
         var dependencyList = {};
