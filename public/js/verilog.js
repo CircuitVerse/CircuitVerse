d18ebf0ba98d9b35a74ed8a09e21c311c6574233
Jmssix <jmssix@yahoo.com>
2020-10-23 22:40:00 -0700

    add comment for sanitization

--- public/js/verilog.js
+++ public/js/verilog.js
@@ -257,13 +257,19 @@ verilog = {
     santizeLabel: function(name){
 //        return name.replace(/ Inverse/g, "_inv").replace(/ /g , "_");
 
+
         var temp = name;
+        //if there is a space anywhere but the last place
+        //replace spaces by "_"
+        //last space is required for escaped id
         if (temp.search(/ /g) < temp.length-1 && temp.search(/ /g) >= 0) {
             temp = temp.replace(/ Inverse/g, "_inv");
             temp = temp.replace(/ /g , "_");
         }
 
+        //if first character is not \ already
         if (temp.substring(0,1).search(/\\/g) < 0) {
+            //if there are non-alphanum_ character, or first character is num, add \
             if (temp.search(/[\W]/g) > -1 || temp.substring(0,1).search(/[0-9]/g) > -1)
                 temp = "\\" + temp + " ";
         }
