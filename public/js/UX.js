7fd5be617ea078aa3989152823920b436d4e09d0
Dipansh Parmar <dipanshparmar870@gmail.com>
2020-11-04 16:30:16 +0530

    linked header title input with sidebar title input to sync changes

--- public/js/UX.js
+++ public/js/UX.js
@@ -149,6 +149,11 @@ function showProperties(obj) {
         $('#moduleProperty-inner').append("<p>Lite Mode : <label class='switch'> <input type='checkbox' " + ["", "checked"][lightMode + 0] + " class='objectPropertyAttributeChecked' name='changeLightMode' > <span class='slider'></span> </label></p>");
         // $('#moduleProperty-inner').append("<p>  ");
         $('#moduleProperty-inner').append("<p><button type='button' class='objectPropertyAttributeChecked btn btn-danger btn-xs' name='deleteCurrentCircuit' >Delete Circuit</button>  <button type='button' class='objectPropertyAttributeChecked btn btn-primary btn-xs' name='toggleLayoutMode' >Edit Layout</button> </p>");
+
+        // setting the name of the title bar as same as it is in the side bar
+        $('.projectName').val($('[name="setProjectName"]').val()); // setting the initial value of title heading
+        $('[name="setProjectName"]').on('input', (e) => $('.projectName').val(e.target.value));// setting the value of header title on sidebar title change
+        $('.projectName').on('input', (e) => $('[name="setProjectName"]').val(e.target.value));// setting the value of sidebar title on header title change
     } else {
         $('#moduleProperty').show();
 
