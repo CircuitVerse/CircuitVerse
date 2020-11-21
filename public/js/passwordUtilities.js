c54864720423cd0c55b1dae7416313115ee1cf54
Akash Chetia <achetia037@gmail.com>
2020-11-21 21:25:55 +0530

    Added func. to trigger capsAlert & modified related pages

--- public/js/passwordUtilities.js
+++ public/js/passwordUtilities.js
@@ -1,3 +1,4 @@
+//Function for password toggler
 function togglePassword() {
     if ($('.users-password-input').attr('type') === 'text') {
         $('.users-password-input').attr('type', 'password');
@@ -8,4 +9,12 @@ function togglePassword() {
         $('#toggle-icon').addClass('fa-eye');
         $('#toggle-icon').removeClass('fa-eye-slash');
     }
+}
+
+//Function for capsLock Alert
+function capsLock(e) {
+    var kc = e.keyCode ? e.keyCode : e.which;
+    var sk = e.shiftKey ? e.shiftKey : kc === 16;
+    var visibility = ((kc >= 65 && kc <=90) && !sk) || ((kc >= 97 && kc <= 122) && sk) ? 'visible' : 'hidden';
+    document.getElementById('caps-alert').style.visibility = visibility;
 }
