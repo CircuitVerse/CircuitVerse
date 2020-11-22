cef68bd57e47166980dcb5ec3b9c3c9908eba64f
pavanjoshi914 <pavanj914@gmail.com>
2020-11-22 19:33:16 +0530

    specs fix

--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -11,15 +11,6 @@ describe "Sign up", type: :system do
     visit "/users/sign_up"
   end
 
-  it "do not sign-up when name is invalid" do
-    fill_in "Name", with: "user@123"
-    fill_in "Email", with: "user1@example.com"
-    fill_in "Password", with: "secret"
-    click_button "Sign up"
-
-    expect(page).to have_text("Name :only alphabets and spaces are allowed")
-  end
-
   it "should not sign-up when password is less than 6 characters" do
     fill_in "Name", with: "user"
     fill_in "Email", with: "user1@example.com"
