5792faec9caee23c9b3186cded0e59e900023a50
akshaydayma <66819508+akshaydayma@users.noreply.github.com>
2020-12-14 16:48:28 +0530

    Update circuit_element_spec.rb

--- spec/models/circuit_element_spec.rb
+++ spec/models/circuit_element_spec.rb
@@ -22,8 +22,8 @@ RSpec.describe CircuitElement, type: :model do
 
   let(:test_bench) { ["TB_Input", "TB_Output", "ForceGate"] }
 
-  let(:misc) { ["Flag", "Splitter", "Adder", "TriState", "Buffer", "ControlledInverter", "ControlledBuffer", "ALU",
-   "Rectangle", "Arrow", "Text", "Tunnel"] }
+  let(:misc) { ["Flag", "Splitter", "Adder", "TriState", "Buffer", "ControlledInverter", 
+    "ControlledBuffer", "ALU","Rectangle", "Arrow", "Text", "Tunnel"] }
 
   describe "methods" do
     it "should return category wise element lists" do
