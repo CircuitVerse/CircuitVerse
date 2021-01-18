301e9129beb6a341cf8718bfe94a0424120c1611
Shivansh Srivastava <42182955+Shivansh2407@users.noreply.github.com>
2021-01-18 17:15:44 +0530

    Fixes redirect to simulator after logging in from simulator  (#1398)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
 
+  before_action :store_user_location!, if: :storable_location?
+
   rescue_from Pundit::NotAuthorizedError, with: :auth_error
   rescue_from ApplicationPolicy::CustomAuthException, with: :custom_auth_error
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
@@ -19,4 +21,18 @@ class ApplicationController < ActionController::Base
   def not_found
     render "errors/not_found.html.erb", status: :not_found
   end
+
+  private
+
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+    end
+
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
+    end
+
+    def after_sign_in_path_for(resource_or_scope)
+      stored_location_for(resource_or_scope) || super
+    end
 end
