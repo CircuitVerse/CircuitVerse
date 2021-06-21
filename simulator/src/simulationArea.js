2a9fc2a51d0ce7ea47ffd60bb36fb6384ffe83aa
ZadeAbhishek <zabhidoc@gmail.com>
2021-06-21 15:49:53 +0530

    TouchCompatibility: resolving clasrity issues  -ZadeAbhishek
    **resolving clarity issue in simulationArea.js**

--- simulator/src/simulationArea.js
+++ simulator/src/simulationArea.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-undef */
-/* eslint-disable import/no-cycle */
 import EventQueue from './eventQueue';
 import { clockTick } from './utils';
 
@@ -41,69 +40,69 @@ import { clockTick } from './utils';
  * @category simulationArea
  */
 const simulationArea = {
-  canvas: document.getElementById('simulationArea'),
-  selected: false,
-  hover: false,
-  clockState: 0,
-  clockEnabled: true,
-  lastSelected: undefined,
-  stack: [],
-  prevScale: 0,
-  oldx: 0,
-  oldy: 0,
-  objectList: [],
-  maxHeight: 0,
-  maxWidth: 0,
-  minHeight: 0,
-  minWidth: 0,
-  multipleObjectSelections: [],
-  copyList: [],
-  shiftDown: false,
-  controlDown: false,
-  timePeriod: 500,
-  mouseX: 0,
-  mouseY: 0,
-  mouseDownX: 0,
-  mouseDownY: 0,
-  simulationQueue: undefined,
-  multiAddElement: false,
-  touch: false,
+    canvas: document.getElementById('simulationArea'),
+    selected: false,
+    hover: false,
+    clockState: 0,
+    clockEnabled: true,
+    lastSelected: undefined,
+    stack: [],
+    prevScale: 0,
+    oldx: 0,
+    oldy: 0,
+    objectList: [],
+    maxHeight: 0,
+    maxWidth: 0,
+    minHeight: 0,
+    minWidth: 0,
+    multipleObjectSelections: [],
+    copyList: [],
+    shiftDown: false,
+    controlDown: false,
+    timePeriod: 500,
+    mouseX: 0,
+    mouseY: 0,
+    mouseDownX: 0,
+    mouseDownY: 0,
+    simulationQueue: undefined,
+    multiAddElement: false,
+    touch: false,
 
-  clickCount: 0, // Double click
-  lock: 'unlocked',
-  timer() {
-    ckickTimer = setTimeout(() => {
-      simulationArea.clickCount = 0;
-    }, 600);
-  },
+    clickCount: 0, // Double click
+    lock: 'unlocked',
+    timer() {
+        ckickTimer = setTimeout(() => {
+            simulationArea.clickCount = 0;
+        }, 600);
+    },
 
-  setup() {
-    this.canvas = document.getElementById('simulationArea');
-    this.canvas.width = width;
-    this.canvas.height = height;
-    this.simulationQueue = new EventQueue(10000);
-    this.context = this.canvas.getContext('2d');
-    simulationArea.changeClockTime(simulationArea.timePeriod);
-    this.mouseDown = false;
-  },
-  changeClockTime(t) {
-    if (t < 50) {
-      return;
-    }
+    setup() {
+        this.canvas = document.getElementById('simulationArea');
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.simulationQueue = new EventQueue(10000);
+        this.context = this.canvas.getContext('2d');
+        simulationArea.changeClockTime(simulationArea.timePeriod);
+        this.mouseDown = false;
+    },
+    changeClockTime(t) {
+        if (t < 50) {
+            return;
+        }
 
-    clearInterval(simulationArea.ClockInterval);
-    // eslint-disable-next-line no-param-reassign
-    t = t || prompt('Enter Time Period:');
-    simulationArea.timePeriod = t;
-    simulationArea.ClockInterval = setInterval(clockTick, t);
-  },
-  clear() {
-    if (!this.context) {
-      return;
-    }
+        clearInterval(simulationArea.ClockInterval);
+        // eslint-disable-next-line no-param-reassign
+        t = t || prompt('Enter Time Period:');
+        simulationArea.timePeriod = t;
+        simulationArea.ClockInterval = setInterval(clockTick, t);
+    },
+    clear() {
+        if (!this.context) {
+            return;
+        }
 
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
 };
 export const { changeClockTime } = simulationArea;
 export default simulationArea;
