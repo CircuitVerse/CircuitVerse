46a7cf5177b0b885abe40a645299c6e26544511b
Ruturaj Mohite <mohite.ruturaj15@gmail.com>
2021-08-23 00:51:52 +0530

    Include password reset and API endpoints to rate limit

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,46 +1,81 @@
+# frozen_string_literal: true
+
 class Rack::Attack
   class Request < ::Rack::Request
     # Take remote IP from Cloudfare's headers instead of rev proxy IP
     def remote_ip
       # Cloudflare stores remote IP in CF_CONNECTING_IP header
-      @remote_ip ||= (env['HTTP_CF_CONNECTING_IP'] ||
-                      env['action_dispatch.remote_ip'] ||
+      @remote_ip ||= (env["HTTP_CF_CONNECTING_IP"] ||
+                      env["action_dispatch.remote_ip"] ||
                       ip).to_s
     end
+
+    # Hack to get JSON request params
+    # Reads from the IO stream and resets the stream
+    def json_params
+      unless @json_params
+        @json_params = JSON.parse env["rack.input"].read
+        env["rack.input"].rewind
+      end
+      @json_params
+    end
   end
+
   # Disable if DISABLE_RACK_ATTACK and if env is not production
-  if !Rails.env.production?
-    Rack::Attack.enabled = !ENV['DISABLE_RACK_ATTACK']
-  end
+  # Disabled in test env in environments/test.rb
+  Rack::Attack.enabled = !ENV["DISABLE_RACK_ATTACK"] unless Rails.env.production?
 
-  # Use redis for rack attack since config.cache_store is not set
-  redis_client = Redis.new
-  Rack::Attack.cache.store = Rack::Attack::StoreProxy::RedisStoreProxy.new(redis_client)
-
-  # Throttle all non asset requests
-  throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-    req.remote_ip unless req.path.start_with?('/assets')
-  end
-
-  # More agressively throttle login requests
+  ### Throttle logins ###
 
   # Throttle by IP
-  throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
-    if req.path == '/users/sign_in' && req.post?
-      req.remote_ip
-    end
+  throttle("throttle logins by ip", limit: 5, period: 20.seconds) do |req|
+    req.remote_ip if req.path == "/users/sign_in" && req.post?
   end
 
   # Throttle by email
-  throttle('logins/email', limit: 5, period: 20.seconds) do |req|
-    if req.path == '/users/sign_in' && req.post?
-      req.params['user']['email']
-    end
+  throttle("throttle logins by email", limit: 5, period: 20.seconds) do |req|
+    req.params["user"]["email"].to_s.downcase if req.path == "/users/sign_in" && req.post?
   end
 
-  self.throttled_response = lambda do |env|
-   [ 429,  # status
-     {},   # headers
-     ['Too many requests, please try again later']] # body
+  ### Throttle password resets ###
+
+  # Throttle by IP
+  throttle("throttle password resets by ip", limit: 5, period: 20.seconds) do |req|
+    req.remote_ip if req.path == "/users/password" && req.post?
+  end
+
+  # Throttle by email
+  throttle("throttle password resets by email", limit: 5, period: 20.seconds) do |req|
+    req.params["user"]["email"].to_s.downcase if req.path == "/users/password" && req.post?
+  end
+
+  ### Throttle logins on API ###
+
+  # Throttle by IP
+  throttle("throttle api logins by ip", limit: 5, period: 20.seconds) do |req|
+    req.remote_ip if req.path == "/api/v1/auth/login" && req.post?
+  end
+
+  # Throttle by email
+  throttle("throttle api logins by email", limit: 5, period: 20.seconds) do |req|
+    req.json_params["email"].to_s.downcase if req.path == "/api/v1/auth/login" && req.post?
+  end
+
+  ### Throttle password resets on API ###
+
+  # Throttle by IP
+  throttle("throttle api password resets by ip", limit: 5, period: 20.seconds) do |req|
+    req.remote_ip if req.path == "/api/v1/password/forgot" && req.post?
+  end
+
+  # Throttle by email
+  throttle("throttle api password resets by email", limit: 5, period: 20.seconds) do |req|
+    req.json_params["email"].to_s.downcase if req.path == "/api/v1/password/forgot" && req.post?
+  end
+
+  self.throttled_response = lambda do |_env|
+    [429, # status
+     {}, # headers
+     ["Too many requests, please try again later"]] # body
   end
 end
