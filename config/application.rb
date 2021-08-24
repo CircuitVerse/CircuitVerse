eef9763843201fd3ef22b28564e15decfbca0b97
pavanjoshi914 <pavanj914@gmail.com>
2021-08-24 09:46:50 +0530

    feat(i18n): localize projects module commontator breaks if translation for current locale not available - enable fallbacks use form_helpers instead of html tags for consistency signed-off-by: Pavan Joshi <pavanj914@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Logix
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :hi]
     config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
 
     # Site config
     config.site_url = "https://circuitverse.org/"
