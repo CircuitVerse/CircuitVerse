02145aaf0e5be3a551e91ec346b67ac0bb239aec
vedant_jain03 <vedantjainben10@gmail.com>
2021-11-06 09:30:44 +0530

    fixes: code-climate issues

--- app/javascript/controllers/addMember_controller.js
+++ app/javascript/controllers/addMember_controller.js
@@ -1,60 +1,57 @@
 import { Controller } from 'stimulus';
-import "bootstrap-tagsinput/dist/bootstrap-tagsinput.js";
-import * as $ from 'jquery/dist/jquery'
+import 'bootstrap-tagsinput/dist/bootstrap-tagsinput.js';
+import * as $ from 'jquery/dist/jquery';
 import 'bootstrap/dist/js/bootstrap';
 
 export default class extends Controller {
-  connect() {
-    $(document).ready(() => {
-      $('#group_member_emails').tagsinput({
-        trimValue: true,
-        confirmKeys: [13, 44, 32],
-      });
+    connect() {
+        $(document).ready(() => {
+            $('#group_member_emails').tagsinput({
+                trimValue: true,
+                confirmKeys: [13, 44, 32],
+            });
 
-      $('.bootstrap-tagsinput input').attr('maxlength', '30');
-      $('.bootstrap-tagsinput input').attr('id','group_email_input');
+            $('.bootstrap-tagsinput input').attr('maxlength', '30');
+            $('.bootstrap-tagsinput input').attr('id', 'group_email_input');
+            $('.add-members-button').attr('disabled', true);
+            $('.bootstrap-tagsinput input').on('keyup', () => {
+                if ($('.bootstrap-tagsinput').children().length > 1) {
+                    $('.add-members-button').attr('disabled', false);
+                }
+                else {
+                    $('.add-members-button').attr('disabled', true);
+                }
+            });
+            $('.bootstrap-tagsinput').on('click', () => {
+                if ($('.bootstrap-tagsinput').children().length > 1) {
+                    $('.add-members-button').attr('disabled', false);
+                } else {
+                    $('.add-members-button').attr('disabled', true);
+                }
+            });
+            document.querySelector('.bootstrap-tagsinput input').addEventListener('paste', (e) => {
+                e.preventDefault();
+                let pastedEmails = '';
+                if (window.clipboardData && window.clipboardData.getData) {
+                    pastedEmails = window.clipboardData.getData('Text');
+                } else if (e.clipboardData && e.clipboardData.getData) {
+                    pastedEmails = e.clipboardData.getData('text/plain');
+                }
 
-      $('.add-members-button').attr('disabled', true);
-
-      $('.bootstrap-tagsinput input').on('keyup', () => {
-        if ($('.bootstrap-tagsinput').children().length > 1) {
-          $('.add-members-button').attr('disabled', false);
-        } else {
-          $('.add-members-button').attr('disabled', true);
-        }
-      });
-
-      $('.bootstrap-tagsinput').on('click', () => {
-        if ($('.bootstrap-tagsinput').children().length > 1) {
-          $('.add-members-button').attr('disabled', false);
-        } else {
-          $('.add-members-button').attr('disabled', true);
-        }
-      })
-
-      document.querySelector('.bootstrap-tagsinput input').addEventListener('paste', (e) => {
-        e.preventDefault();
-        let pastedEmails = '';
-        if (window.clipboardData && window.clipboardData.getData) {
-          pastedEmails = window.clipboardData.getData('Text');
-        } else if (e.clipboardData && e.clipboardData.getData) {
-          pastedEmails = e.clipboardData.getData('text/plain');
-        }
-
-        if (pastedEmails.includes('\n')) {
-          const newLinesIntoSpaces = pastedEmails.replace(/\n/g, ' ');
-          const newLinesIntoSpacesSplitted = newLinesIntoSpaces.split(' ');
-          this.value = pastedEmails.replace(/./g, '');
-          newLinesIntoSpacesSplitted.forEach(value => $('#group_member_emails').tagsinput('add', value))
-          $('.add-members-button').attr('disabled', false)
-        }
-        else {
-          const pastedEmailsSplittedBySpace = pastedEmails.split(' ');
-          this.value = pastedEmails.replace(/./g, '');
-          pastedEmailsSplittedBySpace.forEach(value => $('#group_member_emails').tagsinput('add', value))
-          $('.add-members-button').attr('disabled', false)
-        }
-      });
-    });
-  }
+                if (pastedEmails.includes('\n')) {
+                    const newLinesIntoSpaces = pastedEmails.replace(/\n/g, ' ');
+                    const newLinesIntoSpacesSplitted = newLinesIntoSpaces.split(' ');
+                    this.value = pastedEmails.replace(/./g, '');
+                    newLinesIntoSpacesSplitted.forEach(value => $('#group_member_emails').tagsinput('add', value));
+                    $('.add-members-button').attr('disabled', false);
+                }
+                else {
+                    const pastedEmailsSplittedBySpace = pastedEmails.split(' ');
+                    this.value = pastedEmails.replace(/./g, '');
+                    pastedEmailsSplittedBySpace.forEach(value => $('#group_member_emails').tagsinput('add', value));
+                    $('.add-members-button').attr('disabled', false);
+                }
+            });
+        });
+    }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,9 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
 
-const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+const application = Application.start();
+const context = require.context('controllers', true, /_controller\.js$/);
+application.load(definitionsFromContext(context));
