e426e87304659621f7a88563518840e1f04a1ec6
Prerna02 <sharmaprerna0052@gmail.com>
2021-11-28 15:03:44 +0000

    Added the right functionality to button

--- simulator/src/subcircuit.js
+++ simulator/src/subcircuit.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-cycle */
-import Scope, { scopeList, switchCircuit } from "./circuit";
+import Scope, { changeCircuitName, newCircuit, scopeList, switchCircuit } from "./circuit";
 import CircuitElement from "./circuitElement";
 import simulationArea from "./simulationArea";
 import { scheduleBackup, checkIfBackup } from "./data/backupCircuit";
@@ -19,6 +19,7 @@ import { colors } from "./themer/themer";
 import { layoutModeGet } from "./layoutMode"
 import { verilogModeGet } from "./Verilog2CV"
 import { sanitizeLabel } from './verilogHelpers';
+import { newCircuitCall } from "./hotkey_binder/model/actions";
 /**
  * Function to load a subcicuit
  * @category subcircuit
@@ -32,24 +33,30 @@ export function loadSubCircuit(savedData, scope) {
  * @param {Scope=} scope
  * @category subcircuit
  */
+
+ export const newCircuitall = () => $("#newCircuit").trigger('click');
 export function createSubCircuitPrompt(scope = globalScope) {
     if(verilogModeGet() || layoutModeGet()) {
         showError("Subcircuit cannot be inserted in this mode");
         return;
     }
-    
+    $("#insertSubcircuitDialog").empty();
     let flag = true;
     for (id in scopeList) {
         if (!scopeList[id].checkDependency(scope.id) && scopeList[id].isVisible()) {
-            flag = false;
-            $("#insertSubcircuitDialog").empty();
+            flag = false; 
             $("#insertSubcircuitDialog").append(
                 `<label class="option custom-radio inline"><input type="radio" name="subCircuitId" value="${id}" />${scopeList[id].name}<span></span></label>`
             );
         }
-    }
-    if (flag){
-        $('#insertSubcircuitDialog').show();
+      
+    }  
+    if(flag)
+    {    
+        $("#insertSubcircuitDialog").append(
+            [$("#insertSubcircuitcontent").dialog(),
+             $("#insertSubcircuitcontent").dialog("close")]
+        );
     } 
     $("#insertSubcircuitDialog").dialog({
         resizable:false,
@@ -77,6 +84,7 @@ export function createSubCircuitPrompt(scope = globalScope) {
             : [],
     });
 }
+
 
 /**
  * @class
