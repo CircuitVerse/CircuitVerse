d8343fdfa2c91783b326257fd35805e4d831a8e8
vedant_jain03 <vedantjainben10@gmail.com>
2021-12-12 09:42:30 +0530

    fixes: codeclimate issue

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,8 +5,8 @@ class ProjectsController < ApplicationController
   include SanitizeDescription
   include UsersCircuitverseHelper
 
-  before_action :set_project, only: %i[show edit update destroy generate_collab_token project_invite create_fork change_stars]
-  before_action :authenticate_user!, only: %i[edit update destroy generate_collab_token create_fork change_stars]
+  before_action :set_project, only: %i[show edit update destroy project_invite create_fork change_stars]
+  before_action :authenticate_user!, only: %i[edit update destroy create_fork change_stars]
 
   before_action :check_access, only: %i[edit update destroy]
   before_action :check_delete_access, only: [:destroy]
