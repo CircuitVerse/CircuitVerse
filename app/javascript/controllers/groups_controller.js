972485a25360ecf2bae7f1008a4ef8e1a4c16eee
h2o-Noob <arindamraina7743@gmail.com>
2022-01-06 11:58:12 +0530

    modified bootstrap imports

--- app/javascript/controllers/groups_controller.js
+++ app/javascript/controllers/groups_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from 'stimulus';
 import 'js/jquery.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap-tagsinput/dist/bootstrap-tagsinput.js';
 
 export default class extends Controller {
