6fa280df69f45f213e35c449f4e13dfea3e4795e
Apoorve shukla <apoorve.shukla.cse20@itbhu.ac.in>
2022-01-20 04:50:21 +0000

    edited

--- app/helpers/users_circuitverse_helper.rb
+++ app/helpers/users_circuitverse_helper.rb
@@ -4,7 +4,7 @@ module UsersCircuitverseHelper
   def get_priority_countries
     priority_countries = ["IN"]
     geo_contry = Geocoder.search(request.remote_ip).first&.country
-    if !priority_countries.include?(geo_contry)
+    unless priority_countries.include?(geo_contry)
       priority_countries.prepend(geo_contry) unless geo_contry.nil?
     end
     priority_countries
