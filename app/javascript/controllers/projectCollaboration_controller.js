4e89d877a2385326207708b0d6ebf127373ea995
vedant_jain03 <vedantjainben10@gmail.com>
2022-01-25 23:01:47 +0530

    fix: onclick event integration using stimulus

--- app/javascript/controllers/projectCollaboration_controller.js
+++ app/javascript/controllers/projectCollaboration_controller.js
@@ -2,9 +2,9 @@ import { Controller } from 'stimulus';
 import 'bootstrap-tagsinput/dist/bootstrap-tagsinput.js';
 
 export default class extends Controller {
-    connect() {
-        $('#invite_collab_button').on('click', () => {
-            setTimeout(function () { $("#embedInviteLink").select(); document.execCommand("copy") }, 100);
-        })
+    embedInviteLink() {
+        setTimeout(()=>{
+            $("#embedInviteLink").select(); document.execCommand("copy")
+        }, 100);
     }
 }
