26477d956dd55d755c1690246cc7cbe1b06b5e93
Rachit Agrawal <arachit3006@gmail.com>
2022-01-27 19:02:34 +0000

    minor changes

--- app/controllers/concerns/sanitize_description.rb
+++ app/controllers/concerns/sanitize_description.rb
@@ -8,6 +8,6 @@ module SanitizeDescription
       description,
       tags: %w[img p strong em a sup sub del u span h1 h2 h3 h4 hr li ol ul blockquote br],
       attributes: %w[style src href alt title target]
-      )
+    )
   end
 end
