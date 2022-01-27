b62e479bf61eb7cc49bdf6965e9d7345ea5a0b01
Rachit Agrawal <arachit3006@gmail.com>
2022-01-27 18:45:15 +0000

    changed sanitize_description's styling

--- app/controllers/concerns/sanitize_description.rb
+++ app/controllers/concerns/sanitize_description.rb
@@ -1,11 +1,9 @@
+# frozen_string_literal: true
+
 module SanitizeDescription
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
   
-    def sanitize_description(description)
-        return sanitize(
-            description,
-            tags: %w[img p strong em a sup sub del u span h1 h2 h3 h4 hr li ol ul blockquote br],
-            attributes: %w[style src href alt title target]
-        )
-    end
+  def sanitize_description(description)
+    sanitize(description, tags: %w[img p strong em a sup sub del u span h1 h2 h3 h4 hr li ol ul blockquote br], attributes: %w[style src href alt title target])
+  end
 end
