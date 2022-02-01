72438efa1a105611c7ee38f7c7f0df92a6ea47e9
vedant_jain03 <vedantjainben10@gmail.com>
2022-02-01 20:47:41 +0530

    added helper for memberCardViewer

--- app/helpers/group_members_helper.rb
+++ app/helpers/group_members_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module GroupMembersHelper
+  def membersCardViewer(group)
+    if policy(group).admin_access?
+      return 'col-7 groups-members-card-details'
+    else
+      return 'col-11 groups-members-card-details groups-members-card-details-non-admin'
+    end
+  end
 end
