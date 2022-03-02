4796a5a4441a344f3d7215b57d08129642198083
aashimawadhwa <ashimawadhwa1208@gmail.com>
2022-03-03 00:24:42 +0530

    refractored the code

--- public/js/togglePassword.js
+++ public/js/togglePassword.js
@@ -1,23 +1,24 @@
-function togglePassword() {
-    if ($('.users-password-input').attr('type') === 'text') {
-        $('.users-password-input').attr('type', 'password');
-        $('#toggle-icon').addClass('fa-eye-slash');
-        $('#toggle-icon').removeClass('fa-eye');
-    } else {
-        $('.users-password-input').attr('type', 'text');
-        $('#toggle-icon').addClass('fa-eye');
-        $('#toggle-icon').removeClass('fa-eye-slash');
+function togglePassword(toggle) {
+    if(toggle==0){
+        if ($('.users-password-input').attr('type') === 'text') {
+            $('.users-password-input').attr('type', 'password');
+            $('#toggle-icon').addClass('fa-eye-slash');
+            $('#toggle-icon').removeClass('fa-eye');
+        } else {
+            $('.users-password-input').attr('type', 'text');
+            $('#toggle-icon').addClass('fa-eye');
+            $('#toggle-icon').removeClass('fa-eye-slash');
+        }
     }
-}
-
-function togglePasswordConfirmation() {
-    if ($('.users-password-confirmation-input').attr('type') === 'text') {
-        $('.users-password-confirmation-input').attr('type', 'password');
-        $('#confirmation-toggle-icon').addClass('fa-eye-slash');
-        $('#confirmation-toggle-icon').removeClass('fa-eye');
-    } else {
-        $('.users-password-confirmation-input').attr('type', 'text');
-        $('#confirmation-toggle-icon').addClass('fa-eye');
-        $('#confirmation-toggle-icon').removeClass('fa-eye-slash');
-    }
+    else {
+        if ($('.users-password-confirmation-input').attr('type') === 'text') {
+                    $('.users-password-confirmation-input').attr('type', 'password');
+                    $('#confirmation-toggle-icon').addClass('fa-eye-slash');
+                    $('#confirmation-toggle-icon').removeClass('fa-eye');
+                } else {
+                    $('.users-password-confirmation-input').attr('type', 'text');
+                    $('#confirmation-toggle-icon').addClass('fa-eye');
+                    $('#confirmation-toggle-icon').removeClass('fa-eye-slash');
+                }
+    }  
 }
