52a6c2952d34afe224bc5154331f46e082d9421c
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-03-16 16:41:57 +0530

    fixed codeclimate issue

--- app/controllers/users/circuitverse_controller.rb
+++ app/controllers/users/circuitverse_controller.rb
@@ -11,8 +11,8 @@ class Users::CircuitverseController < ApplicationController
   def index
     @profile = ProfileDecorator.new(@user)
     @projects = @user.rated_projects
-    @is_Admin = @current_user ? @profile.id == @current_user.id : false
-    @public_projects_exist = @is_Admin || @user.projects.open.exists?
+    @is_admin = @current_user ? @profile.id == @current_user.id : false
+    @public_projects_exist = @is_admin || @user.projects.open.exists?
   end
 
   def edit; end
