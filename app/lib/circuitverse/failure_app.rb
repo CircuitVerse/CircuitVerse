27047dd2ac66d31200a66049ee4996f7be03b36e
Aboobacker MK <aboobackervyd@gmail.com>
2022-05-09 20:01:52 +0530

    fix: added factory bot methods in global scope

--- app/lib/circuitverse/failure_app.rb
+++ app/lib/circuitverse/failure_app.rb
@@ -12,7 +12,7 @@ module Circuitverse
 
     def json_api_error_response
       self.status = 401
-      self.media_type = "application/json"
+      self.content_type = "application/json"
       self.response_body = { errors: [{ status: "401", title: i18n_message,
                                         detail: i18n_message }] }.to_json
     end
