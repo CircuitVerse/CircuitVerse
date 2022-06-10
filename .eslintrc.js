45c28ad0dc79a01ad23f0570f5775840ceb09499
Devjit Choudhury <devjitc1@gmail.com>
2022-06-10 17:07:47 +0530

    added EsLint + Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+	'eslint:recommended',
+	"plugin:vue/vue3-recommended",
+	"prettier"
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
