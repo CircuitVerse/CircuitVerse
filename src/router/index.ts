b97ed72f79885073322b4689727b1fafc5856e95
Devjit Choudhury <devjitc1@gmail.com>
2022-06-10 17:23:55 +0530

    format document wrt prettier rules

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/Home.vue'
-import Simulator from '../components/Simulator.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import Simulator from "../components/Simulator.vue";
 
 const routes = [
-	{
-		path: '/',
-		name: 'Home',
-		component: Home
-	},
-	{
-		path: '/simulator',
-		name: 'Simulator',
-		component: Simulator
-	},
-]
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/simulator",
+        name: "Simulator",
+        component: Simulator,
+    },
+];
 const router = createRouter({
-	history: createWebHistory(),
-	routes,
-})
+    history: createWebHistory(),
+    routes,
+});
 
-export default router
+export default router;
--- src/store/Home.ts
+++ src/store/Home.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 
-export const HomeStore = defineStore('main', {
-	state: () => ({
-		title: "Welcome to CircuitVerse Home Page",
-	})
-})
+export const HomeStore = defineStore("main", {
+    state: () => ({
+        title: "Welcome to CircuitVerse Home Page",
+    }),
+});
