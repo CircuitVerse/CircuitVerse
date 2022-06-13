a476c242b7a8447b9673730098fe2a405a9df923
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-06-13 18:02:19 +0530

    fix: remove flipper from config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,11 +29,9 @@ Devise.setup do |config|
   config.omniauth :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], {
     :scope => 'read:user'
   }
-  if Flipper.enabled?(:gitlab_integration)
-    config.omniauth :gitlab, ENV['GITLAB_CLIENT_ID'], ENV['GITLAB_CLIENT_SECRET'], {
-      :scope => 'read_user'
-    }
-  end
+  config.omniauth :gitlab, ENV['GITLAB_CLIENT_ID'], ENV['GITLAB_CLIENT_SECRET'], {
+    :scope => 'read_user'
+  }
   config.omniauth :microsoft_office365,ENV['MICROSOFT_CLIENT_ID'], ENV['MICROSOFT_CLIENT_SECRET']
 
   # Load and configure the ORM. Supports :active_record (default) and
