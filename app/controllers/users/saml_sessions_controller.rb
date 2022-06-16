5c2b5c491710f4d9d702686371afbd498aaef11e
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-06-16 15:43:38 +0530

    [requested-changes] added feature flag flipper

--- app/controllers/users/saml_sessions_controller.rb
+++ app/controllers/users/saml_sessions_controller.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
 class Users::SamlSessionsController < Devise::SamlSessionsController
-  # after_action :store_winning_strategy, only: :create
 
-  # def create
-  #   super
-  # end
-
-  # private
-
-  #   def store_winning_strategy
-  #     warden.session(:user)[:strategy] = warden.winning_strategies[:user].class.name.demodulize.underscore.to_sym
-  #   end
 end
