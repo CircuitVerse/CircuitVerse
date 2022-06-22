29a7802ce5001479b7b1c316362d38ece8443608
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-06-22 11:15:04 +0530

    [feature] : added stimulus code for bootstrap-tagsinput, added translation, added responsiveness

--- app/javascript/controllers/groups_controller.js
+++ app/javascript/controllers/groups_controller.js
@@ -2,6 +2,44 @@ import { Controller } from 'stimulus';
 import 'bootstrap-tagsinput/dist/bootstrap-tagsinput.js';
 
 export default class extends Controller {
+    addMentorToGroup() {
+        $('#group_mentor_emails').tagsinput({
+            trimValue: true,
+            confirmKeys: [13, 44, 32],
+        });
+        $('.bootstrap-tagsinput input').attr('maxlength', '30');
+        $('.bootstrap-tagsinput input').attr('id', 'group_email_input_mentor');
+        $('.add-mentor-button').attr('disabled', true);
+        $('.bootstrap-tagsinput input').on('keyup click', () => {
+            if ($('.bootstrap-tagsinput').children().length > 1) {
+                $('.add-mentor-button').attr('disabled', false);
+            } else {
+                $('.add-mentor-button').attr('disabled', true);
+            }
+        });
+        document.querySelector('.bootstrap-tagsinput input').addEventListener('paste', (e) => {
+            e.preventDefault();
+            let pastedEmails = '';
+            if (window.clipboardData && window.clipboardData.getData) {
+                pastedEmails = window.clipboardData.getData('Text');
+            } else if (e.clipboardData && e.clipboardData.getData) {
+                pastedEmails = e.clipboardData.getData('text/plain');
+            }
+
+            if (pastedEmails.includes('\n')) {
+                const newLinesIntoSpaces = pastedEmails.replace(/\n/g, ' ');
+                const newLinesIntoSpacesSplitted = newLinesIntoSpaces.split(' ');
+                this.value = pastedEmails.replace(/./g, '');
+                newLinesIntoSpacesSplitted.forEach((value) => $('#group_mentor_emails').tagsinput('add', value));
+                $('.add-mentor-button').attr('disabled', false);
+            } else {
+                const pastedEmailsSplittedBySpace = pastedEmails.split(' ');
+                this.value = pastedEmails.replace(/./g, '');
+                pastedEmailsSplittedBySpace.forEach((value) => $('#group_mentor_emails').tagsinput('add', value));
+                $('.add-mentor-button').attr('disabled', false);
+            }
+        });
+    }
     addMemberToGroup() {
         $('#group_member_emails').tagsinput({
             trimValue: true,
