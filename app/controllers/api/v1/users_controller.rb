6e5a830e57c3f5af7b84f2f1ca2b17db9041dd93
Aboobacker MK <aboobackervyd@gmail.com>
2022-06-24 05:53:12 +0530

    chore: fail with error when the update fails

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   # PATCH api/v1/users/:id
   def update
     @user.update!(user_params)
-    if @user.update(user_params)
+    if @user.update!(user_params)
       render json: Api::V1::UserSerializer.new(@user, @options), status: :accepted
     else
       invalid_resource!(@user.errors)
