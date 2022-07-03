27cb7953fc6453854df3be98ee8d3670ce89e6d4
Devjit Choudhury <devjitc1@gmail.com>
2022-07-04 01:04:32 +0530

    feat: created input group component as replacement from jQueryUI inputSpinner

--- src/components/Panels/PropertiesPanel/PanelTypes/PanelType3.vue
+++ src/components/Panels/PropertiesPanel/PanelTypes/PanelType3.vue
@@ -1,75 +1,19 @@
 <template>
-    <p>
-        <span>BitWidth:</span>
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <button 
-                    style=" border:none; min-width: 2.5rem" 
-                    class="btnDecrement" 
-                    type="button"
-                     @click="decreaseValue()"
-                >
-                    <strong>-</strong>
-                </button>
-            </div>
-            <input
-                id="number"
-                style="text-align: center"
-                class="objectPropertyAttribute form-control"
-                type="number"
-                name="newBitWidth"
-                min="1"
-                max="32"
-                :value="obj.bitWidth"
-            />
-            <div class="input-group-append">
-                <button 
-                    style="border:none; min-width: 2.5rem" 
-                    class="btnIncrement" 
-                    type="button"
-                    @click="increaseValue()"
-                >
-                    <strong>+</strong>
-                </button>
-            </div>
-        </div>
-    </p>
+    <InputGroups
+        property-name="BitWidth: "
+        :property-value="obj.bitWidth"
+        property-value-type="number"
+        value-min="1"
+        value-max="32"
+        property-input-name="newBitWidth"
+        property-input-id="number"
+    />
 </template>
 
 <script lang="ts" setup>
-import simulationArea from '#/simulator/src/simulationArea'
-import { checkPropertiesUpdate, objectPropertyAttributeUpdate } from '#/simulator/src/ux'
-
+import InputGroups from '../../Shared/InputGroups.vue'
 const props = defineProps({
     data: { type: Object, default: undefined },
 })
 const obj = props.data
-// console.log(obj)
-// console.log(simulationArea.lastSelected)
-
-function increaseValue() {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    obj.bitWidth++;
-    document.getElementById('number').value = value;
-    // manually triggering on change event
-    const e = new Event("change");
-    const element = document.querySelector('.objectPropertyAttribute')
-    element.dispatchEvent(e);
-}
-
-function decreaseValue() {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value < 1 ? value = 1 : '';
-    value--;
-    obj.bitWidth--;
-    document.getElementById('number').value = value;
-    // manually triggering on change event
-    const e = new Event("change");
-    const element = document.querySelector('.objectPropertyAttribute')
-    element.dispatchEvent(e);
-}
-
 </script>
---
+++ src/components/Panels/Shared/InputGroups.vue
@@ -0,0 +1,75 @@
+<template>
+	<p>
+        <span>{{propertyName}}</span>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <button 
+                    style=" border:none; min-width: 2.5rem" 
+                    class="btnDecrement" 
+                    type="button"
+                     @click="decreaseValue()"
+                >
+                    <strong>-</strong>
+                </button>
+            </div>
+            <input
+                :id="propertyInputId"
+                style="text-align: center"
+                class="objectPropertyAttribute form-control"
+                :type="propertyValueType"
+                :name="propertyInputName"
+                :min="valueMin"
+                :max="valueMax"
+                :value="propertyValue"
+            />
+            <div class="input-group-append">
+                <button 
+                    style="border:none; min-width: 2.5rem" 
+                    class="btnIncrement" 
+                    type="button"
+                    @click="increaseValue()"
+                >
+                    <strong>+</strong>
+                </button>
+            </div>
+        </div>
+    </p>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+	propertyName: { type: String, default: 'Property Name' },
+	propertyValue: { type: Number, default: 0 },
+	propertyValueType: { type: String, default: "number" },
+	valueMin: { type: String, default: "0" },
+	valueMax: { type: String, default: "100" },
+	propertyInputName: { type: String, default: 'Property_Input_Name' },
+	propertyInputId: { type: String, default: 'Property_Input_Id' }
+})
+
+function increaseValue() {
+	const ele = document.getElementById(props.propertyInputId);
+    var value = parseInt(ele.value, 10);
+	value = isNaN(value) ? 0 : value;
+	value++;
+    props.propertyValue = value;
+    ele.value = value;
+    // manually triggering on change event
+    const e = new Event("change");
+    const element = document.querySelector('.objectPropertyAttribute')
+    element.dispatchEvent(e);
+}
+
+function decreaseValue() {
+	const ele = document.getElementById(props.propertyInputId);
+    var value = parseInt(ele.value, 10);
+	value = isNaN(value) ? 0 : value;
+	value--;
+    props.propertyValue = value;
+    ele.value = value;
+    // manually triggering on change event
+    const e = new Event("change");
+    const element = document.querySelector('.objectPropertyAttribute')
+    element.dispatchEvent(e);
+}
+</script>
