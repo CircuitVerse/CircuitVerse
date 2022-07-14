51c832c93c7cda2804e3ff269d2e450e8dcbb723
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-07-14 21:54:52 +0530

    revert last commit

--- app/controllers/api/v1/group_members_controller.rb
+++ app/controllers/api/v1/group_members_controller.rb
@@ -19,8 +19,7 @@ class Api::V1::GroupMembersController < Api::V1::BaseController
   def create
     dummy = GroupMember.new
     dummy.group_id = params[:group_id]
-    group = Group.find(:group_id)
-    authorize group.members.new, :primary_mentor?
+    authorize dummy, :primary_mentor?
     @group = Group.find(params[:group_id])
     is_mentor = false
     is_mentor = params[:mentor] if params[:mentor]
