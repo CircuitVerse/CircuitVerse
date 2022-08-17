acc2d39cff69f63927116ab8cf25626b07fb179c
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-08-17 17:27:30 +0530

    fix: added specs for notifications api

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -9,16 +9,17 @@ class Api::V1::NotificationsController < Api::V1::BaseController
     render json: Api::V1::NotificationSerializer.new(@notifications)
   end
 
-  # POST /api/v1/notification
+  # POST /api/v1/notifications/mark_as_read/:notification_id
   def mark_as_read
     @notification = NoticedNotification.find(params[:notification_id])
     @notification.update(read_at: Time.zone.now)
     render json: Api::V1::NotificationSerializer.new(@notification), status: :created
   end
 
-  # GET /api/v1/notification
+  # GET /api/v1/notifications/mark_all_as_read
   def mark_all_as_read
-    @notifications = NoticedNotification.where(recipient: current_user, read_at: nil).update_all(read_at: Time.zone.now)
-    render json: Api::V1::NotificationSerializer.new(@notification)
+    NoticedNotification.where(recipient: current_user, read_at: nil).update_all(read_at: Time.zone.now)
+    @notifications = NoticedNotification.where(recipient: current_user).newest_first
+    render json: Api::V1::NotificationSerializer.new(@notifications)
   end
 end
---
+++ spec/requests/api/v1/notifications_controller/mark_all_as_read_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::NotificationsController, "#mark_all_as_read", type: :request do
+  describe "mark all notifications as read" do
+    context "when not authenticated" do
+      before do
+        get "/api/v1/notifications", as: :json
+      end
+
+      it "returns status unauthorized" do
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.parsed_body).to have_jsonapi_errors
+      end
+    end
+
+    context "when authenticated" do
+      before do
+        @author = FactoryBot.create(:user)
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, author: @author)
+        @notification = FactoryBot.create(
+          :noticed_notification,
+          recipient: @author,
+          params:
+            { user_id: @user.id, project_id: @project.id },
+          read_at: nil
+        )
+      end
+
+      it "mark all notifications as read" do
+        token = get_auth_token(@author)
+        get "/api/v1/notifications/mark_all_as_read",
+            headers: { Authorization: "Token #{token}" }, as: :json
+        expect(response).to have_http_status(:ok)
+        expect(response).to match_response_schema("read_notifications")
+        expect(response.parsed_body["data"].length).to eq(1)
+      end
+    end
+  end
+end
