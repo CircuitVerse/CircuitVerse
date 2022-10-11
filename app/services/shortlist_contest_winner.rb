4b5a1560caa37bc52c814168a30ea3bbf21fff10
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-10-11 17:13:00 +0530

    feat: added rspec testcases

--- app/services/shortlist_contest_winner.rb
+++ app/services/shortlist_contest_winner.rb
@@ -4,7 +4,7 @@ class ShortlistContestWinner
   def initialize(contest_id)
     @contest = Contest.find(contest_id)
     @most_voted_submission = Submission.where(contest_id: @contest.id).order("submission_votes_count DESC").limit(1).first
-    return if @most_voted_submission.nil? && @most_voted_submission.submission_votes_count.zero?
+    return if @most_voted_submission.nil?
 
     contest_winner = ContestWinner.new
     contest_winner.contest_id = @contest.id
