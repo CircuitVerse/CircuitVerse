fee24bde86caf9d6b7610b19ba21000694b6ed83
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-10-15 00:27:07 +0530

    feat: code review changes

--- app/notifications/new_assignment_notification.rb
+++ app/notifications/new_assignment_notification.rb
@@ -6,7 +6,7 @@ class NewAssignmentNotification < Noticed::Base
   def message
     # user = params[:group_member.user]
     assignment = params[:assignment]
-    # t("users.notifications.new_assignment_notification", assignment: assingment.name)
+    #  t("users.notifications.new_assignment_notification", assignment: assingment.name)
     t("users.notifications.new_assignment_notification", assignment_name: assignment.name)
   end
 
