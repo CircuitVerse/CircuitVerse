3d3765183b57b60314b812000c699240768a3eed
Vaibhav Upreti <85568177+VaibhavUpreti@users.noreply.github.com>
2022-12-09 13:44:05 +0530

    Update group_member_spec.rb

--- spec/models/group_member_spec.rb
+++ spec/models/group_member_spec.rb
@@ -28,7 +28,6 @@ RSpec.describe GroupMember, type: :model do
         group_member.send_welcome_email
       end.to have_enqueued_job.on_queue("mailers")
     end
-    
     it "sends welcome notification" do
       group_member = FactoryBot.create(:group_member, user: @user, group: @group)
       group_member.send_welcome_notification
