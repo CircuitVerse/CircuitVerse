bf65848c157a460b8b827b756c7c3f874c86f935
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-12-14 19:52:20 +0530

    fix: added group name

--- app/notifications/new_group_notification.rb
+++ app/notifications/new_group_notification.rb
@@ -4,8 +4,9 @@ class NewGroupNotification < Noticed::Base
   deliver_by :database, association: :noticed_notifications
 
   def message
-    group = params[:group][:group_id]
-    t("users.notifications.new_group_notification", group: group)
+    @group = params[:group][:group_id]
+    group = Group.find(@group)
+    t("users.notifications.new_group_notification", group: group.name)
   end
 
   def icon
