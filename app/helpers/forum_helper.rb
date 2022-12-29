316f142e94771a890b126f19c6bef6624c532d23
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-12-30 01:45:26 +0530

    feat: add helper

--- app/helpers/forum_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module ForumHelper
-  def formatted_content(text)
-    options = %i[hard_wrap filter_html autolink tables]
-    Redcarpet.new(text, *options).to_html
-  end
-end
