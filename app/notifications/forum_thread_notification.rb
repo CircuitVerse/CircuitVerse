6fbbbe3707c7f4409429001edbc42c69fa10649d
Vaibhav Upreti <85568177+VaibhavUpreti@users.noreply.github.com>
2023-01-20 18:10:41 +0530

    feat: comment and new thread notification (#3483)
    * feat: comment and new thread notification
    
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

---
+++ app/notifications/forum_thread_notification.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ForumThreadNotification < Noticed::Base
+  deliver_by :database, association: :noticed_notifications
+
+  def message
+    user = params[:user]
+    thread = params[:forum_thread]
+    t("users.notifications.forum_thread_notification", user: user.name, thread: thread.title.truncate_words(4))
+  end
+
+  def icon
+    "far fa-comments"
+  end
+end
