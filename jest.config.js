02b6d46c80dd4224f2b48918a03ffd223052c294
Prerna02 <sharmaprerna0052@gmail.com>
2023-01-22 12:03:45 +0000

    resolved code climate issues

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     setupFiles: ['./jest.setup.js'],
     testMatch: ['**/simulator/spec/**/*.spec.js'],
-    maxWorkers: "50%",
+    maxWorkers: '50%',
     verbose: true,
     moduleNameMapper: {
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
