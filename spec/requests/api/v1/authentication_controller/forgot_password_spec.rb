35c30b344b71bf4682ae188049040be3cd8d5460
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-01-22 22:48:48 +0530

    fix: api spec
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- spec/requests/api/v1/authentication_controller/forgot_password_spec.rb
+++ spec/requests/api/v1/authentication_controller/forgot_password_spec.rb
@@ -25,7 +25,6 @@ RSpec.describe Api::V1::AuthenticationController, "#forgot_password", type: :req
 
       it "returns status 200 and should send reset password instructions" do
         expect(response).to have_http_status(:ok)
-        # Ensure the mailer is sent
         expect(response.parsed_body["message"]).to eq("password reset instructions sent to #{@user.email}")
       end
     end
