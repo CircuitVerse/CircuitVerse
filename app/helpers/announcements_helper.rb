194bf651a1ce9abc0a38352e8c7970520f6d90b3
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2023-01-25 21:53:56 +0530

    [revamp] : announcement UI

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -4,4 +4,8 @@ module AnnouncementsHelper
   def current_announcement
     Announcement.current
   end
+
+  def show_announcement(announcement)
+    announcement.count.positive? ? "navbar navbar-announcement navbar-expand-lg bg-white" : "navbar navbar-expand-lg bg-white"
+  end
 end
