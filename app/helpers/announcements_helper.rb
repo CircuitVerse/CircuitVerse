1e83875c416eb49b630dd9e190a771cb202b4e00
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2023-01-27 16:43:53 +0530

    [fix] : rubocop linting

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -6,6 +6,8 @@ module AnnouncementsHelper
   end
 
   def show_announcement(announcement)
+    # rubocop:disable Layout/LineLength
     announcement.count.positive? ? "navbar navbar-announcement navbar-expand-lg bg-white" : "navbar navbar-expand-lg bg-white"
+    # rubocop:enable Layout/LineLength
   end
 end
