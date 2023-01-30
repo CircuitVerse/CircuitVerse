4f6bd4722c7165f9f42c864e7071e47c8d370d87
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2023-01-30 20:44:15 +0530

    [fix]: requested-changes

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -4,8 +4,4 @@ module AnnouncementsHelper
   def current_announcement
     Announcement.current
   end
-
-  def show_announcement
-    current_announcement ? "navbar navbar-announcement navbar-expand-lg bg-white" : "navbar navbar-expand-lg bg-white"
-  end
 end
