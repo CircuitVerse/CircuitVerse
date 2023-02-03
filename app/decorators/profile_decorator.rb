d99900a1e6ed6e44be5ec43a574e2a9453624630
Aboobacker MK <aboobackervyd@gmail.com>
2023-02-03 14:31:08 +0530

    Handle empty institution field

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -10,7 +10,7 @@ class ProfileDecorator < SimpleDelegator
   end
 
   def educational_institute
-    profile.educational_institute.strip.presence || "Not Entered"
+    profile.educational_institute&.strip&.presence || "Not Entered"
   end
 
   def country_name
