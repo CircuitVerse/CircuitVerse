4367f9723ad204c6bff0738e88b9a5865d6c29f1
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-02-03 19:21:11 +0530

    feat: default star notification
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- spec/factories/noticed_notifications.rb
+++ spec/factories/noticed_notifications.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-arr1 = %w[ForumCommentNotification ForumThreadNotification]
-arr2 = %w[StarNotification ForkNotification NewAssignmentNotification NoType]
+# Notification Events: ForumCommentNotification, ForumThreadNotification, StarNotification, ForkNotification,
+# NewAssignmentNotification, NoType
+
 FactoryBot.define do
   factory :noticed_notification do
-    type { arr2.concat(arr1) }
+    type { "StarNotification" }
   end
 end
