370b003026c526a15d49dcedef7e4d77308b294a
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-02-06 18:31:54 +0530

    refactor: controller
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- app/controllers/users/noticed_notifications_controller.rb
+++ app/controllers/users/noticed_notifications_controller.rb
@@ -8,11 +8,27 @@ class Users::NoticedNotificationsController < ApplicationController
     @unread = NoticedNotification.where(recipient: current_user).newest_first.unread
   end
 
-  # robocop:disable Metrics/MethodLength
   def mark_as_read
     notification = NoticedNotification.find(params[:notification_id])
     notification.update(read_at: Time.zone.now)
     answer = NotifyUser.new(params).call
+    notification_path(answer)
+  end
+
+  def mark_all_as_read
+    NoticedNotification.where(recipient: current_user, read_at: nil).update_all(read_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
+    redirect_to notifications_path(current_user)
+  end
+
+  def read_all_notifications
+    NoticedNotification.where(recipient: current_user, read_at: nil).update_all(read_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
+    redirect_back(fallback_location: root_path)
+  end
+end
+
+  private
+
+  def notification_path(answer)
     case answer.type
     when "new_group"
       redirect_to group_path(answer.first_param)
@@ -28,15 +44,3 @@ class Users::NoticedNotificationsController < ApplicationController
       redirect_to root_path
     end
   end
-  # robocop:enable Metrics/MethodLength
-
-  def mark_all_as_read
-    NoticedNotification.where(recipient: current_user, read_at: nil).update_all(read_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
-    redirect_to notifications_path(current_user)
-  end
-
-  def read_all_notifications
-    NoticedNotification.where(recipient: current_user, read_at: nil).update_all(read_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
-    redirect_back(fallback_location: root_path)
-  end
-end
