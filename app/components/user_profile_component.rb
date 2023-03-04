77779bbe3aecf1ecc0f6f7ca58ef1013fbb3fbaa
Waishnav <waishnavdeore@gmail.com>
2023-03-04 09:35:04 +0000

    feat: profile view component added

---
+++ app/components/user_profile_component.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+require "#{Rails.root}/app/policies/user_policy.rb"
+
+class UserProfileComponent < ViewComponent::Base
+  attr_reader :current_user
+
+  def initialize(profile:, current_user:)
+    super
+    @profile = profile
+    @current_user = current_user
+  end
+
+  def policy(user)
+    UserPolicy.new(user, @profile)
+  end
+
+  def show_edit_link?
+    # Check permissions using the policy object
+    policy(current_user).edit?
+  end
+end
