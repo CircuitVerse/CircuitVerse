d01c82ba550f87e7ee153f7ebc62ac105b9b84e3
Waishnav <waishnavdeore@gmail.com>
2023-03-04 09:35:54 +0000

    feat: project view component added

---
+++ app/components/users_project_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+require "#{Rails.root}/app/policies/project_policy.rb"
+
+class UsersProjectComponent < ViewComponent::Base
+  attr_reader :current_user
+
+  def initialize(project:, current_user:)
+    super
+    @project = project
+    @current_user = current_user
+  end
+
+  def policy(user)
+    ProjectPolicy.new(user, @project)
+  end
+
+  def check_direct_view_access?
+    policy(current_user).check_direct_view_access?
+  end
+
+  def user_access?
+    policy(current_user).user_access?
+  end
+
+end
