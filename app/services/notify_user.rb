7fec00ff5e77ce32d388e09717c1fdcd883d7a92
vedant-jain03 <vedantjainben10@gmail.com>
2023-04-10 22:39:14 +0530

    bump

--- app/services/notify_user.rb
+++ app/services/notify_user.rb
@@ -4,8 +4,8 @@ class NotifyUser
   Result = Struct.new(:success, :type, :first_param, :second)
   def initialize(params)
     @notification = NoticedNotification.find(params[:notification_id])
-    @assignment = @notification.params[:assignment]
     @project = @notification.params[:project]
+    @assignment = @notification.params[:assignment]
     @thread = @notification.params[:forum_thread]
     @contest = @notification.params[:contest]
   end
@@ -21,15 +21,14 @@ class NotifyUser
 
     def type_check
       case @notification.type
-      when "StarNotification"
-        Result.new("true", "star", @project.author, @project)
-      when "ForkNotification"
-        Result.new("true", "fork", @project.author, @project)
+      when "NewGroupNotification"
+        handle_group_notification
+      when "StarNotification", "ForkNotification"
+        Result.new("true", @notification.type[0, 4].downcase, @project.author, @project)
       when "NewAssignmentNotification"
         Result.new("true", "new_assignment", @assignment.group, @assignment)
       when "ForumCommentNotification"
-        @post = @notification.params[:forum_post]
-        Result.new("true", "forum_comment", @thread, @post.id)
+        handle_forum_comment
       when "ForumThreadNotification"
         Result.new("true", "forum_thread", @thread)
       when "ContestNotification"
@@ -39,5 +38,15 @@ class NotifyUser
       else
         Result.new("false", "no_type", root_path)
       end
+    end
+
+    def handle_group_notification
+      @group = @notification.params[:group][:group_id]
+      Result.new("true", "new_group", @group, 1)
+    end
+
+    def handle_forum_comment
+      @post = @notification.params[:forum_post]
+      Result.new("true", "forum_comment", @thread, @post.id)
     end
 end
