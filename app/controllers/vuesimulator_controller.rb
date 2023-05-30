8f2af7154ba7e83c5834c7c4b998f1e98def84d7
Arnabdaz <arnab-das@outlook.com>
2023-05-30 16:53:23 +0000

    feat(gsoc): update vuesimulaotr_controller.rb, fix cross-site scripting issue
    Signed-off-by: Arnabdaz <arnab-das@outlook.com>

--- app/controllers/vuesimulator_controller.rb
+++ app/controllers/vuesimulator_controller.rb
@@ -1,6 +1,16 @@
+# app/controllers/vuesimulator_controller.rb
+# frozen_string_literal: true
+
 class VuesimulatorController < ApplicationController
-    def simulatorvue
-        html = File.read(Rails.root.join("public","simulatorvue","index.html"))
-        render html: html.html_safe, layout: false
-    end
+  def simulatorvue
+    html = File.read(Rails.root.join("public", "simulatorvue", "index.html"))
+    sanitized_html = sanitize(html)
+    render html: sanitized_html.html_safe, layout: false
+  end
+
+  private
+
+  def sanitize(html)
+    ActionController::Base.helpers.sanitize(html)
+  end
 end
