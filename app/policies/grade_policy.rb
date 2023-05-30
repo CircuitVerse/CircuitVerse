a8ddaff965c7dfa3933f227aa32445baadd531a1
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-05-30 16:17:35 +0530

    feat(solargraph): yard docs for policies added

--- app/policies/grade_policy.rb
+++ app/policies/grade_policy.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 class GradePolicy < ApplicationPolicy
-  attr_reader :user, :grade
+  # @return [User]
+  attr_reader :user
+  # @return [Grade]
+  attr_reader :grade
 
+  # @param [User] user
+  # @param [Grade] grade
   def initialize(user, grade)
     @user = user
     @grade = grade
   end
 
+  # Verify if the user is a mentor of the group
+  # @return [Boolean]
   def mentor?
     grade.assignment&.group&.primary_mentor_id == user.id \
     || grade.assignment&.group&.group_members&.exists?(user_id: user.id, mentor: true)
