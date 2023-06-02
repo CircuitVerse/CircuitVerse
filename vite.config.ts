cd9fc22e718af1a6451ff04009489fe5d6b5374d
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-06-02 18:43:27 +0530

    feat(vite_ruby): remove unnecessary dependecies changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
-import  viteEnvCompatible  from 'vite-plugin-env-compatible';
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
-    viteEnvCompatible()
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {}
-    }
-  }
+    RubyPlugin()
+  ]
 })
