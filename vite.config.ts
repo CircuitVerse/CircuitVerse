91865abddcddbce17a28fe760fa48be6d099ffa3
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-06-02 16:24:38 +0530

    feat(vite_ruby): everything working

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import  viteEnvCompatible  from 'vite-plugin-env-compatible';
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    viteEnvCompatible()
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {}
+    }
+  }
 })
