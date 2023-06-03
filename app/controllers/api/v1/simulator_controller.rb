43f8967d03f3faf49cbfdc2450cddd4674ffa294
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-06-04 02:15:09 +0530

    feat(API): fix linting issues
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- app/controllers/api/v1/simulator_controller.rb
+++ app/controllers/api/v1/simulator_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::SimulatorController < Api::V1::BaseController
   include SimulatorHelper
   include ActionView::Helpers::SanitizeHelper
 
-  before_action :authenticate_user!, only: %i[update edit update_image]
+  before_action :authenticate_user!, only: %i[update edit]
   before_action :set_project, only: %i[show edit]
   before_action :check_view_access, only: %i[show]
-  before_action :set_user_project, only: %i[update update_image]
-  before_action :check_edit_access, only: %i[edit update update_image]
-  skip_before_action :verify_authenticity_token, only: %i[show update]
+  before_action :set_user_project, only: %i[update]
+  before_action :check_edit_access, only: %i[edit update]
+  # skip_before_action :verify_authenticity_token, only: %i[show update]
 
   # Get api/v1/simulator/:id/edit
   def edit
@@ -28,18 +28,11 @@ class Api::V1::SimulatorController < Api::V1::BaseController
 
   # POST api/v1/simulator/update
   def update
-    @project.build_project_datum unless ProjectDatum.exists?(project_id: @project.id)
-    @project.project_datum.data = sanitize_data(@project, params[:data])
-
-    image_file = return_image_file(params[:image])
-
-    @project.image_preview = image_file
-    @project.name = sanitize(params[:name])
+    build_project_datum
+    update_project_params
 
     if @project.save && @project.project_datum.save
-      image_file.close
-      File.delete(image_file) if check_to_delete(params[:image])
-
+      handle_image_file_cleanup
       render json: { status: "success", project: @project }, status: :ok
     else
       render json: { status: "error", errors: @project.errors.full_messages }, status: :unprocessable_entity
@@ -48,23 +41,39 @@ class Api::V1::SimulatorController < Api::V1::BaseController
 
   private
 
-    def set_project
-      @project = Project.friendly.find(params[:id])
-    end
+  def build_project_datum
+    @project.build_project_datum unless ProjectDatum.exists?(project_id: @project.id)
+    @project.project_datum.data = sanitize_data(@project, params[:data])
+  end
 
-    # FIXME: remove this logic after fixing production data
-    # def set_user_project
-    #   @project = current_user.projects.friendly.find_by(id: params[:id]) || Project.friendly.find(params[:id])
-    # end
-    def set_user_project
-      @project = current_user.projects.friendly.find(params[:id])
-    end
+  def update_project_params
+    @image_file = return_image_file(params[:image])
+    @project.image_preview = @image_file
+    @project.name = sanitize(params[:name])
+  end
 
-    def check_edit_access
-      authorize @project, :check_edit_access?
-    end
+  def handle_image_file_cleanup
+    @image_file.close
+    File.delete(@image_file) if check_to_delete(params[:image])
+  end
 
-    def check_view_access
-      authorize @project, :check_view_access?
-    end
+  def set_project
+    @project = Project.friendly.find(params[:id])
+  end
+
+  # FIXME: remove this logic after fixing production data
+  # def set_user_project
+  #   @project = current_user.projects.friendly.find_by(id: params[:id]) || Project.friendly.find(params[:id])
+  # end
+  def set_user_project
+    @project = current_user.projects.friendly.find(params[:id])
+  end
+
+  def check_edit_access
+    authorize @project, :check_edit_access?
+  end
+
+  def check_view_access
+    authorize @project, :check_view_access?
+  end
 end
--- app/helpers/api/v1/simulator_helper.rb
+++ app/helpers/api/v1/simulator_helper.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
+
 module Api::V1::SimulatorHelper
   def return_image_file(data_url)
-    str = data_url[("data:image/jpeg;base64,".length)..]
+    str = data_url[23..]
     if str.to_s.empty?
       path = Rails.public_path.join("images/default.png")
       image_file = File.open(path, "rb")
     else
       jpeg = Base64.decode64(str)
-      image_file = File.new("tmp/preview_#{Time.zone.now.to_f.to_s.sub(".", "")}.jpeg", "wb")
+      image_file = File.new("tmp/preview_#{Time.zone.now.to_f.to_s.sub('.', '')}.jpeg", "wb")
       image_file.write(jpeg)
     end
 
@@ -15,7 +16,7 @@ module Api::V1::SimulatorHelper
   end
 
   def check_to_delete(data_url)
-    !data_url[("data:image/jpeg;base64,".length)..].to_s.empty?
+    !data_url[23..].to_s.empty?
   end
 
   def sanitize_data(project, data)
@@ -25,18 +26,22 @@ module Api::V1::SimulatorHelper
     saved_restricted_elements = Oj.safe_load(project.assignment.restrictions)
     scopes = data["scopes"] || []
 
-    parsed_scopes = scopes.each_with_object([]) do |scope, new_scopes|
-      restricted_elements_used = []
+    data["scopes"] = parse_scopes(scopes, saved_restricted_elements)
+    data.to_json
+  end
 
-      saved_restricted_elements.each do |element|
-        restricted_elements_used.push(element) if scope[element].present?
-      end
+  private
 
-      scope["restrictedCircuitElementsUsed"] = restricted_elements_used
+  def parse_scopes(scopes, saved_restricted_elements)
+    scopes.each_with_object([]) do |scope, new_scopes|
+      scope["restrictedCircuitElementsUsed"] = find_restricted_elements_used(scope, saved_restricted_elements)
       new_scopes.push(scope)
     end
+  end
 
-    data["scopes"] = parsed_scopes
-    data.to_json
+  def find_restricted_elements_used(scope, saved_restricted_elements)
+    saved_restricted_elements.each_with_object([]) do |element, restricted_elements_used|
+      restricted_elements_used.push(element) if scope[element].present?
+    end
   end
 end
