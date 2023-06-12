a77a0b210e178953f95184967f9c4d5b787e1cf0
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-06-12 10:18:21 +0530

    feat: remove comments

--- config/definition.rb
+++ config/definition.rb
@@ -1,11 +1,3 @@
-# The following comments fill some of the gaps in Solargraph's understanding of
-# Rails apps. Since they're all in YARD, they get mapped in Solargraph but
-# ignored at runtime.
-#
-# You can put this file anywhere in the project, as long as it gets included in
-# the workspace maps. It's recommended that you keep it in a standalone file
-# instead of pasting it into an existing one.
-#
 # @!parse
 #   class ActionController::Base
 #     include ActionController::MimeResponds
