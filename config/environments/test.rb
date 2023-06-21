39350209cb33540ec66bb74ec7cf8dae6c6564ae
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-06-21 21:19:18 +0530

    fix(undercoverci): enable branch covergae

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,6 +4,7 @@ SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start do
   add_filter(/^\/spec\//) # For RSpec, use `test` for MiniTest
+  enable_coverage(:branch)
 end
 
 Rails.application.configure do
