82c811373a29947989d88a760f8533313883e719
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-06-22 15:55:07 +0530

    feat(solagraph/yard/query_uploader): added yard docs for query

--- app/queries/projects_query.rb
+++ app/queries/projects_query.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class ProjectsQuery < GenericQuery
+  # @return [ActiveRecord::Relation]
   attr_reader :relation
 
+  # @param [Hash] query_params
+  # @param [ActiveRecord::Relation] relation
   def initialize(query_params, relation = Project.all)
     super query_params, relation
   end
--- app/queries/users_query.rb
+++ app/queries/users_query.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class UsersQuery < GenericQuery
+  # @return [ActiveRecord::Relation]
   attr_reader :relation
 
+  # @param [Hash] query_params
+  # @param [ActiveRecord::Relation] relation
   def initialize(query_params, relation = User.all)
     super query_params, relation
   end
