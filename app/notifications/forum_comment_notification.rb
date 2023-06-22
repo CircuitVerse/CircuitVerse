55718b52a19ceff3e3f98ff6ad41ff070c41af07
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-06-22 14:46:32 +0530

    feat(solagraph/yard/notification): added yard docs for notification

--- app/notifications/forum_comment_notification.rb
+++ app/notifications/forum_comment_notification.rb
@@ -3,13 +3,16 @@
 class ForumCommentNotification < Noticed::Base
   deliver_by :database, association: :noticed_notifications
 
+  # @return [String] returns the message to be displayed
   def message
+    # @type [User]
     user = params[:user]
     # post = params[:forum_post]
     thread = params[:forum_thread]
     t("users.notifications.comment_notif", user: user.name, thread: thread.title) # , mssg: post.body.truncate_words(4))
   end
 
+  # @return [String] returns the fontawesome icon for the forum comment notification
   def icon
     "far fa-comment"
   end
