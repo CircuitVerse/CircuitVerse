265f258d778e59e8c823201d60cd155bc52607cd
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-06-22 15:20:04 +0530

    feat(solagraph/yard/job_component_decorator): added yard docs for decorator

--- app/decorators/assignment_decorator.rb
+++ app/decorators/assignment_decorator.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 class AssignmentDecorator < SimpleDelegator
+  # @return [Assignment]
   def assignment
     __getobj__
   end
 
+  # @return [String] Text of grade
   def graded
     assignment.graded? ? "Graded(#{assignment.grading_scale})" : "Not Graded"
   end
 
+  # @return [String] Grade scale text
   def grading_scale_str
     case assignment.grading_scale
     when "letter"
@@ -22,6 +25,7 @@ class AssignmentDecorator < SimpleDelegator
     end
   end
 
+  # @return [String] Restricted elements JSON string
   def restricted_circuit_elements
     restricted_elements_str = JSON.parse(assignment.restrictions).reduce("") do |str, element|
       str += "#{element}, "
@@ -31,14 +35,17 @@ class AssignmentDecorator < SimpleDelegator
     restricted_elements_str.present? ? restricted_elements_str.slice(0..-3) : "None"
   end
 
+  # @return [String] Restricted features string
   def restricted_features
     assignment.feature_restrictions.presence || "None"
   end
 
+  # @return [Boolean] True if assignment is closed
   def closed?
     assignment.status == "closed"
   end
 
+  # @return [String] Time remaining for assignment
   def time_remaining
     str = ""
     str += "#{(assignment.deadline.to_i - Time.current.to_i) / 1.day} days "
