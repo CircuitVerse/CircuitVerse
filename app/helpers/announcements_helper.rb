7e1528ad34b2d5a1a3273bd8aef18b819bb430ed
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-06-22 15:15:30 +0530

    feat(solagraph/yard/helper): added yard docs for helper

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module AnnouncementsHelper
+  # @return [Announcement] Current announcement
   def current_announcement
     Announcement.current
   end
--- app/helpers/sorting_helper.rb
+++ app/helpers/sorting_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 module SortingHelper
+  # @param sort [String] the sort string specifying the fields to sort
+  # @param allowed [Array<String>] the allowed fields for sorting
+  # @return [Hash] a hash representing the ordered fields with their sort direction
   def self.sort_fields(sort, allowed)
     allowed = allowed.map(&:to_s)
     fields = sort.to_s.split(",")
@@ -8,6 +11,8 @@ module SortingHelper
     ordered_fields.select { |key, _value| allowed.include?(key) }
   end
 
+  # @param fields [Array<String>] the fields to sort
+  # @return [Hash] a hash representing the ordered fields with their sort direction
   def self.ordered_hash(fields)
     fields.each_with_object({}) do |field, hash|
       if field.start_with?("-")
