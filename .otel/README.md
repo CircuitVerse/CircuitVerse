5d6f21ac541f3466d492a08e25f0d001700b25a3
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-06-27 17:51:36 +0530

    feat: distributed tracing using otel
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- .otel/README.md
+++ .otel/README.md
@@ -3,3 +3,15 @@
 Documentation for Opentelemetry
 
 
+Export `NEW_RELIC_API_KEY` to shell profile.
+
+```bash
+export NEW_RELIC_API_KEY= 
+docker compose up -d
+```
+
+```bash
+
+```
+
+Additionally can add multiple exporters
--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -6,5 +6,19 @@ require "opentelemetry/instrumentation/all"
 
 OpenTelemetry::SDK.configure do |c|
   c.service_name = "CircuitVerse"
-  c.use_all()
+  # c.use_all
+  c.use "OpenTelemetry::Instrumentation::Rails"
+  c.use "OpenTelemetry::Instrumentation::ActiveSupport"
+  c.use "OpenTelemetry::Instrumentation::Rack"
+  c.use "OpenTelemetry::Instrumentation::ActionPack"
+  c.use "OpenTelemetry::Instrumentation::ActiveJob"
+  c.use "OpenTelemetry::Instrumentation::ActionView"
+  c.use "OpenTelemetry::Instrumentation::ActiveRecord"
+  c.use "OpenTelemetry::Instrumentation::AwsSdk"
+  c.use "OpenTelemetry::Instrumentation::HTTP"
+  c.use "OpenTelemetry::Instrumentation::ConcurrentRuby"
+  c.use "OpenTelemetry::Instrumentation::Faraday"
+  c.use "OpenTelemetry::Instrumentation::Net::HTTP"
+  c.use "OpenTelemetry::Instrumentation::PG"
+  c.use "OpenTelemetry::Instrumentation::Redis"
 end
