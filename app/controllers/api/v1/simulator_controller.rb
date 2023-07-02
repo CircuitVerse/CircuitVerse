2575ff62fbf6d7a0ef3dcd6f299fb76f19addf96
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-07-02 22:59:23 +0530

    feat(gsoc): update post_issue add check if url is missing and fix lint
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- app/controllers/api/v1/simulator_controller.rb
+++ app/controllers/api/v1/simulator_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/ClassLength
 class Api::V1::SimulatorController < Api::V1::BaseController
   def post_issue
     check_required_params
@@ -10,20 +9,24 @@ class Api::V1::SimulatorController < Api::V1::BaseController
     text = "#{params[:text]}\nCircuit Data: #{circuit_data_url}"
 
     url = ENV.fetch("SLACK_ISSUE_HOOK_URL", nil)
-    response = HTTP.post(url, json: { text: text })
-    unless response.code == 200
-      render json: { error: 'Failed to submit issue to Slack' }, status: :unprocessable_entity and return
+
+    if url.nil? || !url.start_with?("http://", "https://")
+      render json: { error: 'Invalid or missing Slack webhook URL' }, status: :internal_server_error and return
     end
 
-    render json: { success: true, message: 'Issue submitted successfully' }, status: :ok
+    response = HTTP.post(url, json: { text: text })
+    unless response.code == 200
+      render json: { error: 'Failed to submit issue to Slack' }, status: :internal_server_error and return
+    end
+
+    render json: { success: true, message: "Issue submitted successfully" }, status: :ok
   end
 
   private
 
     def check_required_params
-      unless params[:text] && params[:circuit_data]
-        render json: { error: 'Missing required parameters' }, status: :unprocessable_entity and return
-      end
+      return if params[:text] && params[:circuit_data]
+      render json: { error: 'Missing required parameters' }, status: :unprocessable_entity and return
     end
 
     def create_issue_circuit_data
@@ -33,5 +36,5 @@ class Api::V1::SimulatorController < Api::V1::BaseController
         render(json: { error: error_message }, status: :unprocessable_entity) and return
       end
       issue_circuit_data
-    end    
+    end
 end
