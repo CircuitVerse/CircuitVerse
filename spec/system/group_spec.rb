cee0ab67491c49ced767c2c1d9e0e3943f4bbe18
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-07-02 19:56:54 +0530

    feat: integration tests added for group management

--- spec/system/group_spec.rb
+++ spec/system/group_spec.rb
@@ -6,6 +6,7 @@ describe "Group management", type: :system do
   before do
     @user = FactoryBot.create(:user)
     @user2 = FactoryBot.create(:user)
+    @user3 = FactoryBot.create(:user)
     @group = FactoryBot.create(:group, primary_mentor: @user)
     driven_by(:selenium_chrome_headless)
     login_as(@user, scope: :user)
@@ -66,5 +67,68 @@ describe "Group management", type: :system do
     click_on "Save"
 
     expect(page).to have_text("Group was successfully updated.")
+  end
+
+  it "deletes the group" do
+    visit user_groups_path(@user)
+    click_on "Delete"
+    button = find(id: 'groups-group-delete-button')
+    button.click
+
+    expect(page).to have_text("Group was successfully deleted.")
+  end
+
+  it "add secondary mentor" do
+    visit "/groups/#{@group.id}"
+    click_button "+ Add Mentors"
+    execute_script "document.getElementById('add-mentor-to-modal').style.display='block'"
+    execute_script "document.getElementById('add-mentor-to-modal').style.opacity=1"
+    execute_script "var new_email = document.createElement('option')
+                    new_email.innerHTML = '" + @user3.email + "'
+                    document.getElementById('group_mentor_emails').appendChild(new_email)"
+    select @user3.email, from: "group_member[emails][]"
+    execute_script "document.getElementById('group_email_input_mentor').click()"
+    click_button "Add mentors"
+
+    expect(page).to have_text(
+      "Out of 1 Email(s), 1 was valid and 0 were invalid. 1 user(s) will be invited."
+    )
+  end
+
+  it "remove secondary mentor" do
+    GroupMember.create(group_id: @group.id, user_id: @user3.id, mentor: true)
+    visit "/groups/#{@group.id}"
+    click_on "Remove"
+    execute_script "document.getElementById('deletememberModal').style.display='block'"
+    execute_script "document.getElementById('deletememberModal').style.opacity=1"
+    click_on "Delete"
+
+    expect(page).to have_text("Group member was successfully removed.")
+  end
+
+  it "convert member to mentor" do
+    GroupMember.create(group_id: @group.id, user_id: @user2.id, mentor: false)
+    visit "/groups/#{@group.id}"
+    make_mentor_btn = find("a[data-target='#promote-member-modal']")
+    make_mentor_btn.click
+    execute_script "document.getElementById('promote-member-modal').style.display='block'"
+    execute_script "document.getElementById('promote-member-modal').style.opacity=1"
+    make_mentor_btn = find(id: "groups-member-promote-button")
+    make_mentor_btn.click
+
+    expect(page).to have_text("Group member was successfully updated.")
+  end
+
+  it "convert mentor to member" do
+    GroupMember.create(group_id: @group.id, user_id: @user3.id, mentor: true)
+    visit "/groups/#{@group.id}"
+    make_mentor_btn = find("a[data-target='#demote-member-modal']")
+    make_mentor_btn.click
+    execute_script "document.getElementById('demote-member-modal').style.display='block'"
+    execute_script "document.getElementById('demote-member-modal').style.opacity=1"
+    make_mentor_btn = find(id: "groups-member-demote-button")
+    make_mentor_btn.click
+
+    expect(page).to have_text("Group member was successfully updated.")
   end
 end
