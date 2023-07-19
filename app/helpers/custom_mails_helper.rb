e800fbab2bc127072d8d5fa65f346a485d69d81c
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-07-19 20:03:13 +0530

    feat: add yard docs for helpers

--- app/helpers/custom_mails_helper.rb
+++ app/helpers/custom_mails_helper.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 module CustomMailsHelper
+  # @type [Integer]
   BATCH_SIZE = 1000
 
+  # @param [CustomMail] mail
   def send_mail_in_batches(mail)
     User.subscribed.find_each(batch_size: BATCH_SIZE) do |user|
       UserMailer.custom_email(user, mail).deliver_later
     end
   end
 
+  # @param [User] user
+  # @param [CustomMail] mail
   def send_mail_to_self(user, mail)
     UserMailer.custom_email(user, mail).deliver_later
   end
--- app/helpers/lti_helper.rb
+++ app/helpers/lti_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module LtiHelper
+  # @return [String] LTI Integration Tutorial Link
   def lms_integration_tutorial
     # link to the tutorial of linking cv assignment with LMS
     "https://docs.circuitverse.org/#/chapter2/3lmsintegration"
