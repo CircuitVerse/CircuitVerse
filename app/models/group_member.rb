ca431f1e4bf5ce2a071828350bde5dc207e462cb
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-07-19 19:45:37 +0530

    feat: add yard docs for models

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+#
+# == Schema Information
+#
+# Table name: group_members
+#
+#  id         :bigint           not null, primary key
+#  group_id   :bigint
+#  user_id    :bigint
+#  mentor     :boolean          default(FALSE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_group_members_on_group_id  (group_id)
+#  index_group_members_on_user_id   (user_id)
+#  index_group_members_on_group_id_and_user_id  (group_id,user_id) UNIQUE
+#
+
 class GroupMember < ApplicationRecord
   belongs_to :group, counter_cache: true
   belongs_to :user
