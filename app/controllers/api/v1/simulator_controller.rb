ee39964958cd0f7c682f1b19711973eb9efc10a9
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-07-30 12:56:31 +0530

    chore(lint): fix linting issues
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- app/controllers/api/v1/simulator_controller.rb
+++ app/controllers/api/v1/simulator_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::SimulatorController < Api::V1::BaseController
-  #POST /api/v1/simulator/post_issue
+  # POST /api/v1/simulator/post_issue
   def post_issue
     issue_circuit_data = IssueCircuitDatum.new(data: params[:circuit_data]).tap(&:save)
 
@@ -22,7 +22,7 @@ class Api::V1::SimulatorController < Api::V1::BaseController
     render json: { success: true, message: "Issue submitted successfully" }, status: :ok
   end
 
-  #POST /api/v1/simulator/verilogcv
+  # POST /api/v1/simulator/verilogcv
   def verilog_cv
     url = "#{ENV.fetch('YOSYS_PATH', 'http://127.0.0.1:3040')}/getJSON"
     response = HTTP.post(url, json: { code: params[:code] })
