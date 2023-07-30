e4d5784840a093b586409bdb0eb7f62d2b9e8739
Arnabdaz <arnab-das@outlook.com>
2023-07-30 09:13:24 +0000

    chore(lint): fix linting
    Signed-off-by: Arnabdaz <arnab-das@outlook.com>

--- spec/requests/api/v1/simulator_controller/verilog_cv_spec.rb
+++ spec/requests/api/v1/simulator_controller/verilog_cv_spec.rb
@@ -4,14 +4,15 @@ require "rails_helper"
 
 RSpec.describe Api::V1::SimulatorController, type: :request do
   describe "POST /api/v1/simulator/verilogcv" do
-    let(:code) { 'sample_code' }
+    let(:code) { "sample_code" }
     let(:yosys_url) { "#{ENV.fetch('YOSYS_PATH', 'http://127.0.0.1:3040')}/getJSON" }
-    let(:yosys_response) { { data: 'response_from_yosys' } }
+    let(:yosys_response) { { "data" => "response_from_yosys" } }
 
     context "when YOSYS_PATH is valid and returns a successful response" do
       before do
-        allow(ENV).to receive(:fetch).with('YOSYS_PATH', 'http://127.0.0.1:3040').and_return('http://127.0.0.1:3040')
-        allow(HTTP).to receive(:post).and_return(OpenStruct.new(code: 200, to_s: yosys_response.to_json))
+        allow(ENV).to receive(:fetch).with("YOSYS_PATH", "http://127.0.0.1:3040").and_return("http://127.0.0.1:3040")
+        response_double = instance_double("HTTP::Response", code: 200, to_s: yosys_response.to_json)
+        allow(HTTP).to receive(:post).and_return(response_double)
       end
 
       it "returns a successful response with correct JSON" do
@@ -23,8 +24,9 @@ RSpec.describe Api::V1::SimulatorController, type: :request do
 
     context "when YOSYS_PATH is valid but returns a failed response" do
       before do
-        allow(ENV).to receive(:fetch).with('YOSYS_PATH', 'http://127.0.0.1:3040').and_return('http://127.0.0.1:3040')
-        allow(HTTP).to receive(:post).and_return(OpenStruct.new(code: 500, to_s: ""))
+        allow(ENV).to receive(:fetch).with("YOSYS_PATH", "http://127.0.0.1:3040").and_return("http://127.0.0.1:3040")
+        response_double = instance_double("HTTP::Response", code: 500, to_s: "")
+        allow(HTTP).to receive(:post).and_return(response_double)
       end
 
       it "returns the failed status code" do
