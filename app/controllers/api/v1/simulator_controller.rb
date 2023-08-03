5a572a3402da9b3819f7b1cda6f361d8f34b79d4
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-08-04 01:16:30 +0530

    chore: update status code
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- app/controllers/api/v1/simulator_controller.rb
+++ app/controllers/api/v1/simulator_controller.rb
@@ -11,12 +11,12 @@ class Api::V1::SimulatorController < Api::V1::BaseController
     url = ENV.fetch("SLACK_ISSUE_HOOK_URL", nil)
 
     if url.nil? || !url.start_with?("http://", "https://")
-      render json: { error: "Invalid or missing Slack webhook URL" }, status: :internal_server_error and return
+      render json: { error: "Invalid or missing Slack webhook URL" }, status: 422 and return
     end
 
     response = HTTP.post(url, json: { text: text })
     unless response.code == 200
-      render json: { error: "Failed to submit issue to Slack" }, status: :internal_server_error and return
+      render json: { error: "Failed to submit issue to Slack" }, status: 422 and return
     end
 
     render json: { success: true, message: "Issue submitted successfully" }, status: :ok
