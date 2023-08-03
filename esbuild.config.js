cbb6c462f03396f08d00912f9d8357a2975b0309
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-08-04 00:49:28 +0530

    feat(gsoc): add auto building of vue simualtor with bin/dev
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const fs = require('fs');
 const esbuild = require('esbuild');
 const rails = require('esbuild-rails');
 const sassPlugin = require('esbuild-plugin-sass');
+const { execSync } = require('child_process');
 
 const watchDirectories = [
     './app/javascript/**/*.js',
@@ -10,6 +12,7 @@ const watchDirectories = [
 ];
 
 const watch = process.argv.includes('--watch');
+const rebuildVueSimulator = process.env.REBUILD_VUE === 'true';
 
 const watchPlugin = {
     name: 'watchPlugin',
@@ -30,6 +33,55 @@ const watchPlugin = {
     },
 };
 
+function updateGitSubmodule() {
+    execSync('git submodule update --init --remote', { cwd: process.cwd() });
+}
+
+function validatePackageJsonAndLock() {
+    const packageJsonPath = path.join(process.cwd(), 'cv-frontend-vue', 'package.json');
+    const packageLockJsonPath = path.join(process.cwd(), 'cv-frontend-vue', 'package-lock.json');
+
+    if (
+        !fs.existsSync(packageJsonPath) || !fs.existsSync(packageLockJsonPath)
+    ) {
+        throw new Error('package.json or package-lock.json is not found inside submodule directory');
+    }
+}
+
+function installAndBuildPackage() {
+    const vueDir = path.join(process.cwd(), 'cv-frontend-vue');
+    execSync('npm install', { cwd: vueDir });
+    execSync(rebuildVueSimulator ? 'npm run build -- --watch' : 'npm run build', {
+        cwd: vueDir,
+    });
+}
+
+function logErrorAndExit(err) {
+    // eslint-disable-next-line no-console
+    console.error(`Error building Vue simulator: ${new Date(Date.now()).toLocaleString()}\n\n${err}`);
+    process.exit(1);
+}
+
+async function buildVue() {
+    try {
+        updateGitSubmodule();
+        validatePackageJsonAndLock();
+        installAndBuildPackage();
+    } catch (err) {
+        logErrorAndExit(err);
+    }
+}
+
+const vuePlugin = {
+    name: 'vuePlugin',
+    setup(build) {
+        build.onStart(() => {
+            // eslint-disable-next-line no-console
+            console.log(`Building Vue site: ${new Date(Date.now()).toLocaleString()}`);
+        });
+    },
+};
+
 async function run() {
     const context = await esbuild.context({
         entryPoints: ['application.js', 'testbench.js'],
@@ -38,9 +90,14 @@ async function run() {
         absWorkingDir: path.join(process.cwd(), 'app/javascript'),
         sourcemap: 'inline',
         loader: {
-            '.png': 'file', '.svg': 'file', '.ttf': 'file', '.woff': 'file', '.woff2': 'file', '.eot': 'file',
+            '.png': 'file',
+            '.svg': 'file',
+            '.ttf': 'file',
+            '.woff': 'file',
+            '.woff2': 'file',
+            '.eot': 'file',
         },
-        plugins: [rails(), sassPlugin(), watchPlugin],
+        plugins: [rails(), sassPlugin(), vuePlugin, watchPlugin],
     });
 
     if (watch) {
@@ -51,6 +108,7 @@ async function run() {
     }
 }
 
+buildVue();
 run().catch(() => {
     process.exit(1);
 });
