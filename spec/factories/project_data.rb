152b013b703f093e0cf491abc089846f3fc5eac1
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-08-04 01:17:14 +0530

    Update spec/factories/project_data.rb
    Co-authored-by: Aboobacker MK <aboobackervyd@gmail.com>

--- spec/factories/project_data.rb
+++ spec/factories/project_data.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :project_datum, class: "ProjectDatum" do
-    data { { name: "circuit data" }.to_json }
+    data { { name: "circuit data" }}
     project
   end
 end
