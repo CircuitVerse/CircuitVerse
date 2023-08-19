2db7cd414d4cc6c1bb7aa71f010fde091d86ea87
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-08-20 02:52:51 +0530

    chore: disable generating spans for default settings
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-require "opentelemetry/sdk"
-require "opentelemetry/exporter/otlp"
-require "opentelemetry/instrumentation/all"
+if Flipper.enabled?(:opentelemetry)
+  require "opentelemetry/sdk"
+  require "opentelemetry/exporter/otlp"
+  require "opentelemetry/instrumentation/all"
 
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = "CircuitVerse"
-  # c.use_all
-  c.use "OpenTelemetry::Instrumentation::Rails"
-  c.use "OpenTelemetry::Instrumentation::ActiveSupport"
-  c.use "OpenTelemetry::Instrumentation::Rack"
-  c.use "OpenTelemetry::Instrumentation::ActionPack"
-  c.use "OpenTelemetry::Instrumentation::ActiveJob"
-  c.use "OpenTelemetry::Instrumentation::ActionView"
-  c.use "OpenTelemetry::Instrumentation::ActiveRecord"
-  c.use "OpenTelemetry::Instrumentation::AwsSdk"
-  c.use "OpenTelemetry::Instrumentation::HTTP"
-  c.use "OpenTelemetry::Instrumentation::ConcurrentRuby"
-  c.use "OpenTelemetry::Instrumentation::Faraday"
-  c.use "OpenTelemetry::Instrumentation::Net::HTTP"
-  c.use "OpenTelemetry::Instrumentation::PG"
-  c.use "OpenTelemetry::Instrumentation::Redis"
+  OpenTelemetry::SDK.configure do |c|
+    c.service_name = "CircuitVerse"
+    # c.use_all
+    c.use "OpenTelemetry::Instrumentation::Rails"
+    c.use "OpenTelemetry::Instrumentation::ActiveSupport"
+    c.use "OpenTelemetry::Instrumentation::Rack"
+    c.use "OpenTelemetry::Instrumentation::ActionPack"
+    c.use "OpenTelemetry::Instrumentation::ActiveJob"
+    c.use "OpenTelemetry::Instrumentation::ActionView"
+    c.use "OpenTelemetry::Instrumentation::ActiveRecord"
+    c.use "OpenTelemetry::Instrumentation::AwsSdk"
+    c.use "OpenTelemetry::Instrumentation::HTTP"
+    c.use "OpenTelemetry::Instrumentation::ConcurrentRuby"
+    c.use "OpenTelemetry::Instrumentation::Faraday"
+    c.use "OpenTelemetry::Instrumentation::Net::HTTP"
+    c.use "OpenTelemetry::Instrumentation::PG"
+    c.use "OpenTelemetry::Instrumentation::Redis"
+  end
 end
