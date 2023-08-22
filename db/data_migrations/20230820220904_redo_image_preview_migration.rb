f4b7fb311b5011d58e404de105be0166ef2b1463
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-08-23 01:57:50 +0530

    chore: optimise migration pluck from activerecord in batch of 100
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- db/data_migrations/20230820220904_redo_image_preview_migration.rb
+++ db/data_migrations/20230820220904_redo_image_preview_migration.rb
@@ -5,34 +5,33 @@ require "redis"
 
 class RedoImagePreviewMigration < ActiveRecord::DataMigration
   def up
-    log_file = Rails.root.join("log/redo_image_preview_migration.log")
-    custom_logger = Logger.new(log_file)
-    Rails.logger = custom_logger
-   latest_migrated_project_id = redis.get("latest_migrated_project_id").to_i
+    Rails.logger = Logger.new(Rails.root.join("log/redo_image_preview_migration.log"))
+    latest_migrated_project_id = redis.get("latest_migrated_project_id").to_i
     Rails.logger.info "Migrating from project_id #{latest_migrated_project_id + 1}"
     migrate_carrierwave_assets(latest_migrated_project_id - 1)
   end
 
   def migrate_carrierwave_assets(latest_migrated_project_id = 0)
-    Project.where("id > ?", latest_migrated_project_id).find_each do |project|
+    Project.where("id > ?", latest_migrated_project_id).find_each(batch_size: 100) do |project|
       next unless project.image_preview.file
       next if !project.image_preview.file.exists?
       next if project.circuit_preview.attached?
 
       begin
-        image_file = File.open(project.image_preview.path)
-        blob = ActiveStorage::Blob.create_and_upload!(
-          io: image_file,
-          filename: project.image_preview.identifier,
-          content_type: "image/jpeg"
-        )
-        project.circuit_preview.attach(blob)
-        image_file.close
+        File.open(project.image_preview.path) do |image_file|
+          blob = ActiveStorage::Blob.create_and_upload!(
+            io: image_file,
+            filename: project.image_preview.identifier,
+            content_type: "image/jpeg"
+          )
+          project.circuit_preview.attach(blob)
+        end
         Rails.logger.info "Finished migrating circuit_preview with project_id: #{project.id}"
         redis.set("latest_migrated_project_id", project.id)
       rescue Interrupt
         break
-      rescue StandardError
+      rescue StandardError => e
+        Rails.logger.error "Error migrating project_id #{project.id}: #{e.message}"
         next
       end
     end
