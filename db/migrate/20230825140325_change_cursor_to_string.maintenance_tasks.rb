1cc25fbbd479c4098fc2ec39af86243e914ce970
Vaibhav Upreti <85568177+VaibhavUpreti@users.noreply.github.com>
2023-08-27 22:47:27 +0530

    chore: mark maintenance tasks migrations safe (#4000)

--- db/migrate/20230825140325_change_cursor_to_string.maintenance_tasks.rb
+++ db/migrate/20230825140325_change_cursor_to_string.maintenance_tasks.rb
@@ -6,14 +6,18 @@ class ChangeCursorToString < ActiveRecord::Migration[6.0]
   # Ensure no Tasks are paused when this migration is deployed, or they will be resumed from the start.
   # Running tasks are able to gracefully handle this change, even if interrupted.
   def up
-    change_table(:maintenance_tasks_runs) do |t|
-      t.change(:cursor, :string)
-    end
+    safety_assured {
+      change_table(:maintenance_tasks_runs) do |t|
+        t.change(:cursor, :string)
+      end
+      }
   end
 
   def down
+    safety_assured {
     change_table(:maintenance_tasks_runs) do |t|
       t.change(:cursor, :bigint)
     end
+    }
   end
 end
