db00a1dc529507e61134492cf3b06d9ebb9619f7
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-09-03 18:13:35 +0530

    fix: use proxy url for assets for cdn caching
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- config/initializers/commontator.rb
+++ config/initializers/commontator.rb
@@ -23,7 +23,7 @@ Commontator.configure do |config|
   # through the view object (for example, view.flash)
   # However, the view does not include the main application's helpers
   # Default: ->(view) { '' }
-  config.javascript_proc = ->(view) { '' }
+  config.javascript_proc = ->(_view) { '' }
 
 
   # User (acts_as_commontator) Configuration
@@ -62,12 +62,13 @@ Commontator.configure do |config|
   # Default: ->(user, view) {
   #   view.commontator_gravatar_image_tag(user, 1, s: 60, d: 'mm')
   # }
-  config.user_avatar_proc = ->(user, view) {
+  config.user_avatar_proc = ->(user, _view) {
     # image_tag(user.profile_picture.url(:thumb), { alt: user.name,
     #                  title: user.name,
     #                  border: 1 })
     if user.profile_picture.attached?
-      return ActionController::Base.helpers.image_tag(user.profile_picture.url)
+      image_path = Rails.application.routes.url_helpers.rails_blob_url(user.profile_picture, only_path: true)
+      return ActionController::Base.helpers.image_tag(image_path)
     else
       return ActionController::Base.helpers.image_tag("thumb/Default.jpg")
     end
@@ -85,7 +86,7 @@ Commontator.configure do |config|
   # If the mailer argument is not nil, then Commontator intends to send an email to
   # the address returned; you can prevent it from being sent by returning a blank String
   # Default: ->(user, mailer) { user.try(:email) || '' }
-  config.user_email_proc = ->(user, mailer) { user.try(:email) || '' }
+  config.user_email_proc = ->(user, _mailer) { user.try(:email) || '' }
 
 
 
@@ -109,7 +110,7 @@ Commontator.configure do |config|
   # Returns: a Boolean, true if and only if the user should be allowed to read that thread
   # Note: can be called with a user object that is nil (if they are not logged in)
   # Default: ->(thread, user) { true } (anyone can read any thread)
-  config.thread_read_proc = ->(thread, user) { 
+  config.thread_read_proc = ->(thread, user) {
     return true if thread.commontable.public?
 
     ProjectPolicy.new(user, thread.commontable).check_view_access?
@@ -121,7 +122,7 @@ Commontator.configure do |config|
   # Returns: a Boolean, true if and only if the user is a moderator for that thread
   # If you want global moderators, make this proc true for them regardless of thread
   # Default: ->(thread, user) { false } (no moderators)
-  config.thread_moderator_proc = ->(thread, user) { user.admin? }
+  config.thread_moderator_proc = ->(_thread, user) { user.admin? }
 
   # comment_editing
   # Type: Symbol
@@ -174,7 +175,7 @@ Commontator.configure do |config|
   # pos is the number of likes, or the rating, or the reputation
   # neg is the number of dislikes, if applicable, or 0 otherwise
   # Default: ->(thread, pos, neg) { "%+d" % (pos - neg) }
-  config.vote_count_proc = ->(thread, pos, neg) { "%+d" % (pos - neg) }
+  config.vote_count_proc = ->(_thread, pos, neg) { "%+d" % (pos - neg) }
 
   # comment_order
   # Type: Symbol
@@ -234,7 +235,7 @@ Commontator.configure do |config|
   # Default: ->(thread) {
   #   "no-reply@#{Rails.application.class.parent.to_s.downcase}.com"
   # }
-  config.email_from_proc = ->(thread) {
+  config.email_from_proc = ->(_thread) {
     # "no-reply@#{Rails.application.class.parent.to_s.downcase}.com"
     return "#{ENV["CIRCUITVERSE_EMAIL_ID"]}"
   }
@@ -300,7 +301,7 @@ Commontator.configure do |config|
   # Default: ->(current_user, query) {
   #   current_user.class.where('username LIKE ?', "#{query}%")
   # }
-  config.user_mentions_proc = ->(current_user, thread, query) {
+  config.user_mentions_proc = ->(current_user, _thread, query) {
     current_user.class.where('name LIKE ?', "#{query}%")
   }
 end
