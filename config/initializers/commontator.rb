85d297c2a50dc91e2aa6530f6028e485a05544af
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-09-08 03:15:25 +0530

    chore: remove lint changes
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- config/initializers/commontator.rb
+++ config/initializers/commontator.rb
@@ -23,7 +23,7 @@ Commontator.configure do |config|
   # through the view object (for example, view.flash)
   # However, the view does not include the main application's helpers
   # Default: ->(view) { '' }
-  config.javascript_proc = ->(_view) { '' }
+  config.javascript_proc = ->(view) { '' }
 
 
   # User (acts_as_commontator) Configuration
@@ -62,7 +62,7 @@ Commontator.configure do |config|
   # Default: ->(user, view) {
   #   view.commontator_gravatar_image_tag(user, 1, s: 60, d: 'mm')
   # }
-  config.user_avatar_proc = ->(user, _view) {
+  config.user_avatar_proc = ->(user, view) {
     # image_tag(user.profile_picture.url(:thumb), { alt: user.name,
     #                  title: user.name,
     #                  border: 1 })
@@ -86,7 +86,7 @@ Commontator.configure do |config|
   # If the mailer argument is not nil, then Commontator intends to send an email to
   # the address returned; you can prevent it from being sent by returning a blank String
   # Default: ->(user, mailer) { user.try(:email) || '' }
-  config.user_email_proc = ->(user, _mailer) { user.try(:email) || '' }
+  config.user_email_proc = ->(user, mailer) { user.try(:email) || '' }
 
 
 
@@ -122,7 +122,7 @@ Commontator.configure do |config|
   # Returns: a Boolean, true if and only if the user is a moderator for that thread
   # If you want global moderators, make this proc true for them regardless of thread
   # Default: ->(thread, user) { false } (no moderators)
-  config.thread_moderator_proc = ->(_thread, user) { user.admin? }
+  config.thread_moderator_proc = ->(thread, user) { user.admin? }
 
   # comment_editing
   # Type: Symbol
@@ -175,7 +175,7 @@ Commontator.configure do |config|
   # pos is the number of likes, or the rating, or the reputation
   # neg is the number of dislikes, if applicable, or 0 otherwise
   # Default: ->(thread, pos, neg) { "%+d" % (pos - neg) }
-  config.vote_count_proc = ->(_thread, pos, neg) { "%+d" % (pos - neg) }
+  config.vote_count_proc = ->(thread, pos, neg) { "%+d" % (pos - neg) }
 
   # comment_order
   # Type: Symbol
@@ -235,7 +235,7 @@ Commontator.configure do |config|
   # Default: ->(thread) {
   #   "no-reply@#{Rails.application.class.parent.to_s.downcase}.com"
   # }
-  config.email_from_proc = ->(_thread) {
+  config.email_from_proc = ->(thread) {
     # "no-reply@#{Rails.application.class.parent.to_s.downcase}.com"
     return "#{ENV["CIRCUITVERSE_EMAIL_ID"]}"
   }
@@ -301,7 +301,7 @@ Commontator.configure do |config|
   # Default: ->(current_user, query) {
   #   current_user.class.where('username LIKE ?', "#{query}%")
   # }
-  config.user_mentions_proc = ->(current_user, _thread, query) {
+  config.user_mentions_proc = ->(current_user, thread, query) {
     current_user.class.where('name LIKE ?', "#{query}%")
   }
 end
