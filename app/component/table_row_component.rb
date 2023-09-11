62c978a5f9b084ceec2c680bd8888c0fbfacecbc
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2023-09-11 10:01:49 +0000

    feat: refactoring code to resolve conversation

--- app/component/table_row_component.rb
+++ app/component/table_row_component.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class TableRowComponent < ViewComponent::Base
-  def initialize(assignment, current_user, policy, deadline_time, group)
+  def initialize(assignment, current_user, group_and_policy, deadline_time)
     super
     @assignment = assignment
     @current_user = current_user
-    @policy = policy
+    @policy = group_and_policy[:policy]
     @deadline_minute = deadline_time[:minute]
     @deadline_day = deadline_time[:day]
     @deadline_year = deadline_time[:year]
     @deadline_second = deadline_time[:second]
     @deadline_hour = deadline_time[:hour]
     @deadline_month = deadline_time[:month]
-    @group = group
+    @group = group_and_policy[:group]
   end
 end
