636fed381a891c3db17fb38e7ba9194b977a4931
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2023-09-28 07:38:16 +0000

    fix: resolve conversations

--- spec/components/table_row_component_spec.rb
+++ spec/components/table_row_component_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe TableRowComponent, type: :component do
+RSpec.describe Table_row::TableRowComponent, type: :component do
   pending "add some examples to (or delete) #{__FILE__}"
 
   # it "renders something useful" do
