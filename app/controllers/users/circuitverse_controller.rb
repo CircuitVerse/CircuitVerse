84b9b8598b8b023ad220a49488ec34a235a26262
rajat mohan <77490034+rajatmohan22@users.noreply.github.com>
2023-10-05 11:17:04 +0530

    Update circuitverse_controller.rb

--- app/controllers/users/circuitverse_controller.rb
+++ app/controllers/users/circuitverse_controller.rb
@@ -44,13 +44,15 @@ class Users::CircuitverseController < ApplicationController
 
   private
 
-    def profile_params
-      params.require(: user).permit(: name,: profile_picture,: country,: subscribed,: locale,: remove_picture,: avatar,: vuesim).tap do |whitelisted|
-      educational_institute = params[: user][: educational_institute].strip
-      whitelisted[: educational_institute] = educational_institute.present || "Not Entered"
-      end
+  def profile_params
+    params.require(:user).permit(
+      :name, :profile_picture, :country, :subscribed, :locale, :remove_picture, :avatar, :vuesim
+    ).tap do |whitelisted|
+      educational_institute = params[:user][:educational_institute].strip
+      whitelisted[:educational_institute] = educational_institute.present? || "Not Entered"
     end
-
+  end
+  
     def set_user
       @profile = current_user
       @user = User.find(params[:id])
