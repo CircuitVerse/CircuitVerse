86ad1b52c7fceedb9af1ffd470e5849466782b41
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2023-11-07 01:12:34 +0000

    fix: enabling signup for test cases

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  Flipper.enable :signup
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Alphanumeric.alphanumeric number: 10 }
