982a5cc278162b7d52fe8b3fb6e3d28ca984ab3c
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2023-11-08 12:02:04 +0000

    fix: trailing space

--- spec/controllers/users_registrations_controller_spec.rb
+++ spec/controllers/users_registrations_controller_spec.rb
@@ -73,7 +73,7 @@ RSpec.describe Users::RegistrationsController, type: :controller do
     before do
       allow(Flipper).to receive(:enabled?).with(:signup).and_return(false)
     end
-  
+
     it "redirects to login page with an alert message" do
       post :create, params: valid_attributes
       expect(response).to redirect_to(new_user_session_path)
