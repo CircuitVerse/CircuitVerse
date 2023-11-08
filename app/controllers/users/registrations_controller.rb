debf43f721c415b52cc91fe90d644bfc4837a517
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2023-11-08 10:01:48 +0000

    revert: previous commit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -64,10 +64,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
     def check_captcha
-      self.resource = resource_class.new sign_up_params
-      resource.validate # Look for any other validation errors besides reCAPTCHA
-      set_minimum_password_length
-      respond_with_navigational(resource) { render :new }
+      if Flipper.enabled?(:recaptcha) && !verify_recaptcha
+        self.resource = resource_class.new sign_up_params
+        resource.validate # Look for any other validation errors besides reCAPTCHA
+        set_minimum_password_length
+        respond_with_navigational(resource) { render :new }
+      end
     end
 
     # If you have extra params to permit, append them to the sanitizer.
