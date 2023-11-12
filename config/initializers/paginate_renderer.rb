adb24b74cb67a7c8e0a022b0da078b0dc2749f83
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2023-11-12 21:08:48 +0530

    fix: resolving conversations

--- config/initializers/paginate_renderer.rb
+++ config/initializers/paginate_renderer.rb
@@ -19,12 +19,12 @@ class PaginateRenderer < WillPaginate::ActionView::LinkRenderer
   end
 
   def previous_page
-    num = @collection.current_page > 1 && (@collection.current_page - 1)
+    num = @collection.current_page > 1 && @collection.current_page - 1
     previous_or_next_page(num, @options[:previous_label], 'page-link')
   end
 
   def next_page
-    num = @collection.current_page < total_pages && (@collection.current_page + 1)
+    num = @collection.current_page < total_pages && @collection.current_page + 1
     previous_or_next_page(num, @options[:next_label], 'page-link')
   end
 
