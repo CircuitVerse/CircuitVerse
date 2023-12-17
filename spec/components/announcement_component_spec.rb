f8d3c1e1f0c8dc8c4fd8c3c766cbea149897dc6d
Harsh <102225113+senbo1@users.noreply.github.com>
2023-12-17 11:33:25 +0530

    Update announcement_component_spec.rb

--- spec/components/announcement_component_spec.rb
+++ spec/components/announcement_component_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe AnnouncementBox::AnnouncementComponent, type: :component do
   end
 
   it "does not render the announcement when outside the date range" do
-    start_date = Date.current + - 2
+    start_date = Date.current - 2
     end_date = Date.current - 1
     current_announcement = instance_double(Announcement, start_date: start_date, end_date: end_date,
                                                          body: "Expired announcement", link?: false)
