092d9131d33d7dba505344629742c81a64152dca
fillingtothemomo <rockingpenny4@gmail.com>
2023-12-29 00:56:18 +0530

    fix(comments): makes the urls clickable

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ProjectsHelper
+  def auto_link_comments(comment)
+        comment.body.gsub(URI::DEFAULT_PARSER.make_regexp, '<a href="\0" target="_blank">\0</a>').html_safe
+  end
 end
