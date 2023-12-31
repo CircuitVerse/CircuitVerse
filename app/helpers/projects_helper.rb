ed31fd71b458017590c2c71f1da8debbe3aa41d5
fillingtothemomo <rockingpenny4@gmail.com>
2023-12-31 21:59:24 +0530

    fix(comments): used built in auto_link method

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
 module ProjectsHelper
-  def auto_link_comments(comment)
-    escaped_body = h(comment.body)
-    sanitized_body = sanitize(escaped_body)
-    auto_linked_body = sanitized_body.gsub(URI::DEFAULT_PARSER.make_regexp, '<a href="\0" target="_blank">\0</a>')
-    raw(auto_linked_body)
-  end
 end
-
-
-
-
-
-
-
