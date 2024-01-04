6ddd6703a18e77b76b652a6d9b09e131fe14fd2c
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2024-01-04 20:25:51 +0530

    Update project_card_component_spec.rb

--- spec/components/project_card_component_spec.rb
+++ spec/components/project_card_component_spec.rb
@@ -3,12 +3,29 @@
 require "rails_helper"
 
 RSpec.describe ProjectCard::ProjectCardComponent, type: :component do
+  include Pundit::Authorization
+  include Devise::Controllers::Helpers
+
   before do
     @author = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, author: @author)
+    @project = FactoryBot.create(:project, author: @author, project_access_type: "Public")
+    @current_user = FactoryBot.create(:user)
+
+    render_inline(described_class.new(@project, @author))
   end
 
   it "renders the project card correctly" do
-    render_inline(described_class.new(@project, @author))
+    expect(page).to have_text(@project.name)
+    expect(page).to have_text(I18n.t("view"))
+    expect(page).to have_text(I18n.t("more"))
+    expect(page).to have_text(I18n.t("launch"))
+  end
+
+  it "renders the project card for user with no access" do
+    allow(policy(@project)).to receive(:user_access?).and_return(false)
+
+    render_inline(described_class.new(@project, @current_user))
+
+    expect(page).to have_text(I18n.t("fork"))
   end
 end
