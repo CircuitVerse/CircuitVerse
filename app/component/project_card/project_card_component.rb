d92af6fec096283c3b48059c1f68826f71d1b945
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2024-01-04 20:26:35 +0530

    fix: wrong placement

--- app/component/project_card/project_card_component.rb
+++ app/component/project_card/project_card_component.rb
@@ -1,31 +1,14 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+module ProjectCard
+  class ProjectCardComponent < ViewComponent::Base
+    include Pundit::Authorization
+    include Devise::Controllers::Helpers
 
-RSpec.describe ProjectCard::ProjectCardComponent, type: :component do
-  include Pundit::Authorization
-  include Devise::Controllers::Helpers
-
-  before do
-    @author = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, author: @author, project_access_type: "Public")
-    @current_user = FactoryBot.create(:user)
-
-    render_inline(described_class.new(@project, @author))
-  end
-
-  it "renders the project card correctly" do
-    expect(page).to have_text(@project.name)
-    expect(page).to have_text(I18n.t("view"))
-    expect(page).to have_text(I18n.t("more"))
-    expect(page).to have_text(I18n.t("launch"))
-  end
-
-  it "renders the project card for user with no access" do
-    allow(policy(@project)).to receive(:user_access?).and_return(false)
-
-    render_inline(described_class.new(@project, @current_user))
-
-    expect(page).to have_text(I18n.t("fork"))
+    def initialize(project, current_user)
+      super
+      @project = project
+      @current_user = current_user
+    end
   end
 end
