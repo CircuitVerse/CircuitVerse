964abba182b18bafee901b94493336cc5c93a457
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2024-01-04 20:22:06 +0530

    feat: added missing test cases for the component

--- app/component/project_card/project_card_component.rb
+++ app/component/project_card/project_card_component.rb
@@ -1,14 +1,31 @@
 # frozen_string_literal: true
 
-module ProjectCard
-  class ProjectCardComponent < ViewComponent::Base
-    include Pundit::Authorization
-    include Devise::Controllers::Helpers
+require "rails_helper"
 
-    def initialize(project, current_user)
-      super
-      @project = project
-      @current_user = current_user
-    end
+RSpec.describe ProjectCard::ProjectCardComponent, type: :component do
+  include Pundit::Authorization
+  include Devise::Controllers::Helpers
+
+  before do
+    @author = FactoryBot.create(:user)
+    @project = FactoryBot.create(:project, author: @author, project_access_type: "Public")
+    @current_user = FactoryBot.create(:user)
+
+    render_inline(described_class.new(@project, @author))
+  end
+
+  it "renders the project card correctly" do
+    expect(page).to have_text(@project.name)
+    expect(page).to have_text(I18n.t("view"))
+    expect(page).to have_text(I18n.t("more"))
+    expect(page).to have_text(I18n.t("launch"))
+  end
+
+  it "renders the project card for user with no access" do
+    allow(policy(@project)).to receive(:user_access?).and_return(false)
+
+    render_inline(described_class.new(@project, @current_user))
+
+    expect(page).to have_text(I18n.t("fork"))
   end
 end
