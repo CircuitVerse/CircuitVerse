09647c5aaebcacd04ce64918311b5acd472c68cd
Priyam <priyam.saha2003@gmail.com>
2024-02-15 10:42:06 +0530

    primary mentor can add himself as a mentor fixed

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,7 +25,9 @@ class GroupsController < ApplicationController
 
   def group_invite
     if Group.with_valid_token.exists?(group_token: params[:token])
-      if current_user.groups.exists?(id: @group)
+      if current_user.email == @group.primary_mentor.email
+        notice = "Primary mentor cannot be added as a member of the group."
+      elsif current_user.groups.exists?(id: @group)
         notice = "Member is already present in the group."
       else
         current_user.group_members.create!(group: @group)
