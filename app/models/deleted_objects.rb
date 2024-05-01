f946c95c7e9ff80569227774ee78f41f2d6bb068
adarsh7raj yadav <adarshrajyadav68@gmail.com>
2024-05-01 09:33:37 +0000

    feat: addition of null objects

---
+++ app/models/deleted_objects.rb
@@ -0,0 +1,14 @@
+# app/models/null_objects.rb
+
+class DeletedUser
+    def name
+      "Deleted User"
+    end
+  end
+  
+  class DeletedProject
+    def name
+      "Deleted Project"
+    end
+  end
+  
