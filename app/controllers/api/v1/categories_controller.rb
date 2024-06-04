2c7d08b2d6ece1a63bf07ff38e3b8600da15b44f
jaydip1235 <jaydipdeyalok@gmail.com>
2024-06-04 20:59:41 +0530

    fix: fixed catergories controller spaces

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,28 +1,26 @@
 # frozen_string_literal: true
 
-class Api::V1::CategoriesController < ApplicationController 
-    before_action :authenticate_user!
-    # GET /api/v1/categories
-    def index
-        @categories = Category.all
-        render json: @categories
-      end
+class Api::V1::CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  # GET /api/v1/categories
+  def index
+    @categories = Category.all
+    render json: @categories
+  end
 
-      # POST /api/v1/categories
-    def create
-        @category = Category.new(category_params)
-        if @category.save
-          render json: @category, status: :created
-        else
-          render json: @category.errors, status: :unprocessable_entity
-        end
-      end
-
-      private
-
-      # Strong parameters for category
-    def category_params
-        params.require(:category).permit(:name)
+    # POST /api/v1/categories
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+        render json: @category, status: :created
+    else
+        render json: @category.errors, status: :unprocessable_entity
     end
+  end
 
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
