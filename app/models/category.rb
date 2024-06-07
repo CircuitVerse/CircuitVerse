e597f605756c5e4afc14c2bfe8cf56b12f68b3d5
jaydip1235 <jaydipdeyalok@gmail.com>
2024-06-07 23:32:20 +0530

    fix: removed apis

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  validates :name, presence: true
   has_many :questions, dependent: :destroy
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,16 +3,5 @@
 require "rails_helper"
 
 RSpec.describe Category, type: :model do
-  subject { described_class.new(name: "Test Category") }
-
-  describe "Validations" do
-    it "is valid with valid attributes" do
-      expect(subject).to be_valid
-    end
-
-    it "is not valid without a name" do
-      subject.name = nil
-      expect(subject).not_to be_valid
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
