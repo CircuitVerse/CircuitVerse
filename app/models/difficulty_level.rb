9daaa483b4a8f6cca9174b3a487d1ee840d694ab
jaydip1235 <jaydipdeyalok@gmail.com>
2024-06-16 13:05:05 +0530

    feat: all the controllers added (test not added)

--- app/models/difficulty_level.rb
+++ app/models/difficulty_level.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class DifficultyLevel < ApplicationRecord
-  enum value: { easy: 0, medium: 1, hard: 2 }
-
-  validates :name, presence: true
-  validates :value, presence: true
+  enum difficulty_level: {
+    easy: 0,
+    medium: 1,
+    hard: 2
+  }
 end
--- db/migrate/20240601071847_create_difficulty_levels.rb
+++ db/migrate/20240601071847_create_difficulty_levels.rb
@@ -1,9 +1,7 @@
 class CreateDifficultyLevels < ActiveRecord::Migration[7.0]
   def change
     create_table :difficulty_levels do |t|
-      t.string :name
-      t.integer :value
-
+      t.integer :difficulty_level, default: 0 
       t.timestamps
     end
   end
