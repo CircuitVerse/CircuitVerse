2cb6490009646344290d4c58ad2e06d945c290ce
jaydip1235 <jaydipdeyalok@gmail.com>
2024-06-16 15:59:34 +0530

    feat: all the controllers added (test not added)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_moderator, only: %i[create update destroy]
-  before_action :set_question, only: %i[update destroy]
+  before_action :set_question, only: %i[show update destroy]
 
   def index
     @questions = Question.all
