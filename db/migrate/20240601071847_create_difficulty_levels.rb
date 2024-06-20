b005cdca0910d7af0049f728f3167255d78e0184
jaydip1235 <jaydipdeyalok@gmail.com>
2024-06-21 01:48:22 +0530

    fix: removed controller logic

--- db/migrate/20240601071847_create_difficulty_levels.rb
+++
@@ -1,8 +0,0 @@
-class CreateDifficultyLevels < ActiveRecord::Migration[7.0]
-  def change
-    create_table :difficulty_levels do |t|
-      t.integer :difficulty_level, default: 0 
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20240601071931_create_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategories < ActiveRecord::Migration[7.0]
-  def change
-    create_table :categories do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
