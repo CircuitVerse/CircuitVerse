920f340c753e0afb1fdd9b468b4911d6c5cb8896
Asrani-Aman <amanasrani1712@gmail.com>
2024-07-03 09:13:19 +0530

    feat: add test for featurecard component

---
+++ spec/components/feature_card_component_spec.rb
@@ -0,0 +1,18 @@
+# spec/components/homepage_components/feature_card_component_spec.rb
+require "rails_helper"
+
+RSpec.describe HomepageComponents::FeatureCardComponent, type: :component do
+  it "renders the feature card with correct content" do
+    render_inline(described_class.new(
+      image_path: "homepage/export-hd.png",
+      image_alt: "Export HD",
+      title_key: "circuitverse.index.features.item1_heading",
+      text_key: "circuitverse.index.features.item1_text"
+    ))
+
+    expect(page).to have_css(".card.home-feature-card")
+    expect(page).to have_css("img.card-img-top[alt='Export HD']")
+    expect(page).to have_css(".card-title", text: I18n.t("circuitverse.index.features.item1_heading"))
+    expect(page).to have_css(".card-text", text: I18n.t("circuitverse.index.features.item1_text"))
+  end
+end
