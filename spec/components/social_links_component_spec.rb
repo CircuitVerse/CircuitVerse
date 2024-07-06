00aa3b2e67422c1d5038982fd8328afbf97890e4
Asrani-Aman <amanasrani1712@gmail.com>
2024-07-07 00:24:46 +0530

    feat: add tests for social link view component

---
+++ spec/components/social_links_component_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe SocialLinksComponent, type: :component do
+  it "renders social links" do
+    render_inline(described_class.new)
+
+    expect(page).to have_css(".footer-social-icons-row")
+    expect(page).to have_link(href: "/facebook")
+    expect(page).to have_link(href: "/twitter")
+    expect(page).to have_link(href: "/linkedin")
+    expect(page).to have_link(href: "/youtube")
+    expect(page).to have_link(href: "/github")
+    expect(page).to have_css("img.footer-social-icon", count: 5)
+  end
+end
