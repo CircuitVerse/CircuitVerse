72842ee78e9fc88ff8988b5dd980d7117b98b9fe
Asrani-Aman <amanasrani1712@gmail.com>
2024-07-06 14:40:42 +0530

    fix: coldeclimate extra space space fix

--- app/component/circuit_card_component.rb
+++ app/component/circuit_card_component.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 class CircuitCardComponent < ViewComponent::Base
-    with_collection_parameter :circuit
+  with_collection_parameter :circuit
   
-    def initialize(circuit:, current_user:)
-      super
-      @circuit = circuit
-      @current_user = current_user
-    end
-  
-    def image_path
-      helpers.project_image_preview(@circuit, @current_user)
-    end
-  
-    def circuit_name
-      @circuit.name
-    end
-  
-    def circuit_url
-      helpers.user_project_url(@circuit.author, @circuit)
-    end
+  def initialize(circuit:, current_user:)
+    super
+    @circuit = circuit
+    @current_user = current_user
   end
+  
+  def image_path
+    helpers.project_image_preview(@circuit, @current_user)
+  end
+  
+  def circuit_name
+    @circuit.name
+  end
+  
+  def circuit_url
+    helpers.user_project_url(@circuit.author, @circuit)
+  end
+end
   
