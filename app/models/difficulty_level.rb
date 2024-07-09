5657e48e133ac8b54187e4083f4cc02a87cd1e26
jaydip1235 <jaydipdeyalok@gmail.com>
2024-07-09 15:38:58 +0530

    feat: add questions

--- app/models/difficulty_level.rb
+++ app/models/difficulty_level.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class QuestionDifficultyLevel < ApplicationRecord
+class DifficultyLevel < ApplicationRecord
   enum difficulty_level: {
     easy: 0,
     medium: 1,
