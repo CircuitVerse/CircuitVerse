e06ea416631397c56e5f3f7649cec3ca09a1dde7
Asrani-Aman <amanasrani1712@gmail.com>
2024-07-29 20:02:12 +0530

    fix: code climate indentation issue

--- app/controllers/users/saml_sessions_controller.rb
+++ app/controllers/users/saml_sessions_controller.rb
@@ -25,21 +25,21 @@ class Users::SamlSessionsController < Devise::SamlSessionsController
   
   private
   
-  def saml_settings
-    settings = OneLogin::RubySaml::Settings.new
-    settings.assertion_consumer_service_url     = Devise.saml_config.assertion_consumer_service_url
-    settings.assertion_consumer_service_binding = Devise.saml_config.assertion_consumer_service_binding
-    settings.name_identifier_format             = Devise.saml_config.name_identifier_format
-    settings.issuer                             = Devise.saml_config.issuer
-    settings.authn_context                      = Devise.saml_config.authn_context
-    settings.idp_slo_target_url                 = Devise.saml_config.idp_slo_target_url
-    settings.idp_sso_target_url                 = Devise.saml_config.idp_sso_target_url
-    settings.idp_cert_fingerprint               = Devise.saml_config.idp_cert_fingerprint
-    settings.idp_cert_fingerprint_algorithm     = Devise.saml_config.idp_cert_fingerprint_algorithm
-    settings
-  end
+    def saml_settings
+      settings = OneLogin::RubySaml::Settings.new
+      settings.assertion_consumer_service_url     = Devise.saml_config.assertion_consumer_service_url
+      settings.assertion_consumer_service_binding = Devise.saml_config.assertion_consumer_service_binding
+      settings.name_identifier_format             = Devise.saml_config.name_identifier_format
+      settings.issuer                             = Devise.saml_config.issuer
+      settings.authn_context                      = Devise.saml_config.authn_context
+      settings.idp_slo_target_url                 = Devise.saml_config.idp_slo_target_url
+      settings.idp_sso_target_url                 = Devise.saml_config.idp_sso_target_url
+      settings.idp_cert_fingerprint               = Devise.saml_config.idp_cert_fingerprint
+      settings.idp_cert_fingerprint_algorithm     = Devise.saml_config.idp_cert_fingerprint_algorithm
+      settings
+    end
 
-  def store_winning_strategy
-    warden.session(:user)[:strategy] = warden.winning_strategies[:user].class.name.demodulize.underscore.to_sym
-  end 
+    def store_winning_strategy
+      warden.session(:user)[:strategy] = warden.winning_strategies[:user].class.name.demodulize.underscore.to_sym
+    end 
 end
