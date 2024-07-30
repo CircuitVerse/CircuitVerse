ff2a6304b4b97e0ac35d1c20b85811bd2626e409
AMAN ASRANI <96644946+Asrani-Aman@users.noreply.github.com>
2024-07-30 18:05:31 +0530

    refactor: adding methods in  saml_sessions_controller.rb

--- app/controllers/users/saml_sessions_controller.rb
+++ app/controllers/users/saml_sessions_controller.rb
@@ -2,9 +2,23 @@
 
 class Users::SamlSessionsController < Devise::SamlSessionsController
   after_action :store_winning_strategy, only: :create
+
+  def new
+    super
+  end
+
+  def metadata
+    super
+  end
+
+  def create
+    super
+  end
+
   
   private
-  
+    
+    # for Supporting multiple authentication strategies
     def store_winning_strategy
       warden.session(:user)[:strategy] = warden.winning_strategies[:user].class.name.demodulize.underscore.to_sym
     end
