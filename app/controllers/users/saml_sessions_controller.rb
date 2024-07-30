dc9fd1dedfdd5cd30dcdb4b423f26b26e7c937e3
AMAN ASRANI <96644946+Asrani-Aman@users.noreply.github.com>
2024-07-30 19:46:58 +0530

    fix: remove explicitly called methods in saml_session_controller

--- app/controllers/users/saml_sessions_controller.rb
+++ app/controllers/users/saml_sessions_controller.rb
@@ -1,25 +1,4 @@
 # frozen_string_literal: true
 
 class Users::SamlSessionsController < Devise::SamlSessionsController
-  after_action :store_winning_strategy, only: :create
-
-  def new
-    super
-  end
-
-  def metadata
-    super
-  end
-
-  def create
-    super
-  end
-
-  
-  private
-    
-    # for Supporting multiple authentication strategies
-    def store_winning_strategy
-      warden.session(:user)[:strategy] = warden.winning_strategies[:user].class.name.demodulize.underscore.to_sym
-    end
 end
