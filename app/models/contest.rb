c97843d697eda959da4b9d0da031370eebd99499
Asrani-Aman <amanasrani1712@gmail.com>
2024-08-01 10:47:49 +0530

    feat: close contest auto by deadline using active jobs

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class Contest < ApplicationRecord
-    has_noticed_notifications model_name: "NoticedNotification"
-    has_many :submissions, dependent: :destroy
-    has_many :submission_votes, dependent: :destroy
+  has_noticed_notifications model_name: "NoticedNotification"
+  has_many :submissions, dependent: :destroy
+  has_many :submission_votes, dependent: :destroy
+  after_commit :set_deadline_job
+
+  def set_deadline_job
+    if status != "Completed"
+      ContestDeadlineJob.set(wait: ((deadline - Time.zone.now) / 60).minute).perform_later(id)
+    end
+  end
 end
