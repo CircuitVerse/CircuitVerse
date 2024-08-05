754691b6b511797ffe74fe430b9a0a91dfec28e9
Asrani-Aman <amanasrani1712@gmail.com>
2024-08-05 09:58:56 +0530

    fix: remove extra spaces

--- spec/components/previews/feature_card_component_preview.rb
+++ spec/components/previews/feature_card_component_preview.rb
@@ -3,10 +3,10 @@
 class FeatureCardComponentPreview < ViewComponent::Preview
   def default
     render(HomepageComponents::FeatureCardComponent.new(
-             image_path: "homepage/export-hd.png",
-             image_alt: "Export HD",
-             title_key: "circuitverse.index.features.item1_heading",
-             text_key: "circuitverse.index.features.item1_text"
-           ))
+      image_path: "homepage/export-hd.png",
+      image_alt: "Export HD",
+      title_key: "circuitverse.index.features.item1_heading",
+      text_key: "circuitverse.index.features.item1_text"
+    ))
   end
 end
