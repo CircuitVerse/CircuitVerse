c733a970f08f37f3a03191755ff7519833ca6e59
Asrani-Aman <amanasrani1712@gmail.com>
2024-08-08 23:44:57 +0530

    fix: saving user_id while closing the contest

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -85,6 +85,7 @@ class ContestsController < ApplicationController
       @submission = Submission.new
       @submission.project_id = params[:submission][:project_id]
       @submission.contest_id = params[:contest_id]
+      @submission.user_id = current_user.id
       if @submission.save
         redirect_to contest_page_path(params[:contest_id]), notice: "Submission was successfully added."
       end
