db1c6e8884b299ec1f9e60583a5ae51492c145b6
Deeppjp116 <deeppjp1656@gmail.com>
2024-08-09 09:58:19 +0530

    fix(omniauth): migrate delete button to view component

---
+++ app/component/delete_button_component.rb
@@ -0,0 +1,11 @@
+class DeleteButtonComponent < ViewComponent::Base
+    def initialize(current_user:, profile_id:)
+        super
+      @current_user = current_user
+      @profile_id = profile_id
+    end
+  
+    def render?
+      @current_user.present?
+    end
+  end
