29a0c781e1cc174cb097c6679933229e6e079634
Asrani-Aman <amanasrani1712@gmail.com>
2024-08-09 20:15:38 +0530

    fix: codeclimate issues in shortlist_contest_winner.rb

--- app/services/shortlist_contest_winner.rb
+++ app/services/shortlist_contest_winner.rb
@@ -3,7 +3,11 @@
 class ShortlistContestWinner
   def initialize(contest_id)
     @contest = Contest.find(contest_id)
-    @most_voted_submission = Submission.where(contest_id: @contest.id).order("submission_votes_count DESC").limit(1).first
+    @most_voted_submission = Submission
+                             .where(contest_id: @contest.id)
+                             .order("submission_votes_count DESC")
+                             .limit(1)
+                             .first
     return if @most_voted_submission.nil?
 
     contest_winner = ContestWinner.new
