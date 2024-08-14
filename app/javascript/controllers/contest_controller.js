d19c00b44c8f108585dca08f92b795a47bf47091
Asrani-Aman <amanasrani1712@gmail.com>
2024-08-15 02:01:44 +0530

    fix: codeClimate issues by refactoring contest_controller.js

--- app/javascript/controllers/contest_controller.js
+++ app/javascript/controllers/contest_controller.js
@@ -1,57 +1,68 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-    static values = { timeleft: String }
+    static get values() {
+        return {
+            timeleft: String,
+        };
+    }
+
     connect() {
         this.setCountDownTimer();
         this.setShowModals();
         this.setAdminModals();
     }
 
+    // eslint-disable-next-line class-methods-use-this
     enableSubmitButton() {
         document.getElementById('submission-submit-button').disabled = false;
     }
 
+    // eslint-disable-next-line class-methods-use-this
     setAdminModals() {
-        $("#close-contest-confirmation-modal").on("show.bs.modal", function (e) {
-            let contest = $(e.relatedTarget).data('contest').id;
-            $(e.currentTarget).find('#close-contest-button').attr("href",
-                `/contests/${contest}/close_contest`);
-        })
-        $("#update-new-contest-modal").on("show.bs.modal", function (e) {
-            let contest = $(e.relatedTarget).data('contest').id;
-            $(e.currentTarget).find('#update-contest-button').attr("href",
-                `/contests/${contest}/update_contest`);
-        })
-        $("#update-contest-modal").on("show.bs.modal", function (e) {
-            let contestId = $(e.relatedTarget).data('contest').id;
-            let currentDeadline = $(e.relatedTarget).data('deadline');
-            let form = $(this).find('#update-contest-form');
-            let action = form.attr('action').replace(':contest_id', contestId);
-            form.attr('action', action)
+        const getContestId = (event) => $(event.relatedTarget).data('contest').id;
+
+        const setupModal = (modalId, buttonId, path) => {
+            $(`#${modalId}`).on('show.bs.modal', (e) => {
+                const contestId = getContestId(e);
+                $(e.currentTarget).find(`#${buttonId}`).attr('href', `/contests/${contestId}${path}`);
+            });
+        };
+
+        setupModal('close-contest-confirmation-modal', 'close-contest-button', '/close_contest');
+        setupModal('update-new-contest-modal', 'update-contest-button', '/update_contest');
+
+        $('#update-contest-modal').on('show.bs.modal', (e) => {
+            const contestId = getContestId(e);
+            const currentDeadline = $(e.relatedTarget).data('deadline');
+            const form = $(e.currentTarget).find('#update-contest-form');
+            const action = form.attr('action').replace(':contest_id', contestId);
+            form.attr('action', action);
             form.find('#contest_deadline').val(currentDeadline);
-          });
-        
-        
+        });
     }
 
+    // eslint-disable-next-line class-methods-use-this
     setShowModals() {
-        $("#projectModal").on("show.bs.modal", function (e) {
-            let projectSlug = $(e.relatedTarget).data('project').slug;
-            let projectId = $(e.relatedTarget).data('project').id;
-            let authorId = $(e.relatedTarget).data('project').author_id;
-            var id = projectSlug ? projectSlug : projectId;
-            $(e.currentTarget).find('#project-more-button').attr("href",
-                "/users/" + authorId + "/projects/" + id);
-            $(e.currentTarget).find('#project-ifram-preview').attr("src", "/simulator/" + id.toString())
-        })
-        $("#withdraw-submission-confirmation-modal").on("show.bs.modal", function (e) {
-            let contest = $(e.relatedTarget).data('contest').id;
-            let submission = $(e.relatedTarget).data('submission').id;
-            $(e.currentTarget).find("#withdraw-submission-button").attr("href",
-                `/contests/${contest}/withdraw/${submission}`
-            )
-        })
+        $('#projectModal').on('show.bs.modal', (e) => {
+            const projectSlug = $(e.relatedTarget).data('project').slug;
+            const projectId = $(e.relatedTarget).data('project').id;
+            const authorId = $(e.relatedTarget).data('project').author_id;
+            var id = projectSlug || projectId;
+            $(e.currentTarget).find('#project-more-button').attr(
+                'href',
+                `/users/${authorId}/projects/${id}`,
+            );
+            $(e.currentTarget).find('#project-ifram-preview').attr('src', `/simulator/${id.toString()}`);
+        });
+        $('#withdraw-submission-confirmation-modal').on('show.bs.modal', (e) => {
+            const contest = $(e.relatedTarget).data('contest').id;
+            const submission = $(e.relatedTarget).data('submission').id;
+            $(e.currentTarget).find('#withdraw-submission-button').attr(
+                'href',
+                `/contests/${contest}/withdraw/${submission}`,
+            );
+        });
     }
 
     setCountDownTimer() {
