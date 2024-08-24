40fd470245e1922b3d1a38560a68139395f610ed
Asrani-Aman <amanasrani1712@gmail.com>
2024-08-25 01:29:46 +0530

    feat: remove recent circuits section

--- app/controllers/circuitverse_controller.rb
+++ app/controllers/circuitverse_controller.rb
@@ -4,20 +4,6 @@ class CircuitverseController < ApplicationController
   MAXIMUM_FEATURED_CIRCUITS = 3
 
   def index
-    @projects = Project.select(:id, :author_id, :image_preview, :name, :slug)
-                       .public_and_not_forked
-                       .where.not(image_preview: "default.png")
-                       .order(id: :desc)
-                       .includes(:author)
-                       .limit(Project.per_page)
-
-    page = params[:page].to_i
-    @projects = if page.positive?
-      @projects.paginate(page: page)
-    else
-      @projects.paginate(page: nil)
-    end
-
     @featured_circuits = Project.joins(:featured_circuit)
                                 .order("featured_circuits.created_at DESC")
                                 .includes(:author)
