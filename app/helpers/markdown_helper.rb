8fb345457123806f142a6d087e8e0ba48626d936
jaydip1235 <jaydipdeyalok@gmail.com>
2024-09-17 10:36:38 +0530

    fix: removed redundant markdown_helper

--- app/helpers/markdown_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module MarkdownHelper
-  def markdown(text)
-    renderer = Redcarpet::Render::HTML.new
-    markdown = Redcarpet::Markdown.new(renderer)
-    markdown.render(text).html_safe
-  end
-end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -4,6 +4,6 @@ module QuestionsHelper
   def truncated_markdown(statement, length = 100)
     plain_text = strip_tags(statement)
     truncated_text = truncate(plain_text, length: length, separator: " ")
-    markdown(truncated_text)
+    render_markdown(truncated_text)
   end
 end
