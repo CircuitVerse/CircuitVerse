93ade7fac4a0bf420456e6e4172da8129b50dee0
jaydip1235 <jaydipdeyalok@gmail.com>
2024-09-23 19:40:47 +0530

    fix: some codeclimate errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@
 
 module ApplicationHelper
   def render_markdown(text)
-    sanitized_text = ActionController::Base.helpers.sanitize(text, tags: %w(p strong em a), attributes: %w(href))
+    sanitized_text = ActionController::Base.helpers.sanitize(text, tags: %w[p strong em a], attributes: %w[href])
     renderer = Redcarpet::Render::HTML.new
     markdown = Redcarpet::Markdown.new(renderer, {})
-    markdown.render(sanitized_text).html_safe
+    markdown_output = markdown.render(sanitized_text)
+    ERB::Util.html_escape(markdown_output)
   end
 end
-
