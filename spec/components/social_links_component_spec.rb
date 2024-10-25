c3b723d336794f124808e816c6cde261aaf34c42
Aditya Umesh Singh <salmoneatenbybear@gmail.com>
2024-10-25 15:25:10 +0530

    test(components): fix SocialLinksComponent test and update URLs

--- spec/components/social_links_component_spec.rb
+++ spec/components/social_links_component_spec.rb
@@ -3,17 +3,23 @@
 require "rails_helper"
 
 RSpec.describe SocialLinksComponent, type: :component do
+  include Capybara::RSpecMatchers
+
   it "renders social links in the footer" do
-    render_inline(described_class.new)
+    rendered_html = render_inline(described_class.new).to_html
+    rendered = Capybara.string(rendered_html)
+
+    # Debugging: Output the rendered HTML
+    # puts rendered_html
 
     aggregate_failures do
-      expect(rendered_component).to have_css(".footer-social-icons-container")
-      expect(rendered_component).to have_link(href: "https://www.facebook.com/CircuitVerse")
-      expect(rendered_component).to have_link(href: "https://twitter.com/CircuitVerse")
-      expect(rendered_component).to have_link(href: "https://www.linkedin.com/company/circuitverse")
-      expect(rendered_component).to have_link(href: "https://www.youtube.com/c/CircuitVerse")
-      expect(rendered_component).to have_link(href: "https://github.com/CircuitVerse")
-      expect(rendered_component).to have_css("img.footer-social-icon", count: 5)
+      expect(rendered).to have_css(".footer-social-icons-container")
+      expect(rendered).to have_link(nil, href: "https://www.facebook.com/CircuitVerse")
+      expect(rendered).to have_link(nil, href: "https://twitter.com/CircuitVerse") 
+      expect(rendered).to have_link(nil, href: "https://www.linkedin.com/company/circuitverse")
+      expect(rendered).to have_link(nil, href: "https://www.youtube.com/c/CircuitVerse")
+      expect(rendered).to have_link(nil, href: "https://github.com/CircuitVerse")
+      expect(rendered).to have_css("img.footer-social-icon", count: 5)
     end
   end
 end
