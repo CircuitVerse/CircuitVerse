5e1f16509744ddf9e416d18cca7d461330dea5b9
Aditya Umesh Singh <salmoneatenbybear@gmail.com>
2024-10-25 14:14:18 +0530

    fix(rb): fixing the test to expect correct updated component implementation

--- spec/components/social_links_component_spec.rb
+++ spec/components/social_links_component_spec.rb
@@ -3,17 +3,41 @@
 require "rails_helper"
 
 RSpec.describe SocialLinksComponent, type: :component do
-  it "renders social links" do
-    render_inline(described_class.new)
+  context "when GitLab integration is enabled" do
+    before do
+      allow(Flipper).to receive(:enabled?).with(:gitlab_integration).and_return(true)
+    end
 
-    aggregate_failures do
-      expect(page).to have_css(".footer-social-icons-row")
-      expect(page).to have_link(href: "/facebook")
-      expect(page).to have_link(href: "/twitter")
-      expect(page).to have_link(href: "/linkedin")
-      expect(page).to have_link(href: "/youtube")
-      expect(page).to have_link(href: "/github")
-      expect(page).to have_css("img.footer-social-icon", count: 5)
+    it "renders social links including GitLab" do
+      render_inline(described_class.new)
+
+      aggregate_failures do
+        expect(page).to have_css(".social-icons-row")
+        expect(page).to have_link(href: user_google_oauth2_omniauth_authorize_path)
+        expect(page).to have_link(href: user_facebook_omniauth_authorize_path)
+        expect(page).to have_link(href: user_github_omniauth_authorize_path)
+        expect(page).to have_link(href: user_gitlab_omniauth_authorize_path)
+        expect(page).to have_css("img.users-social-links", count: 4)
+      end
+    end
+  end
+
+  context "when GitLab integration is disabled" do
+    before do
+      allow(Flipper).to receive(:enabled?).with(:gitlab_integration).and_return(false)
+    end
+
+    it "renders social links without GitLab" do
+      render_inline(described_class.new)
+
+      aggregate_failures do
+        expect(page).to have_css(".social-icons-row")
+        expect(page).to have_link(href: user_google_oauth2_omniauth_authorize_path)
+        expect(page).to have_link(href: user_facebook_omniauth_authorize_path)
+        expect(page).to have_link(href: user_github_omniauth_authorize_path)
+        expect(page).not_to have_link(href: user_gitlab_omniauth_authorize_path)
+        expect(page).to have_css("img.users-social-links", count: 3)
+      end
     end
   end
 end
