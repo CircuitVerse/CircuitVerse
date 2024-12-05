4e817c3c962eae172c51386d51d195fe2dc1c1be
Aditya Singh <salmoneatenbybear@gmail.com>
2024-12-05 12:42:59 +0000

    feat(cursor): implemented cursor-based pagination

--- app/controllers/circuitverse_controller.rb
+++ app/controllers/circuitverse_controller.rb
@@ -3,20 +3,27 @@
 class CircuitverseController < ApplicationController
   MAXIMUM_FEATURED_CIRCUITS = 3
 
+  include ActionView::Helpers::AssetUrlHelper
+  include ActionView::Helpers::AssetTagHelper
+
   def index
+    per_page = params.fetch(:per_page, 10).to_i
+    cursor = params[:cursor]
+
     @projects = Project.select(:id, :author_id, :image_preview, :name, :slug)
                        .public_and_not_forked
                        .where.not(image_preview: "default.png")
-                       .order(id: :desc)
                        .includes(:author)
-                       .limit(Project.per_page)
+                       .order(id: :desc)
 
-    page = params[:page].to_i
-    @projects = if page.positive?
-      @projects.paginate(page: page)
-    else
-      @projects.paginate(page: nil)
-    end
+    # Apply cursor-based pagination using the gem
+    paginator = @projects.cursor_paginate(after: cursor, limit: per_page)
+    page = paginator.fetch
+    @projects = page.records
+
+    # Determine if there's a next or previous cursor
+    @next_cursor = page.next_cursor
+    @previous_cursor = page.previous_cursor
 
     @featured_circuits = Project.joins(:featured_circuit)
                                 .order("featured_circuits.created_at DESC")
@@ -25,24 +32,49 @@ class CircuitverseController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { render json: @projects }
+      format.json do
+        render json: {
+          projects: @projects.map { |project| serialize_project(project) },
+          next_cursor: @next_cursor
+        }
+      end
       format.js
     end
   end
 
   def examples
-    @examples = [{ name: "Full Adder from 2-Half Adders", id: "users/3/projects/247",
-                   img: "examples/fullAdder_n.jpg" },
-                 { name: "16 Bit ripple carry adder", id: "users/3/projects/248",
-                   img: "examples/RippleCarry_n.jpg" },
-                 { name: "Asynchronous Counter", id: "users/3/projects/249",
-                   img: "examples/AsyncCounter_n.jpg" },
-                 { name: "Keyboard", id: "users/3/projects/250",
-                   img: "examples/Keyboard_n.jpg" },
-                 { name: "FlipFlop", id: "users/3/projects/251",
-                   img: "examples/FlipFlop_n.jpg" },
-                 { name: "ALU 74LS181 by Ananth Shreekumar", id: "users/126/projects/252",
-                   img: "examples/ALU_n.jpg" }]
+    @examples = [
+      {
+        name: "Full Adder from 2-Half Adders",
+        id: "users/3/projects/247",
+        img: "examples/fullAdder_n.jpg"
+      },
+      {
+        name: "16 Bit ripple carry adder",
+        id: "users/3/projects/248",
+        img: "examples/RippleCarry_n.jpg"
+      },
+      {
+        name: "Asynchronous Counter",
+        id: "users/3/projects/249",
+        img: "examples/AsyncCounter_n.jpg"
+      },
+      {
+        name: "Keyboard",
+        id: "users/3/projects/250",
+        img: "examples/Keyboard_n.jpg"
+      },
+      {
+        name: "FlipFlop",
+        id: "users/3/projects/251",
+        img: "examples/FlipFlop_n.jpg"
+      },
+      {
+        name: "ALU 74LS181 by Ananth Shreekumar",
+        id: "users/126/projects/252",
+        img: "examples/ALU_n.jpg"
+      }
+    ]
   end
 
   def tos; end
@@ -50,4 +82,24 @@ class CircuitverseController < ApplicationController
   def teachers; end
 
   def contribute; end
+
+  private
+
+  def serialize_project(project)
+    {
+      id: project.id,
+      author_id: project.author_id,
+      name: project.name,
+      slug: project.slug,
+      image_preview: project_image_preview(project)
+    }
+  end
+
+  def project_image_preview(project)
+    if project.image_preview.present? && project.image_preview.url != "/uploads/project/image_preview/default.png"
+      project.image_preview.url
+    else
+      asset_path("default.png")
+    end
+  end
 end
