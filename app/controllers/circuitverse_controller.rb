6d93c90f9dfddd9dd1b89b66d727bf4c27727537
Aditya Singh <salmoneatenbybear@gmail.com>
2024-12-11 14:02:16 +0000

    style(controller): revert orignal array formatting

--- app/controllers/circuitverse_controller.rb
+++ app/controllers/circuitverse_controller.rb
@@ -47,14 +47,18 @@ class CircuitverseController < ApplicationController
   end
 
   def examples
-    @examples = [
-      { name: "Full Adder from 2-Half Adders", id: "users/3/projects/247", img: "examples/fullAdder_n.jpg" },
-      { name: "16 Bit ripple carry adder", id: "users/3/projects/248", img: "examples/RippleCarry_n.jpg" },
-      { name: "Asynchronous Counter", id: "users/3/projects/249", img: "examples/AsyncCounter_n.jpg" },
-      { name: "Keyboard", id: "users/3/projects/250", img: "examples/Keyboard_n.jpg" },
-      { name: "FlipFlop", id: "users/3/projects/251", img: "examples/FlipFlop_n.jpg" },
-      { name: "ALU 74LS181 by Ananth Shreekumar", id: "users/126/projects/252", img: "examples/ALU_n.jpg" }
-    ]
+    @examples = [{ name: "Full Adder from 2-Half Adders", id: "users/3/projects/247",
+                   img: "examples/fullAdder_n.jpg" },
+                 { name: "16 Bit ripple carry adder", id: "users/3/projects/248",
+                   img: "examples/RippleCarry_n.jpg" },
+                 { name: "Asynchronous Counter", id: "users/3/projects/249",
+                   img: "examples/AsyncCounter_n.jpg" },
+                 { name: "Keyboard", id: "users/3/projects/250",
+                   img: "examples/Keyboard_n.jpg" },
+                 { name: "FlipFlop", id: "users/3/projects/251",
+                   img: "examples/FlipFlop_n.jpg" },
+                 { name: "ALU 74LS181 by Ananth Shreekumar", id: "users/126/projects/252",
+                   img: "examples/ALU_n.jpg" }]
   end
 
   def tos; end
