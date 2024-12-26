f337d014a37deb922bbc778449e8ec7d408ace6e
Harsh Rao <166894150+ThatDeparted2061@users.noreply.github.com>
2024-12-26 19:35:08 +0000

    few deprecation fix

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,8 +1,8 @@
-require 'aws-sdk'
+require 'aws-sdk-ses'
 
 ActionMailer::Base.add_delivery_method(
   :ses,
-  Aws::SES::SESMailer,
+  Aws::SES::Client,  
   region: ENV['AWS_REGION_SES'],
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID_SES'], ENV['AWS_SECRET_ACCESS_KEY_SES'])
 )
