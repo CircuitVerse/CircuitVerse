aaa7e0c95b989508ca0e01481bed1c58e1375bac
senbo1 <harshbhadu86@gmail.com>
2025-03-04 12:18:31 +0530

    fix: preview error

--- spec/components/previews/social_login_component_preview.rb
+++ spec/components/previews/social_login_component_preview.rb
@@ -2,7 +2,7 @@
 
 class SocialLoginComponentPreview < ViewComponent::Preview
   def default
-    fake_mapping = Struct.new(confirmable?: true)
+    fake_mapping = Struct.new(:confirmable?).new(true)
     render(AuthComponents::SocialLoginComponent.new(devise_mapping: fake_mapping, resource_name: :user))
   end
 end
