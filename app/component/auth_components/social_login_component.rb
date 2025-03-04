5127fcdbe64d1a7165fd948005b30446eccb12a3
senbo1 <harshbhadu86@gmail.com>
2025-03-04 09:37:21 +0530

    fix: linting erros

--- app/component/auth_components/social_login_component.rb
+++ app/component/auth_components/social_login_component.rb
@@ -9,20 +9,5 @@ module AuthComponents
       @devise_mapping = devise_mapping
       @resource_name = resource_name
     end
-
-    def provider_link(provider)
-      case provider
-      when :google
-        user_google_oauth2_omniauth_authorize_path
-      when :facebook
-        user_facebook_omniauth_authorize_path
-      when :github
-        user_github_omniauth_authorize_path
-      when :gitlab
-        user_gitlab_omniauth_authorize_path
-      else
-        "#"
-      end
-    end
   end
 end
--- spec/components/social_login_component_spec.rb
+++ spec/components/social_login_component_spec.rb
@@ -2,8 +2,14 @@
 
 require "rails_helper"
 
+class DeviseMapping
+  def confirmable?
+    true
+  end
+end
+
 RSpec.describe AuthComponents::SocialLoginComponent, type: :component do
-  let(:devise_mapping) { instance_double("DeviseMapping", confirmable?: true) }
+  let(:devise_mapping) { instance_double(DeviseMapping, confirmable?: true) }
   let(:resource_name) { :user }
 
   it "renders the social login component" do
@@ -14,14 +20,8 @@ RSpec.describe AuthComponents::SocialLoginComponent, type: :component do
       expect(page).to have_css("img[alt='Google Icon']")
       expect(page).to have_css("img[alt='Facebook Icon']")
       expect(page).to have_css("img[alt='Github Icon']")
+      expect(page).to have_content(I18n.t("users.shared.login_with"))
+      expect(page).to have_link(I18n.t("users.shared.resend_email"))
     end
   end
-
-  it "renders text content correctly" do
-    render_inline(described_class.new(devise_mapping: devise_mapping, resource_name: resource_name))
-
-    expect(page).to have_content(I18n.t("users.shared.login_with"))
-    expect(page).to have_link(I18n.t("users.shared.resend_email"))
-  end
-
 end
