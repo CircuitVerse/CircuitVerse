0f943948f84da028e1a9fa8dace77b81fa507890
Aditya Singh <salmoneatenbybear@gmail.com>
2025-03-11 11:35:25 +0530

    fix: rails Route Errors

--- spec/system/notification_spec.rb
+++ spec/system/notification_spec.rb
@@ -9,43 +9,33 @@ describe "Notifcation", type: :system do
   before do
     @author = FactoryBot.create(:user)
     @user = sign_in_random_user
-    @project = FactoryBot.create(:project, name: "Project", author: @author, project_access_type: "Public")
+    @project = FactoryBot.create(:project, name: "Project", author: author, project_access_type: "Public")
     driven_by(:selenium_chrome_headless)
   end
 
   it "initiate notification" do
     sign_in @user
-    visit user_project_path(
-      author:  @author,
-      id:      @project
-    )
+
+    visit user_project_path(author, @project)
 
     perform_enqueued_jobs do
       click_on "Fork"
     end
-
-    expect(
-      @author.noticed_notifications.count
-    ).to eq(1)
+    
+    expect(author.noticed_notifications.count).to eq(1)
   end
 
   context "notification page" do
     before do
       sign_in @user
-      visit user_project_path(
-        author:  @author,
-        id:      @project
-      )
+      visit user_project_path(author, @project)
+
       perform_enqueued_jobs do
         click_on "Fork"
       end
 
-      sign_in(
-        @author
-      )
-      visit notifications_path(
-        @author
-      )
+      sign_in author
+      visit notifications_path(author)
     end
 
     it "render all notifications" do
@@ -61,18 +51,19 @@ describe "Notifcation", type: :system do
     it "mark all notifications as read" do
       expect(page).to have_link("Mark all as read")
       click_on "Mark all as read"
-      expect(
-        @author.noticed_notifications.unread.count
-      ).to eq(0)
+      expect(author.noticed_notifications.unread.count).to eq(0)
     end
 
     it "mark notification as read" do
       expect(page).to have_link("#{@user.name} forked your Project #{@project.name}")
       click_on "#{@user.name} forked your Project #{@project.name}"
-
-      expect(
-        @author.noticed_notifications.read.count
-      ).to eq(1)
+      expect(author.noticed_notifications.read.count).to eq(1)
     end
+  end
+
+  private
+
+  def author
+    @author
   end
 end
