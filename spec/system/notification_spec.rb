ffae784a2bdfede98019d6b42ade3f95e60e5f37
Aditya Singh <salmoneatenbybear@gmail.com>
2025-03-11 11:19:52 +0530

    coderrabbit suggestions applied

--- spec/system/notification_spec.rb
+++ spec/system/notification_spec.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require "active_job/test_helper"
 
 describe "Notifcation", type: :system do
+  include ActiveJob::TestHelper
+
   before do
     @author = FactoryBot.create(:user)
     @user = sign_in_random_user
@@ -12,31 +15,37 @@ describe "Notifcation", type: :system do
 
   it "initiate notification" do
     sign_in @user
-    visit user_project_path(@author, @project)
+    visit user_project_path(
+      author:  @author,
+      id:      @project
+    )
 
-    # Wrap the click in perform_enqueued_jobs so any background job for the
-    # notification finishes before we check the notification count.
     perform_enqueued_jobs do
       click_on "Fork"
     end
 
-    # We expect the author to have exactly one notification after the user forks
-    # now we ensure the job has run)
-    expect(author.noticed_notifications.count).to eq(1)
+    expect(
+      @author.noticed_notifications.count
+    ).to eq(1)
   end
 
   context "notification page" do
     before do
-      # Also wrap 'Fork' in perform_enqueued_jobs so that the notification is created
-      # before we switch to the author's account to view notifications.
       sign_in @user
-      visit user_project_path(author, @project)
+      visit user_project_path(
+        author:  @author,
+        id:      @project
+      )
       perform_enqueued_jobs do
         click_on "Fork"
       end
 
-      sign_in author
-      visit notifications_path(author)
+      sign_in(
+        @author
+      )
+      visit notifications_path(
+        @author
+      )
     end
 
     it "render all notifications" do
@@ -44,27 +53,26 @@ describe "Notifcation", type: :system do
     end
 
     it "render all unread notifications" do
-      # Ensure the unread-notifications element is on the page before clicking.
       expect(page).to have_selector("#unread-notifications")
       page.find("#unread-notifications").click
-
       expect(page).to have_text("#{@user.name} forked your Project #{@project.name}")
     end
 
     it "mark all notifications as read" do
-      # Wait for the link to appear (protects against timing issues).
       expect(page).to have_link("Mark all as read")
       click_on "Mark all as read"
-
-      expect(author.noticed_notifications.unread.count).to eq(0)
+      expect(
+        @author.noticed_notifications.unread.count
+      ).to eq(0)
     end
 
     it "mark notification as read" do
-      # Wait for the exact link to appear before clicking it.
       expect(page).to have_link("#{@user.name} forked your Project #{@project.name}")
       click_on "#{@user.name} forked your Project #{@project.name}"
 
-      expect(author.noticed_notifications.read.count).to eq(1)
+      expect(
+        @author.noticed_notifications.read.count
+      ).to eq(1)
     end
   end
 end
