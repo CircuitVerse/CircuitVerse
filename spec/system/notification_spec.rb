ea7136b4eea7df9d3ead55468cd73d0834fd100d
Aditya Singh <salmoneatenbybear@gmail.com>
2025-03-13 08:47:37 +0530

    fix: Set a larger screen size specifically for these tests so notifications are visible

--- spec/system/notification_spec.rb
+++ spec/system/notification_spec.rb
@@ -7,7 +7,7 @@ describe "Notifcation", type: :system do
     @author = FactoryBot.create(:user)
     @user = sign_in_random_user
     @project = FactoryBot.create(:project, name: "Project", author: @author, project_access_type: "Public")
-    driven_by(:selenium_chrome_headless)
+    driven_by(:selenium_chrome_headless, screen_size: [1400, 900])
   end
 
   it "initiate notification" do
