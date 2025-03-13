e7b587fa9460af6be30f4a271443adb551f04dad
Aditya Singh <salmoneatenbybear@gmail.com>
2025-03-13 09:31:27 +0530

    fix: wrap actions inside a perform_enqueued_jobs and do inline background processing

--- spec/system/notification_spec.rb
+++ spec/system/notification_spec.rb
@@ -11,10 +11,10 @@ describe "Notifcation", type: :system do
     page.driver.browser.manage.window.resize_to(1920, 1080)
   end
 
-  it "initiate notification" do
+  it "initiates notification" do
     sign_in @user
     visit user_project_path(@author, @project)
-    click_on "Fork"
+    perform_enqueued_jobs { click_on "Fork" }
     expect(@author.noticed_notifications.count).to eq(1)
   end
 
@@ -22,26 +22,26 @@ describe "Notifcation", type: :system do
     before do
       sign_in @user
       visit user_project_path(@author, @project)
-      click_on "Fork"
+      perform_enqueued_jobs { click_on "Fork" }
       sign_in @author
       visit notifications_path(@author)
     end
 
-    it "render all notifications" do
+    it "renders all notifications" do
       expect(page).to have_text("#{@user.name} forked your Project #{@project.name}")
     end
 
-    it "render all unread notifications" do
+    it "renders all unread notifications" do
       page.find("#unread-notifications").click
       expect(page).to have_text("#{@user.name} forked your Project #{@project.name}")
     end
 
-    it "mark all notifications as read" do
+    it "marks all notifications as read" do
       click_on "Mark all as read"
       expect(@author.noticed_notifications.unread.count).to eq(0)
     end
 
-    it "mark notification as read" do
+    it "marks a notification as read" do
       click_on "#{@user.name} forked your Project #{@project.name}"
       expect(@author.noticed_notifications.read.count).to eq(1)
     end
