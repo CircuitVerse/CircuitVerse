4e7d1e2e7c9b75abc88ca7078ebc6672e95c0327
Aditya Singh <salmoneatenbybear@gmail.com>
2025-06-07 13:08:28 +0530

    chore(spec): explicit rollback to 8f1920d987d71c3de9f163ad7199387b4f235cb4 (#15)

--- spec/support/capybara_chrome.rb
+++ spec/support/capybara_chrome.rb
@@ -2,31 +2,29 @@
 
 require "capybara/rspec"
 require "selenium/webdriver"
+require "tmpdir"
 
-Capybara.register_driver :selenium_chrome_headless do |app|
+Capybara.register_driver :headless_chrome do |app|
   chrome_opts = Selenium::WebDriver::Chrome::Options.new
 
-  chrome_opts.add_argument("--headless=new")
+  chrome_opts.add_argument("--headless")
   chrome_opts.add_argument("--disable-gpu")
   chrome_opts.add_argument("--no-sandbox")
   chrome_opts.add_argument("--disable-dev-shm-usage")
   chrome_opts.add_argument("--window-size=1400,1400")
 
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options: chrome_opts
-  )
+  profile_dir = File.join(Dir.tmpdir, "chrome-profile-#{Process.pid}")
+  chrome_opts.add_argument("--user-data-dir=#{profile_dir}")
+
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :chrome,
+                                 capabilities: chrome_opts)
 end
 
-Capybara.register_driver :headless_chrome, &Capybara.drivers[:selenium_chrome_headless]
+Capybara.javascript_driver = :headless_chrome
 
 RSpec.configure do |config|
-  config.before(type: :system, js: true) do
-    driven_by :selenium_chrome_headless
-  end
-
-  config.before(type: :system, js: false) do
-    driven_by :rack_test
+  config.before(type: :system) do
+    driven_by :headless_chrome
   end
 end
