c86ea012ba700b7b59c03f38e18c117511f4bb59
ANIRUDH <anirudhnarang0@gmail.com>
2025-06-08 17:46:07 +0530

    Fix DonationCardComponent, add previews, specs, and update views

--- spec/components/previews/donation_card_component_preview.rb
+++ spec/components/previews/donation_card_component_preview.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class DonationCardComponentPreview < ViewComponent::Preview
   def default
-    render DonationCardComponent.new(
+    render ContributePageComponents::DonationCardComponent.new(
       image_path: "logos/opencollective-logo.png",
       link: "https://opencollective.com/CircuitVerse/contribute",
       alt_text: "Open Collective Logo",
