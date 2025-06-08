3a57b9e045d8581c0edc3d0dfe00129ad0ba2198
ANIRUDH <anirudhnarang0@gmail.com>
2025-06-08 17:59:17 +0530

    Delete leftover donation_card_component files from app/component

--- app/component/donation_card_component.rb
+++
@@ -1,9 +0,0 @@
-# app/components/donation_card_component.rb
-class DonationCardComponent < ViewComponent::Base
-    def initialize(image_path:, link:, alt_text:, description:)
-      @image_path = image_path
-      @link = link
-      @alt_text = alt_text
-      @description = description
-    end
-  end
