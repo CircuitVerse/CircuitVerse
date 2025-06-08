18709f69a2bdaf071e57d53597e9ddbca7d2812c
Aditya Singh <salmoneatenbybear@gmail.com>
2025-06-08 12:56:45 +0530

    Update db/migrate/20250105113940_create_contests.rb
    Co-authored-by: Vaibhav Upreti <vaibhav.upreti16@gmail.com>

--- db/migrate/20250105113940_create_contests.rb
+++ db/migrate/20250105113940_create_contests.rb
@@ -2,7 +2,7 @@ class CreateContests < ActiveRecord::Migration[7.0]
   def change
     create_table :contests do |t|
       t.datetime :deadline
-      t.integer :status, :integer
+      t.integer :status
 
       t.timestamps
     end
