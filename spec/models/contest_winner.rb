38ec434f4df45f86f09b2f6f1200cd30823036a3
Aditya Singh <salmoneatenbybear@gmail.com>
2025-06-12 23:21:26 +0530

    Update spec/models/contest_winner.rb
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- spec/models/contest_winner.rb
+++ spec/models/contest_winner.rb
@@ -4,7 +4,10 @@ require "rails_helper"
 
 RSpec.describe ContestWinner, type: :model do
   describe "associations" do
-    it { is_expected.to belongs_to(:submissions) }
+# spec/models/contest_winner.rb
+
+-    it { is_expected.to belongs_to(:submissions) }
++    it { is_expected.to belongs_to(:submission) }
     it { is_expected.to belongs_to(:project) }
     it { is_expected.to belongs_to(:contest) }
   end
