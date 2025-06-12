3e7a9f14cf680294c1f4ae8bde9aaa8530992932
Aditya Singh <salmoneatenbybear@gmail.com>
2025-06-12 23:21:52 +0530

    Update spec/models/contest.rb
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- spec/models/contest.rb
+++ spec/models/contest.rb
@@ -6,6 +6,6 @@ RSpec.describe Contest, type: :model do
   describe "associations" do
     it { is_expected.to have_many(:submissions) }
     it { is_expected.to have_many(:submission_votes) }
-    it { is_expected.to has_one(:contest_winner) }
+    it { is_expected.to have_one(:contest_winner) }
   end
 end
