f15414890e17708680afceb03fe77ebbd1d29573
senbo1 <senbocodes@gmail.com>
2025-06-15 13:12:02 +0530

    minor fixes

--- spec/components/user_card_component_spec.rb
+++ spec/components/user_card_component_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 RSpec.describe UserComponents::UserCardComponent, type: :component do
   include ViewComponent::TestHelpers
   include Rails.application.routes.url_helpers
+  include ActionView::Helpers::SanitizeHelper
 
   let(:user) { create(:user) }
   let(:profile) { ProfileDecorator.new(user) }
@@ -31,14 +32,30 @@ RSpec.describe UserComponents::UserCardComponent, type: :component do
     render_inline(described_class.new(profile: profile))
 
     aggregate_failures do
-      expect(page).to have_text("Member since:")
-      expect(page).to have_text("Educational Institution:")
-      expect(page).to have_text("Country:")
-      expect(page).to have_text("Total Circuits Made:")
-      expect(page).to have_text(profile.member_since)
-      expect(page).to have_text(profile.educational_institute)
-      expect(page).to have_text(profile.country_name)
-      expect(page).to have_text(profile.total_circuits)
+      expect(page).to have_content(
+        strip_tags(
+          I18n.t("users.circuitverse.member_since_html",
+                 member_since: profile.member_since)
+        )
+      )
+      expect(page).to have_content(
+        strip_tags(
+          I18n.t("users.circuitverse.educational_institutes_html",
+                 educational_institute: profile.educational_institute)
+        )
+      )
+      expect(page).to have_content(
+        strip_tags(
+          I18n.t("users.circuitverse.country_html",
+                 country_name: profile.country_name)
+        )
+      )
+      expect(page).to have_content(
+        strip_tags(
+          I18n.t("users.circuitverse.total_circuits_made_html",
+                 total_circuits: profile.total_circuits)
+        )
+      )
     end
   end
 
