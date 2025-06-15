685c6c82f728118ec9b47454f0986e5f76945ec7
ANIRUDH <anirudhnarang0@gmail.com>
2025-06-16 00:13:00 +0530

    Fixed Code Climate issues and formatting

--- spec/components/previews/donation_card_component_preview.rb
+++ spec/components/previews/donation_card_component_preview.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class DonationCardComponentPreview < ViewComponent::Preview
-    
-      def default
-        render ContributePageComponents::DonationCardComponent.new(
-          image_path: "logos/opencollective-logo.png",
-          link: "https://opencollective.com/CircuitVerse/contribute",
-          alt_text: "Open Collective Logo",
-          description: "Donate through Open Collective"
-        )
-      end
+    def default
+      render ContributePageComponents::DonationCardComponent.new(
+        image_path: "logos/opencollective-logo.png",
+        link: "https://opencollective.com/CircuitVerse/contribute",
+        alt_text: "Open Collective Logo",
+        description: "Donate through Open Collective"
+      )
     end
-    
+  end
+  
