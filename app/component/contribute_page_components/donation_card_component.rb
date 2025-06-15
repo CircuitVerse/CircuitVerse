1c9216bdb5708bd9edcb0b5b9328f5e95907753b
ANIRUDH <anirudhnarang0@gmail.com>
2025-06-15 23:50:35 +0530

    made donation card component

---
+++ app/component/contribute_page_components/donation_card_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module ContributePageComponents
+    class DonationCardComponent < ViewComponent::Base
+      def initialize(image_path:, link:, alt_text:, description:)
+        super()
+        @image_path = image_path
+        @link = link
+        @alt_text = alt_text
+        @description = description
+      end
+    end
+  end
