3bd2e3b9c8418390aa79a63f2eb104098b0415b3
senbo1 <senbocodes@gmail.com>
2025-06-20 23:07:02 +0530

    migrate contribute card to vc

---
+++ spec/components/previews/contribute_card_component_preview.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ContributeCardComponentPreview < ViewComponent::Preview
+  def student_card
+    render(Circuitverse::ContributeCardComponent.new(
+             image_src: "SVGs/student.svg",
+             alt_text: "Student Icon",
+             title_key: "circuitverse.contribute.student_card.main_heading",
+             items: [
+               "circuitverse.contribute.student_card.item1_text",
+               "circuitverse.contribute.student_card.item2_text_html",
+               "circuitverse.contribute.student_card.item3_text"
+             ]
+           ))
+  end
+end
