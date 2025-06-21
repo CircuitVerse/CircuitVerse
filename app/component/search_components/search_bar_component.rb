16d0be1b8a98980af2448ce64756da71fbff3bb6
senbo1 <senbocodes@gmail.com>
2025-06-21 13:19:52 +0530

    add translations

--- app/component/search_components/search_bar_component.rb
+++ app/component/search_components/search_bar_component.rb
@@ -7,21 +7,25 @@ module SearchComponents
       query: nil,
       search_path: "/search",
       resource_options: %w[Users Projects],
-      placeholders: {
-        "Users" => "Search for users",
-        "Projects" => "Search for projects"
-      }
+      placeholders: nil
     )
       super
       @resource = resource
       @query = query
       @search_path = search_path
       @resource_options = resource_options
-      @placeholders = placeholders
+      @placeholders = placeholders || default_placeholders
     end
 
     private
 
       attr_reader :resource, :query, :search_path, :resource_options, :placeholders
+
+      def default_placeholders
+        {
+          "Users" => I18n.t("components.search_bar.placeholders.users"),
+          "Projects" => I18n.t("components.search_bar.placeholders.projects")
+        }
+      end
   end
 end
