bd175c2a8aa5899a8832f21c53c4f4765b71be85
salmoneatenbybear <salmoneatenbybear@gmail.com>
2025-06-24 10:07:31 +0530

    chore: fix rubocop warnings and stabilize driver settings

--- spec/models/contest_winner_spec.rb
+++ spec/models/contest_winner_spec.rb
@@ -6,9 +6,8 @@ RSpec.describe ContestWinner, type: :model do
   describe "associations" do
     # spec/models/contest_winner.rb
 
-    it { is_expected.to belongs_to(:submissions) }
-    it { is_expected.to belongs_to(:submission) }
-    it { is_expected.to belongs_to(:project) }
-    it { is_expected.to belongs_to(:contest) }
+    it { is_expected.to belong_to(:submission) }
+    it { is_expected.to belong_to(:project) }
+    it { is_expected.to belong_to(:contest) }
   end
 end
