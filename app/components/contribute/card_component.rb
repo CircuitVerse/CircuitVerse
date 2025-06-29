2e397f10c627630e7988d5abf64380835269c005
Harsh <102225113+senbo1@users.noreply.github.com>
2025-06-29 13:15:17 +0530

    Migrate Contribute Card to View Component (#5830)
    * migrate contribute card to vc
    
    * formatting fixes
    
    * change module name
    
    * fix directory structure
    
    * fix hound

---
+++ app/components/contribute/card_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Contribute::CardComponent < ViewComponent::Base
+  def initialize(image_src:, alt_text:, title_key:, items:, column_classes: "col-12 col-sm-12 col-md-6 col-lg-4")
+    super
+    @image_src = image_src
+    @alt_text = alt_text
+    @title_key = title_key
+    @items = items
+    @column_classes = column_classes
+  end
+end
