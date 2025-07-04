3b6e4ef2f3dc0d8669a8e298008ebddf57f2c4c5
ANIRUDH <anirudhnarang0@gmail.com>
2025-07-05 03:33:45 +0530

    removed selenium

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,25 +1,10 @@
 # frozen_string_literal: true
 
 require "capybara/rails"
-require "selenium/webdriver"
-require "tmpdir"
-require "securerandom"
+require "capybara/playwright/driver"
 
-Capybara.register_driver :headless_chrome_unique_profile do |app|
-  chrome_args = %w[
-    headless
-    disable-gpu
-    no-sandbox
-    disable-dev-shm-usage
-    window-size=1400,1400
-  ]
-
-  temp_profile = File.join(Dir.tmpdir, "chrome-profile-#{SecureRandom.uuid}")
-  Dir.mkdir(temp_profile)
-  chrome_args << "user-data-dir=#{temp_profile}"
-
-  options = Selenium::WebDriver::Chrome::Options.new(args: chrome_args)
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+Capybara.register_driver :playwright do |app|
+  Capybara::Playwright::Driver.new(app, browser_type: :chromium)
 end
 
 RSpec.configure do |config|
@@ -28,8 +13,11 @@ RSpec.configure do |config|
   end
 
   config.before(type: :system, js: true) do
-    driven_by :headless_chrome_unique_profile
+    driven_by :playwright
   end
 end
 
-Capybara.javascript_driver = :headless_chrome_unique_profile
+Capybara.javascript_driver = :playwright
+Capybara.default_driver = :playwright
+Capybara.default_max_wait_time = 15
+Capybara.raise_server_errors = false
