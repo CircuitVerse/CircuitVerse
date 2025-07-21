6d8d0fa55cf7c56f7bf04493d0899f19f3206222
ANIRUDH <149250431+blazethunderstorm@users.noreply.github.com>
2025-07-21 22:06:43 +0530

    refactor: extract donation card to component (#5820)
    * made donation card component

---
+++ spec/components/previews/donation_card_component_preview.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class DonationCardComponentPreview < ViewComponent::Preview
+  def default
+    render Donation::DonationCardComponent.new(
+      image_path: "logos/opencollective-logo.png",
+      link: "https://opencollective.com/CircuitVerse/contribute",
+      alt_text: "Open Collective Logo",
+      description: "Donate through Open Collective"
+    )
+  end
+end
