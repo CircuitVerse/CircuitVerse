1d301ca16f0381281252ab92958353bf2b8c219f
senbo1 <senbocodes@gmail.com>
2025-07-26 15:08:50 +0530

    remove unnecessary parameters

--- app/components/search/search_bar_component.rb
+++ app/components/search/search_bar_component.rb
@@ -3,32 +3,29 @@
 class Search::SearchBarComponent < ViewComponent::Base
   def initialize(
     resource: nil,
-    query: nil,
-    search_path: "/search",
-    resource_options: %w[Users Projects],
-    options: {}
+    query: nil
   )
     super
     @resource = resource
     @query = query
-    @search_path = search_path
-    @resource_options = resource_options
-    @placeholders = options[:placeholders] || default_placeholders
-    @option_labels = options[:option_labels] || default_option_labels
   end
 
   private
 
-    attr_reader :resource, :query, :search_path, :resource_options, :placeholders, :option_labels
+    attr_reader :resource, :query
 
-    def default_placeholders
+    def resource_options
+      %w[Users Projects]
+    end
+
+    def placeholders
       {
         "Users" => I18n.t("components.search_bar.placeholders.users"),
         "Projects" => I18n.t("components.search_bar.placeholders.projects")
       }
     end
 
-    def default_option_labels
+    def option_labels
       {
         "Users" => I18n.t("components.search_bar.options.users"),
         "Projects" => I18n.t("components.search_bar.options.projects")
