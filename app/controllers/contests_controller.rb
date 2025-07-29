77c9a31c8ebc5783a7182a035e237c7f52a68607
salmoneatenbybear <salmoneatenbybear@gmail.com>
2025-07-29 21:37:11 +0530

    fix(contests): restore set_contest before_action helper and remove duplicate query

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -7,7 +7,10 @@ class ContestsController < ApplicationController
   before_action :set_user_count, only: :show
 
   def index
-    @contests = Contest.order(id: :desc).paginate(page: params[:page]).limit(Contest.per_page)
+    @contests = Contest.order(id: :desc)
+                       .paginate(page: params[:page])
+                       .limit(Contest.per_page)
+
     respond_to do |format|
       format.html
       format.json { render json: @contests }
@@ -16,25 +19,32 @@ class ContestsController < ApplicationController
   end
 
   def show
-    @contest = Contest.find(params[:id])
     @user_submission = @contest.submissions.where(user_id: current_user&.id)
-    @submissions = @contest.submissions.where.not(user_id: current_user&.id).paginate(page: params[:page]).limit(6)
+    @submissions     = @contest.submissions
+                               .where.not(user_id: current_user&.id)
+                               .paginate(page: params[:page])
+                               .limit(6)
 
     return unless @contest.completed? && Submission.exists?(contest_id: @contest.id)
 
-    contest_winner = ContestWinner.find_by(contest_id: @contest.id)
-    @winner = contest_winner.submission if contest_winner.present?
+    if (contest_winner = ContestWinner.find_by(contest_id: @contest.id))
+      @winner = contest_winner.submission
+    end
   end
 
   private
 
-    def check_contests_feature_flag
-      return if Flipper.enabled?(:contests, current_user)
+  def set_contest
+    @contest = Contest.find(params[:id])
+  end
 
-      redirect_to root_path, alert: t("feature_not_available")
-    end
+  def check_contests_feature_flag
+    return if Flipper.enabled?(:contests, current_user)
 
-    def set_user_count
-      @user_count = Rails.cache.fetch("users/total_count", expires_in: 10.minutes) { User.count }
-    end
+    redirect_to root_path, alert: t("feature_not_available")
+  end
+
+  def set_user_count
+    @user_count = Rails.cache.fetch("users/total_count", expires_in: 10.minutes) { User.count }
+  end
 end
