1860f107af76e54356c58d912247b21a127184eb
salmoneatenbybear <salmoneatenbybear@gmail.com>
2025-08-03 21:09:04 +0530

    fix: add guard clause

--- app/services/contest_scheduler.rb
+++ app/services/contest_scheduler.rb
@@ -3,6 +3,7 @@
 class ContestScheduler
   def self.call(contest)
     return if contest.completed?
+    return unless contest.deadline.present?
 
     wait_seconds = [(contest.deadline - Time.zone.now).to_i, 0].max
     ContestDeadlineJob.set(wait: wait_seconds.seconds).perform_later(contest.id)
