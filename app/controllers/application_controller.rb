0f7beb9c47665bcddb3499921d4834a7eec1b96e
salmoneatenbybear <salmoneatenbybear@gmail.com>
2025-08-09 00:56:59 +0530

    chore: rubocop offense in 6391204

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,10 +26,8 @@ class ApplicationController < ActionController::Base
 
   def switch_locale(&)
     logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-    locale = params[:locale]&.to_sym || current_user&.locale || extract_locale_from_accept_language_header 
-    unless I18n.available_locales.include?(locale)
-      locale = I18n.default_locale
-    end
+    locale = params[:locale]&.to_sym || current_user&.locale || extract_locale_from_accept_language_header
+    locale = I18n.default_locale unless I18n.available_locales.include?(locale)
     logger.debug "* Locale set to '#{locale}'"
     I18n.with_locale(locale, &)
   end
