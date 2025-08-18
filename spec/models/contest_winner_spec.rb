2e54264280bcbc652167a44695305f8bbb908a51
salmoneatenbybear <salmoneatenbybear@gmail.com>
2025-08-18 18:46:06 +0530

    chore(spec): use direct english expectations

--- spec/models/contest_winner_spec.rb
+++ spec/models/contest_winner_spec.rb
@@ -4,8 +4,6 @@ require "rails_helper"
 
 RSpec.describe ContestWinner, type: :model do
   describe "associations" do
-    # spec/models/contest_winner.rb
-
     it { is_expected.to belong_to(:submission) }
     it { is_expected.to belong_to(:project) }
     it { is_expected.to belong_to(:contest) }
