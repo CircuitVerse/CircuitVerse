65b2e28d0d357b219dcf6d4cbe241c0afde36f4d
salmoneatenbybear <salmoneatenbybear@gmail.com>
2025-08-19 16:52:09 +0530

    chore: remove helper use inline

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module ContestsHelper
-  include ActionView::Helpers::DateHelper
-
-  def contest_time_left(contest)
-    return t("contest.timer.expired") if contest.deadline.blank? || contest.deadline <= Time.current
-
-    distance_of_time_in_words(Time.current, contest.deadline)
-  end
 end
