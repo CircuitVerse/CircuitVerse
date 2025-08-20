b664f48f2c715c09b105b4d01c5ef0286a15c0ea
senbo1 <senbocodes@gmail.com>
2025-08-20 22:04:00 +0530

    added tests

--- app/components/search/search_bar_component.rb
+++ app/components/search/search_bar_component.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Search::SearchBarComponent < ViewComponent::Base
+  RESOURCE_OPTIONS = %w[Users Projects].freeze
+  DEFAULT_SORT_BY = "created_at".freeze
+  DEFAULT_SORT_DIRECTION = "desc".freeze
+
   def initialize(
     resource: nil,
     query: nil,
@@ -10,63 +14,51 @@ class Search::SearchBarComponent < ViewComponent::Base
     super
     @resource = resource
     @query = query
-    @sort_by = sort_by || default_sort_by
-    @sort_direction = sort_direction || default_sort_direction
+    @sort_by = sort_by || DEFAULT_SORT_BY
+    @sort_direction = sort_direction || DEFAULT_SORT_DIRECTION
   end
+
+  attr_reader :resource, :query, :sort_by, :sort_direction
 
   def all_sorting_options
     {
       "Users" => sorting_options_for_users,
       "Projects" => sorting_options_for_projects
-    }
+    }.freeze
+  end
+
+  def resource_options
+    RESOURCE_OPTIONS
+  end
+
+  def placeholders
+    {
+      "Users" => I18n.t("components.search_bar.placeholders.users"),
+      "Projects" => I18n.t("components.search_bar.placeholders.projects")
+    }.freeze
+  end
+
+  def option_labels
+    {
+      "Users" => I18n.t("components.search_bar.options.users"),
+      "Projects" => I18n.t("components.search_bar.options.projects")
+    }.freeze
   end
 
   private
 
-    attr_reader :resource, :query, :sort_by, :sort_direction
-
-    def resource_options
-      %w[Users Projects]
-    end
-
-    def placeholders
-      {
-        "Users" => I18n.t("components.search_bar.placeholders.users"),
-        "Projects" => I18n.t("components.search_bar.placeholders.projects")
-      }
-    end
-
-    def option_labels
-      {
-        "Users" => I18n.t("components.search_bar.options.users"),
-        "Projects" => I18n.t("components.search_bar.options.projects")
-      }
-    end
-
     def sorting_options_for_users
       [
-        { value: "created_at", label: I18n.t("components.search_bar.sorting.users.join_date") },
-        { value: "total_circuits", label: I18n.t("components.search_bar.sorting.users.total_circuits") }
-      ]
+        { value: "created_at", label: I18n.t("components.search_bar.sorting.users.join_date") }.freeze,
+        { value: "total_circuits", label: I18n.t("components.search_bar.sorting.users.total_circuits") }.freeze
+      ].freeze
     end
 
     def sorting_options_for_projects
       [
-        { value: "created_at", label: I18n.t("components.search_bar.sorting.projects.created_date") },
-        { value: "views", label: I18n.t("components.search_bar.sorting.projects.views") },
-        { value: "stars", label: I18n.t("components.search_bar.sorting.projects.stars") }
-      ]
-    end
-
-    def current_sorting_options
-      resource == "Users" ? sorting_options_for_users : sorting_options_for_projects
-    end
-
-    def default_sort_by
-      "created_at"
-    end
-
-    def default_sort_direction
-      "desc" # Default to descending (newest first)
+        { value: "created_at", label: I18n.t("components.search_bar.sorting.projects.created_date") }.freeze,
+        { value: "views", label: I18n.t("components.search_bar.sorting.projects.views") }.freeze,
+        { value: "stars", label: I18n.t("components.search_bar.sorting.projects.stars") }.freeze
+      ].freeze
     end
 end
--- spec/components/search_bar_component_spec.rb
+++ spec/components/search_bar_component_spec.rb
@@ -21,4 +21,33 @@ RSpec.describe Search::SearchBarComponent, type: :component do
 
     expect(page).to have_button(type: "submit")
   end
+
+  describe "#current_sorting_options" do
+    context "when resource is Users" do
+      let(:component) { described_class.new(resource: "Users") }
+
+      it "returns sorting options for users" do
+        expected_options = [
+          { value: "created_at", label: I18n.t("components.search_bar.sorting.users.join_date") }.freeze,
+          { value: "total_circuits", label: I18n.t("components.search_bar.sorting.users.total_circuits") }.freeze
+        ].freeze
+
+        expect(component.send(:current_sorting_options)).to eq(expected_options)
+      end
+    end
+
+    context "when resource is Projects" do
+      let(:component) { described_class.new(resource: "Projects") }
+
+      it "returns sorting options for projects" do
+        expected_options = [
+          { value: "created_at", label: I18n.t("components.search_bar.sorting.projects.created_date") }.freeze,
+          { value: "views", label: I18n.t("components.search_bar.sorting.projects.views") }.freeze,
+          { value: "stars", label: I18n.t("components.search_bar.sorting.projects.stars") }.freeze
+        ].freeze
+
+        expect(component.send(:current_sorting_options)).to eq(expected_options)
+      end
+    end
+  end
 end
