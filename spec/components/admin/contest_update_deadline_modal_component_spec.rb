e18d89d2c4ab221fedef08c4aa82b0277733964d
salmoneatenbybear <salmoneatenbybear@gmail.com>
2025-08-24 00:01:46 +0530

    admin-contest-delete-feature

--- spec/components/admin/contest_update_deadline_modal_component_spec.rb
+++ spec/components/admin/contest_update_deadline_modal_component_spec.rb
@@ -4,15 +4,17 @@ require "rails_helper"
 
 RSpec.describe Admin::ContestUpdateDeadlineModalComponent, type: :component do
   it "renders the edit-deadline form pre-filled with current deadline" do
-    contest = create(:contest, :live)
+    contest = create(:contest, status: :live, deadline: 2.days.from_now)
 
     render_inline(described_class.new(contest: contest))
 
-    expect(page).to have_css("#update-contest-modal")
-    expect(page).to have_css("h4.modal-title", text: "Update Contest Deadline")
+    expect(page).to have_css("#update-contest-modal-#{contest.id}")
 
-    formatted_deadline = contest.deadline.in_time_zone.strftime("%Y-%m-%dT%H:%M")
-    expect(page).to have_css("input#contest_deadline[value='#{formatted_deadline}']")
+    expect(page).to have_text("Update Contest Deadline")
+    expect(page).to have_text("Please ensure that the Contest Deadline is set in the future.")
+
+    expected_value = contest.deadline.in_time_zone.strftime("%Y-%m-%dT%H:%M")
+    expect(page).to have_field("contest[deadline]", with: expected_value)
     expect(page).to have_button("Update Deadline")
   end
 end
