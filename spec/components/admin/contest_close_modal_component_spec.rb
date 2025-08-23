74de4ab01cef5e61f2bf832bddfea4dd64c4f99b
salmoneatenbybear <salmoneatenbybear@gmail.com>
2025-08-24 00:07:33 +0530

    Revert "admin-contest-delete-feature"
    This reverts commit e18d89d2c4ab221fedef08c4aa82b0277733964d.

--- spec/components/admin/contest_close_modal_component_spec.rb
+++ spec/components/admin/contest_close_modal_component_spec.rb
@@ -3,15 +3,17 @@
 require "rails_helper"
 
 RSpec.describe Admin::ContestCloseModalComponent, type: :component do
+  include Rails.application.routes.url_helpers
+
   it "renders heading, description and close button" do
-    contest = create(:contest, status: :live)
+    contest = create(:contest, :live)
 
     render_inline(described_class.new(contest: contest))
 
-    expect(page).to have_css("#close-contest-confirmation-modal-#{contest.id}")
-
-    expect(page).to have_text("Close a Contest")
-    expect(page).to have_text("Are you sure you want to close this contest?")
+    expect(page).to have_css("#close-contest-confirmation-modal")
+    expect(page).to have_css("h4.modal-title", text: "Close a Contest")
     expect(page).to have_button("Confirm")
+
+    expect(page).to have_css("form[action='#{admin_contest_path(contest)}']")
   end
 end
--- spec/components/admin/contest_update_deadline_modal_component_spec.rb
+++ spec/components/admin/contest_update_deadline_modal_component_spec.rb
@@ -4,17 +4,15 @@ require "rails_helper"
 
 RSpec.describe Admin::ContestUpdateDeadlineModalComponent, type: :component do
   it "renders the edit-deadline form pre-filled with current deadline" do
-    contest = create(:contest, status: :live, deadline: 2.days.from_now)
+    contest = create(:contest, :live)
 
     render_inline(described_class.new(contest: contest))
 
-    expect(page).to have_css("#update-contest-modal-#{contest.id}")
+    expect(page).to have_css("#update-contest-modal")
+    expect(page).to have_css("h4.modal-title", text: "Update Contest Deadline")
 
-    expect(page).to have_text("Update Contest Deadline")
-    expect(page).to have_text("Please ensure that the Contest Deadline is set in the future.")
-
-    expected_value = contest.deadline.in_time_zone.strftime("%Y-%m-%dT%H:%M")
-    expect(page).to have_field("contest[deadline]", with: expected_value)
+    formatted_deadline = contest.deadline.in_time_zone.strftime("%Y-%m-%dT%H:%M")
+    expect(page).to have_css("input#contest_deadline[value='#{formatted_deadline}']")
     expect(page).to have_button("Update Deadline")
   end
 end
