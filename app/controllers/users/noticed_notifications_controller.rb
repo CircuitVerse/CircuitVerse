0da193dca261cd93b11cfd60808c097ef12961c8
Himanshujays29 <himanshujayswal2006@gmail.com>
2025-09-24 07:57:09 +0530

    Fix ActiveJob serialization error by permitting only safe params (#6075)

--- app/controllers/users/noticed_notifications_controller.rb
+++ app/controllers/users/noticed_notifications_controller.rb
@@ -11,7 +11,7 @@ class Users::NoticedNotificationsController < ApplicationController
   def mark_as_read
     notification = NoticedNotification.find(params[:notification_id])
     notification.update(read_at: Time.zone.now)
-    answer = NotifyUser.new(params).call
+    answer = NotifyUser.new(params.permit(:notification_id)).call
     redirect_to redirect_path_for(answer)
   end
 
