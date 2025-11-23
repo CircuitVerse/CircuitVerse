d000cd5c607b3daaae9e2be3c019346ad807ddf6
Encripted <mauryaramlaxman42@gmail.com>
2025-11-23 09:14:13 +0000

    Improve search page: handle blank query with friendly message and image

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,12 +7,22 @@ class SearchController < ApplicationController
     resource = params[:resource]
     query_params = params
 
+    #If no query param is given, show message instead of 404
+    if query_params[:q].blank?
+      @results = []
+      flash.now[:notice] = "Please enter a search term."
+      render :search_results and return
+    end
+
     @results, template = query(resource, query_params)
 
     if template.present?
       render template
     else
-      not_found
+      #If template not found, fallback to a generic results view with message
+      @results ||= []
+      flash.now[:alert] = "Search results could not be displayed."
+      render :search_resutls,status: :ok
     end
   end
 end
