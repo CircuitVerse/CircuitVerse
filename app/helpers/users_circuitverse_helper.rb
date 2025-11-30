a5bd33669ca43200bf5363d5c72429be81c1bd4c
Aboobacker MK <aboobackervyd@gmail.com>
2025-11-30 20:31:20 +0530

    Fix build failure

--- app/helpers/users_circuitverse_helper.rb
+++ app/helpers/users_circuitverse_helper.rb
@@ -12,7 +12,7 @@ module UsersCircuitverseHelper
     if attachment.attached?
       attachment
     else
-      image_path("thumb/Default.jpg")
+      image_path("/thumb/default.png")
     end
   end
 end
