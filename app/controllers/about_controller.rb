9b782dabfe1fb81a3217c1f6746e8f6d9cd70ada
Aditya Singh <salmoneatenbybear@gmail.com>
2025-12-05 18:42:16 +0530

    chore: update about page with new core team members and remove old issue triaging team. (#6428)
    chore: update about page with new core team members

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -22,7 +22,17 @@ class AboutController < ApplicationController
       { name: "Tanmoy Sarkar",
         img: "https://avatars.githubusercontent.com/u/57363826?v=4", link: "https://github.com/tanmoysrt" },
       { name: "Arnabdaz",
-        img: "https://avatars.githubusercontent.com/u/96580571?v=4", link: "https://github.com/Arnabdaz" }
+        img: "https://avatars.githubusercontent.com/u/96580571?v=4", link: "https://github.com/Arnabdaz" },
+      { name: "Aditya Umesh Singh",
+        img: "https://avatars.githubusercontent.com/u/177742943?v=4", link: "https://github.com/salmoneatenbybear" },
+      { name: "Harsh Rao",
+        img: "https://avatars.githubusercontent.com/u/166894150?v=4", link: "https://github.com/ThatDeparted2061" },
+      { name: "Nihal Rajpal",
+        img: "https://avatars.githubusercontent.com/u/65150640?v=4", link: "https://github.com/nihal4777" },
+      { name: "Yashvant Singh",
+        img: "https://avatars.githubusercontent.com/u/146776812?v=4", link: "https://github.com/JatsuAkaYashvant" },
+      { name: "Harsh Bhadu",
+        img: "https://avatars.githubusercontent.com/u/102225113?v=4", link: "https://github.com/senbo1" }
     ]
 
     @mentors = []
@@ -56,19 +66,7 @@ class AboutController < ApplicationController
       { name: "Aman Singh",
         img: "https://avatars.githubusercontent.com/u/77198905?v=4", link: "https://github.com/aman-singh7" }
     ]
-    @issues_triaging = [
-      { name: "Sannidhi Nair",
-        img: "https://avatars.githubusercontent.com/u/165994623?v=4",
-        link: "https://github.com/SannidhiNair" },
-      { name: "Aditya Singh",
-        img: "https://avatars.githubusercontent.com/u/177742943?v=4",
-        link: "https://github.com/salmoneatenbybear" },
-      { name: "Harsh Rao",
-        img: "https://avatars.githubusercontent.com/u/166894150?v=4",
-        link: "https://github.com/ThatDeparted2061" },
-      { name: "Neeraj Pathak",
-        img: "https://avatars.githubusercontent.com/u/171167348?v=4",
-        link: "https://github.com/Neerajpathak07" }
-    ]
+
+    @issues_triaging = []
   end
 end
