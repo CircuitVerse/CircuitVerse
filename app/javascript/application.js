6b8ebfe30a995ef310ae43d762860710e6dfd2fc
ANIRUDH <anirudhnarang0@gmail.com>
2025-12-23 18:54:41 +0530

    Migrate from Sprockets to Propshaft + Vite

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
+// This line connects your styles to Vite
+import '../stylesheets/application.scss';
+
 import './controllers';
 import 'bootstrap';
 import '@popperjs/core';
---
+++ app/javascript/entrypoints/application.js
@@ -0,0 +1,7 @@
+// app/javascript/entrypoints/application.js
+
+// This tells Vite to build your SCSS file
+import '../stylesheets/application.scss';
+
+// Import other global JS here if needed (like bootstrap/jquery)
+// import 'bootstrap';
