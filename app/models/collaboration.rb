8e71f3d7d56a6858489a7556fba16ee9f497a92f
Janvi Chaturvedi <cjanvi1010@gmail.com>
2026-01-15 16:19:37 +0530

    Prevent duplicate collaborators: add model validation and DB unique index

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -3,4 +3,6 @@
 class Collaboration < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  # Prevent same user from being added twice to same project
+  validates :user_id, uniqueness: { scope: :project_id, message: "is already a collaborator on this project" }
 end
