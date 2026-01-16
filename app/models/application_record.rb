16d72bfddeae4fbc47d190e85c40e22ce1ee897a
kawkoi <haruubusinesss@gmail.com>
2026-01-16 16:54:26 +0530

    fixed sentry issue

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   include Noticed::HasNotifications
+
+  before_validation :sanitize_utf8_attributes
+
+  private
+
+  def sanitize_utf8_attributes
+    attributes.each do |key, value|
+      next unless value.is_a?(String)
+
+      self[key] = value.scrub("")
+    end
+  end
 end
