7f8716b3374d3fd8dd11c63d474edcdca4f6822b
Mustansir-06 <mustansirdabhiya@gmail.com>
2026-01-17 15:02:31 +0530

    feat: add validations to Collaboration model
    - Enforce uniqueness of user scoped to project
    - Prevent project owners from being added as collaborators
    - Add comprehensive RSpec tests for new validations

--- spec/models/collaboration_spec.rb
+++ spec/models/collaboration_spec.rb
@@ -7,4 +7,41 @@ RSpec.describe Collaboration, type: :model do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:project) }
   end
+
+  describe "validations" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:project) { FactoryBot.create(:project) }
+
+    it "is valid with a user and a project" do
+      collaboration = Collaboration.new(user: user, project: project)
+      expect(collaboration).to be_valid
+    end
+
+    it "is invalid without a user" do
+      collaboration = Collaboration.new(user: nil, project: project)
+      expect(collaboration).not_to be_valid
+      expect(collaboration.errors[:user]).to include("must exist")
+    end
+
+    it "is invalid without a project" do
+      collaboration = Collaboration.new(user: user, project: nil)
+      expect(collaboration).not_to be_valid
+      expect(collaboration.errors[:project]).to include("must exist")
+    end
+
+    it "validates uniqueness of user scoped to project" do
+      
+      Collaboration.create!(user: user, project: project)
+      
+      duplicate = Collaboration.new(user: user, project: project)
+      
+      expect(duplicate).not_to be_valid
+    end
+    
+    it "is invalid if the user is already the owner of the project" do
+      owner = project.author
+      collaboration = Collaboration.new(user: owner, project: project)
+      expect(collaboration).not_to be_valid
+    end
+  end
 end
