5fe5318d2b2a35b27ea74d58933942aaaa1ddb28
Teesta Mukherjee <teesta.mukherj55@gmail.com>
2026-01-24 19:57:58 +0000

    Fix reports request specs

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -3,17 +3,18 @@
 require "rails_helper"
 
 RSpec.describe "Reports", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/reports/new"
-      expect(response).to have_http_status(:success)
+  describe "GET /reports/new" do
+    it "redirects when user is not signed in" do
+      get new_report_path
+      expect(response).to have_http_status(:found) # 302
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/reports/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /reports" do
+    it "redirects when user is not signed in" do
+      post reports_path, params: { report: { reported_user_id: 1, reason: "Spam" } }
+      expect(response).to have_http_status(:found) # 302
     end
   end
 end
+
