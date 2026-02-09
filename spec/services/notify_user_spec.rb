cc596fb05d269c2af458c245a6487aabd758d5a7
Me Priyank <pvonlyone29@gmail.com>
2026-02-09 21:58:56 +0530

    fix: Make notification a required param in NotifyUser to prevent IDOR bypass

--- spec/services/notify_user_spec.rb
+++ spec/services/notify_user_spec.rb
@@ -7,20 +7,22 @@ RSpec.describe NotifyUser do
   let(:contest)   { create(:contest) }
 
   it "maps ContestNotification" do
-    n = NoticedNotification.create!(recipient: recipient,
-                                    type: "ContestNotification",
-                                    params: { contest: contest })
-    res = described_class.new(notification_id: n.id).call
+    notification = NoticedNotification.create!(recipient: recipient,
+                                               type: "ContestNotification",
+                                               params: { contest: contest })
+    # SECURITY: Pass the notification object directly (required parameter)
+    res = described_class.new({ notification_id: notification.id }, notification).call
     expect(res.success).to eq("true")
     expect(res.type).to eq("new_contest")
     expect(res.first_param).to eq(contest)
   end
 
   it "maps ContestWinnerNotification" do
-    n = NoticedNotification.create!(recipient: recipient,
-                                    type: "ContestWinnerNotification",
-                                    params: {})
-    res = described_class.new(notification_id: n.id).call
+    notification = NoticedNotification.create!(recipient: recipient,
+                                               type: "ContestWinnerNotification",
+                                               params: {})
+    # SECURITY: Pass the notification object directly (required parameter)
+    res = described_class.new({ notification_id: notification.id }, notification).call
     expect(res.success).to eq("true")
     expect(res.type).to eq("contest_winner")
   end
