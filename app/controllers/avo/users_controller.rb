60962fdadc6dd2ae34f820d7b088f854f3475c72
Supreeth <supreeth2020@gmail.com>
2026-02-10 19:45:33 +0530

    fix: remove user controller(unrelated to this PR)

--- app/controllers/avo/users_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# This controller has been generated to enable Rails' resource routes.
-# More information on https://docs.avohq.io/3.0/controllers.html
-class Avo::UsersController < Avo::ResourcesController
-end
