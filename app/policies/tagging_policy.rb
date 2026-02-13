ce9e71f548a4abbaad7177c22884b4aa5c3dad86
Naman Chhabra <159947791+naman79820@users.noreply.github.com>
2026-02-13 12:58:21 +0530

    Add TaggingPolicy for user permissions

---
+++ app/policies/tagging_policy.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class TaggingPolicy < ApplicationPolicy
+  attr_reader :user, :tagging
+
+  def initialize(user, tagging)
+    @user = user
+    @tagging = tagging
+  end
+
+  def index?
+    user.present? && user.admin?
+  end
+
+  def show?
+    user.present? && user.admin?
+  end
+
+  def create?
+    user.present? && user.admin?
+  end
+
+  def destroy?
+    user.present? && user.admin?
+  end
+end
