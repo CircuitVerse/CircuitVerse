122cb1bf2c17d4ae463e4395d4439028906107fa
Encripted <mauryaramlaxman42@gmail.com>
2026-02-19 06:15:04 +0000

    Add request specs for ExploreController feature flag and section redirect

---
+++ spec/controllers/explore_controller_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ExploreController, type: :controller do
+  describe "GET #index" do
+    context "when feature flag is disabled" do
+      before do
+        allow(Flipper).to receive(:enabled?)
+          .with(:circuit_explore_page, anything)
+          .and_return(false)
+      end
+
+      it "redirects to root path" do
+        get :index
+
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "when feature flag is enabled" do
+      before do
+        allow(Flipper).to receive(:enabled?)
+          .with(:circuit_explore_page, anything)
+          .and_return(true)
+      end
+
+      it "returns http success" do
+        get :index
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      context "when section param is 'examples'" do
+        it "redirects to picks section" do
+          get :index, params: { section: "examples" }
+
+          expect(response).to redirect_to(explore_path(section: "picks"))
+        end
+      end
+    end
+  end
+end
