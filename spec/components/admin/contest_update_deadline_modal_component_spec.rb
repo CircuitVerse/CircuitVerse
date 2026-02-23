a2e38a562b05fc756c5b4250a692e8168b87dd75
Aakansha Chavan <work.aakanshachavan9@gmail.com>
2026-02-23 16:09:05 +0000

    Fix admin contest modal IDs and restrict entry count to completed status

--- spec/components/admin/contest_update_deadline_modal_component_spec.rb
+++ spec/components/admin/contest_update_deadline_modal_component_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe Admin::ContestUpdateDeadlineModalComponent, type: :component do
 
     render_inline(described_class.new(contest: contest))
 
-    expect(page).to have_css("#update-contest-modal")
+    expect(page).to have_css("#update-contest-modal-#{contest.id}")
     expect(page).to have_css("h4.modal-title", text: "Update Contest Deadline")
 
     formatted_deadline = contest.deadline.in_time_zone.strftime("%Y-%m-%dT%H:%M")
-    expect(page).to have_css("input#contest_deadline[value='#{formatted_deadline}']")
+    expect(page).to have_css("input#contest_deadline_#{contest.id}[value='#{formatted_deadline}']")
     expect(page).to have_button("Update Deadline")
   end
 end
