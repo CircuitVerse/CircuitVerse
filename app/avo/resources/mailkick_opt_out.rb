803fc2f366e2a7528cb0c75d7d4da794f45ea162
Supreeth <supreeth2020@gmail.com>
2026-02-26 01:12:10 +0530

    feat: add resources for events,visits and mailkick

---
+++ app/avo/resources/mailkick_opt_out.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Avo::Resources::MailkickOptOut < Avo::BaseResource
+  self.title = :email
+  self.includes = [:user]
+  self.model_class = ::Mailkick::OptOut
+
+  def fields
+    field :id, as: :id, link_to_resource: true
+    field :email, as: :text, sortable: true, required: true
+
+    field :active, as: :boolean, sortable: true
+    field :reason, as: :text
+    field :list, as: :text, sortable: true
+
+    field :created_at, as: :date_time, readonly: true, sortable: true, hide_on: %i[new edit]
+    field :updated_at, as: :date_time, readonly: true, sortable: true, hide_on: %i[new edit]
+  end
+end
