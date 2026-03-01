0fafeab831d891f483f9440edbb54031f8a049ca
Aakansha Chavan <work.aakanshachavan9@gmail.com>
2026-03-01 13:18:28 +0000

    feat: enhance contest card and add submissions count

--- db/migrate/20260227150250_add_submissions_count_to_contests.rb
+++ db/migrate/20260227150250_add_submissions_count_to_contests.rb
@@ -1,5 +1,14 @@
 class AddSubmissionsCountToContests < ActiveRecord::Migration[8.0]
-  def change
-    add_column :contests, :submissions_count, :integer ,default: 0, null: false
+  def up
+    add_column :contests, :submissions_count, :integer, default: 0, null: false
+
+    Contest.reset_column_information
+    Contest.find_each do |contest|
+      Contest.reset_counters(contest.id, :submissions)
+    end
+  end
+
+  def down
+    remove_column :contests, :submissions_count
   end
 end
