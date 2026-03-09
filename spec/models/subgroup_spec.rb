25b9706e0a7b2471777ca9ef79dc2ca87718b3d3
Teesta Mukherjee <168867474+Teesta-Mukherjee@users.noreply.github.com>
2026-03-09 14:49:57 +0000

    model tests for subgroup validation

--- spec/models/subgroup_spec.rb
+++ spec/models/subgroup_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Subgroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it "is valid with a name and group" do
+    group = Group.create!(name: "Test Group")
+    subgroup = Subgroup.new(name: "Subgroup A", group: group)
+
+    expect(subgroup).to be_valid
+  end
+
+  it "is invalid without a name" do
+    group = Group.create!(name: "Test Group")
+    subgroup = Subgroup.new(name: nil, group: group)
+
+    expect(subgroup).not_to be_valid
+  end
+
+  it "is invalid without a group" do
+    subgroup = Subgroup.new(name: "Subgroup A", group: nil)
+
+    expect(subgroup).not_to be_valid
+  end
 end
