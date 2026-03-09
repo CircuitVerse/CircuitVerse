51e3c400e7decb6892ff832ef6529f661c5a2d69
Teesta Mukherjee <168867474+Teesta-Mukherjee@users.noreply.github.com>
2026-03-09 19:45:03 +0000

    Add Subgroup model spec with validations and association

--- spec/models/subgroup_spec.rb
+++ spec/models/subgroup_spec.rb
@@ -1,23 +1,29 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe Subgroup, type: :model do
- it "is valid with a name and group" do
-    group = Group.create!(name: "Test Group")
-    subgroup = Subgroup.new(name: "Subgroup A", group: group)
-
-    expect(subgroup).to be_valid
+  describe "associations" do
+    it { is_expected.to belong_to(:group) }
   end
 
-  it "is invalid without a name" do
-    group = Group.create!(name: "Test Group")
-    subgroup = Subgroup.new(name: nil, group: group)
+  describe "validations" do
+    let(:group) { FactoryBot.create(:group) }
 
-    expect(subgroup).not_to be_valid
-  end
+    it "is valid with valid attributes" do
+      subgroup = Subgroup.new(name: "Subgroup A", group: group)
+      expect(subgroup).to be_valid
+    end
 
-  it "is invalid without a group" do
-    subgroup = Subgroup.new(name: "Subgroup A", group: nil)
+    it "is invalid without a name" do
+      subgroup = Subgroup.new(name: nil, group: group)
+      expect(subgroup).not_to be_valid
+    end
 
-    expect(subgroup).not_to be_valid
+    it "is invalid without a group" do
+      subgroup = Subgroup.new(name: "Subgroup A", group: nil)
+      expect(subgroup).not_to be_valid
+    end
   end
 end
+ 
