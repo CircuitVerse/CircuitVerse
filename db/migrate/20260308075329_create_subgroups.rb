f20346f4c3dda1372fe2d0810fb86de7d2c9c06e
Teesta Mukherjee <168867474+Teesta-Mukherjee@users.noreply.github.com>
2026-03-09 14:38:49 +0000

    Fix: enforce name presence at database level

--- db/migrate/20260308075329_create_subgroups.rb
+++ db/migrate/20260308075329_create_subgroups.rb
@@ -1,7 +1,7 @@
 class CreateSubgroups < ActiveRecord::Migration[8.0]
   def change
     create_table :subgroups do |t|
-      t.string :name
+      t.string :name, null:false
       t.references :group, null: false, foreign_key: true
 
       t.timestamps
