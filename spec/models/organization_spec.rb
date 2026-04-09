9bfd670c2f2f4615ecb54bc328d60a8e1204e4b3
Teesta Mukherjee <168867474+Teesta-Mukherjee@users.noreply.github.com>
2026-04-09 16:20:54 +0000

    Fix: add model tests for organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    org = Organization.new(name: "Test Org", slug: "test-org")
+    expect(org).to be_valid
+  end
+
+  it "is invalid without name" do
+    org = Organization.new(slug: "test-org")
+    expect(org).not_to be_valid
+  end
+
+  it "is invalid without slug" do
+    org = Organization.new(name: "Test Org")
+    expect(org).not_to be_valid
+  end
+
+  it "has many groups" do
+    assoc = Organization.reflect_on_association(:groups)
+    expect(assoc.macro).to eq(:has_many)
+  end
 end
