0ebd8dec1bd3d98cd37e367d52f2565d01f6a9be
Aakansha Chavan <work.aakanshachavan9@gmail.com>
2026-04-23 18:05:24 +0000

    Remove migration to keep PR frontend-only

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Submission < ApplicationRecord
-  belongs_to :contest, counter_cache: true
+  belongs_to :contest
   belongs_to :project
   belongs_to :user
   has_many :submission_votes, dependent: :destroy
