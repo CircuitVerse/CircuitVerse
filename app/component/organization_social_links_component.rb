d83fc4ed165415cafb6d686ed1e0638143136d64
naman79820 <naman79820@gmail.com>
2026-08-03 20:11:47 +0000

    refractor: normalize social link host to base admin and removed root theme SCSS

--- app/component/organization_social_links_component.rb
+++ app/component/organization_social_links_component.rb
@@ -29,18 +29,17 @@ class OrganizationSocialLinksComponent < ViewComponent::Base
     end
 
     def provider_for(host)
-      return { name: "LinkedIn", logo: "logos/linkedin-logo.png" } if linkedin?(host)
-
-      case host
+      case base_domain(host)
       when "github.com" then { name: "GitHub", logo: "logos/github-logo-circle.png" }
       when "facebook.com" then { name: "Facebook", logo: "logos/facebook-logo.png" }
       when "twitter.com", "x.com" then { name: "X", logo: "logos/twitter-x.png" }
       when "youtube.com" then { name: "YouTube", logo: "logos/youtube-logo.png" }
+      when "linkedin.com" then { name: "LinkedIn", logo: "logos/linkedin-logo.png" }
       else { name: I18n.t("organizations.social_links.website"), logo: "logos/link-logo.png" }
       end
     end
 
-    def linkedin?(host)
-      host == "linkedin.com" || host.to_s.end_with?(".linkedin.com")
+    def base_domain(host)
+      host.to_s.split(".").last(2).join(".")
     end
 end
