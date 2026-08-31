e247e545b8629b05ac100550181bb9cee51f48d0
naman79820 <naman79820@gmail.com>
2026-08-31 13:40:56 +0000

    fix(organizations): make the slug removal migration irreversible

--- db/migrate/20260826101823_remove_slug_from_organizations.rb
+++ db/migrate/20260826101823_remove_slug_from_organizations.rb
@@ -1,9 +1,13 @@
 class RemoveSlugFromOrganizations < ActiveRecord::Migration[8.1]
-  def change
+  def up
     safety_assured do
       remove_index :organizations, name: "index_organizations_on_slug"
       remove_check_constraint :organizations, name: "organizations_slug_not_blank"
-      remove_column :organizations, :slug, :string, null: false
+      remove_column :organizations, :slug
     end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, "Slug data cannot be restored once removed"
   end
 end
